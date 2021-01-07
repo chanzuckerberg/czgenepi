88f85c6d57e3ff8d2eecd482a967a56320d1ca16
lvreynoso <lvreynoso@protonmail.com>
2021-01-07 15:17:05 -0800

    Add pre-commit hooks for prettier and eslint

--- src/ts/scripts/start.js
+++ src/ts/scripts/start.js
@@ -1,32 +1,31 @@
-'use strict';
+"use strict";
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'development';
-process.env.NODE_ENV = 'development';
+process.env.BABEL_ENV = "development";
+process.env.NODE_ENV = "development";
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
+process.on("unhandledRejection", (err) => {
   throw err;
 });
 
 // Ensure environment variables are read.
-require('../config/env');
+require("../config/env");
 
-
-const webpack = require('webpack');
-const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
-const paths = require('../config/paths');
-const configFactory = require('../config/webpack.config');
-const getClientEnvironment = require('../config/env');
+const webpack = require("webpack");
+const checkRequiredFiles = require("react-dev-utils/checkRequiredFiles");
+const paths = require("../config/paths");
+const configFactory = require("../config/webpack.config");
+const getClientEnvironment = require("../config/env");
 
 // Warn and crash if required files are missing
 if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
   process.exit(1);
 }
 
-const config = configFactory('development');
+const config = configFactory("development");
 const compiler = webpack(config);
 const watcher = compiler.watch({}, (err, stats) => {
   if (err) {
@@ -34,10 +33,12 @@ const watcher = compiler.watch({}, (err, stats) => {
     return;
   }
 
-  console.log(stats.toString({
-    chunks: false,  // Makes the build much quieter
-    colors: true    // Shows colors in the console
-  }));
-})
+  console.log(
+    stats.toString({
+      chunks: false, // Makes the build much quieter
+      colors: true, // Shows colors in the console
+    })
+  );
+});
 
-console.log("Webpack is watching for changes...\n")
+console.log("Webpack is watching for changes...\n");
