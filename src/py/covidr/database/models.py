de2313ac60e8e7a276d1596bdee933a13a568293
Tony Tung <ttung@chanzuckerberg.com>
2021-01-15 13:51:43 -0800

    Top level user/group objects
    ### Description
    These are the basic objects we need to log someone in.
    
    Also enabled the plugin for mypy+sqlalchemy to be nice to each other.
    
    #### Issue
    [ch65314](https://app.clubhouse.io/genepi/story/65314/)
    
    ### Test plan
    created the tables.

--- src/py/covidr/database/models.py
+++ src/py/covidr/database/models.py
@@ -1,6 +1,7 @@
 # the main models in the database
-from sqlalchemy import MetaData
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, MetaData, String
+from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
+from sqlalchemy.orm import backref, relationship
 
 from . import modelmixins as mx
 
@@ -14,4 +15,34 @@ meta = MetaData(
         "pk": "pk_%(table_name)s",
     },
 )
-base = declarative_base(cls=mx.BaseMixin, metadata=meta)
+# typing "base" as DeclarativeMeta is the ugly workaround for
+# https://github.com/python/mypy/issues/2477
+base: DeclarativeMeta = declarative_base(cls=mx.BaseMixin, metadata=meta)
+
+
+class Group(base):
+    """A group of users, generally a department of public health."""
+
+    __tablename__ = "groups"
+
+    name = Column(String, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Group <{self.name}>"
+
+
+class User(base):
+    """A user."""
+
+    __tablename__ = "users"
+
+    name = Column(String, nullable=False)
+    auth0_user_id = Column(String, unique=True, nullable=False)
+    group_admin = Column(Boolean, nullable=False)
+    system_admin = Column(Boolean, nullable=False)
+
+    group_id = Column(Integer, ForeignKey("groups.id"), nullable=False)
+    group = relationship(Group, backref=backref("users", uselist=True))
+
+    def __repr__(self):
+        return f"User <{self.name}>"
