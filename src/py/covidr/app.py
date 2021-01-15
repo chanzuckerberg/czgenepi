1705f2f0f571cdf24ee0ef71b33e5d237de6e31a
lvreynoso <lvreynoso@protonmail.com>
2021-01-15 15:01:55 -0800

    Fix linting issues

--- src/py/covidr/app.py
+++ src/py/covidr/app.py
@@ -6,11 +6,12 @@ from flask import Flask, send_from_directory
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # EB looks for an 'application' callable by default.
-application = Flask(__name__, static_folder=f'{dir_path}/static')
+application = Flask(__name__, static_folder=f"{dir_path}/static")
 
 
 if os.environ.get("FLASK_ENV") == "development":
     application.config.from_object(DevelopmentConfig())
+
 
 # Catch all routes. If path is a file, send the file;
 # else send index.html. Allows reloading React app from any route.
