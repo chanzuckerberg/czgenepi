0521ff07dfacb1c3e1b609dabde7a4dec1ebcdeb
lvreynoso <lvreynoso@protonmail.com>
2021-01-15 14:13:19 -0800

    Use os-defined path for static folder

--- src/py/covidr/app.py
+++ src/py/covidr/app.py
@@ -3,8 +3,10 @@ import os
 from covidr.config import DevelopmentConfig
 from flask import Flask, send_from_directory
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 # EB looks for an 'application' callable by default.
-application = Flask(__name__, static_folder="static")
+application = Flask(__name__, static_folder=f'{dir_path}/static')
 
 
 if os.environ.get("FLASK_ENV") == "development":
@@ -15,7 +17,8 @@ if os.environ.get("FLASK_ENV") == "development":
 @application.route("/", defaults={"path": ""})
 @application.route("/<path:path>")
 def serve(path):
-    if path != "" and os.path.exists(application.static_folder + "/" + path):
-        return send_from_directory(application.static_folder, path)
-    else:
-        return send_from_directory(application.static_folder, "index.html")
+    return application.static_folder
+    # if path != "" and os.path.exists(application.static_folder + "/" + path):
+    #     return send_from_directory(application.static_folder, path)
+    # else:
+    #     return send_from_directory(application.static_folder, "index.html")
