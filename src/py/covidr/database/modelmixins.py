886ea8fd6e7c666b556dff11e0f9286369254a2e
Tony Tung <ttung@chanzuckerberg.com>
2021-01-15 15:56:18 -0800

    Top level user/group objects (#11)
    ### Description
    These are the basic objects we need to log someone in.
    
    Also enabled the plugin for mypy+sqlalchemy to be nice to each other.
    
    #### Issue
    [ch65314](https://app.clubhouse.io/genepi/story/65314/)
    
    ### Test plan
    created the tables.

--- src/py/covidr/database/modelmixins.py
+++ src/py/covidr/database/modelmixins.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer
 
 # a collection of mixin classes to help build the modelss
 
@@ -8,13 +8,3 @@ class BaseMixin:
     """Base model: all models have integer primary keys"""
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-
-
-# for things that have names
-class NameMixin:
-    """Mixin that adds a required unique name string field"""
-
-    name = Column(String, unique=True, nullable=False)
-
-    def __repr__(self):
-        return self.name
