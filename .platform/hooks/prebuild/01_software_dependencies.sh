03ad692ede23a3592237d59ed2c459e06e070782
Lucia Reynoso <lreynoso@chanzuckerberg.com>
2021-01-19 15:56:24 -0800

    Bump Node version to latest LTS (14.15.4) (#16)

--- .platform/hooks/prebuild/01_software_dependencies.sh
+++ .platform/hooks/prebuild/01_software_dependencies.sh
@@ -2,17 +2,25 @@
 set -x
 
 export NVM_DIR="${HOME}/.nvm"
-export NODE_VERSION="12.16.1"
+# We want to be on the latest Active LTS; but should
+# set this value manually, so we can test it beforehand
+export NODE_VERSION="14.15.4"
 
-# Install Node 12.16.1
+# Install nvm
 if [[ ! -f "${NVM_DIR}/nvm.sh" ]]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 fi
 
+# Activate nvm
 source "${NVM_DIR}/nvm.sh"
 
+# Install our preferred Node version
+# Make sure we are using that version, and also
+# make our version the default
 if [[ $(nvm current) != "v${NODE_VERSION}" ]]; then
     nvm install ${NODE_VERSION}
+    nvm alias default ${NODE_VERSION}
+    nvm use ${NODE_VERSION}
 fi
 
 # Install Frontend dependencies
