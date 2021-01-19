74b245aba611ce34ff6b3a1dd507798186cacdab
Tony Tung <ttung@chanzuckerberg.com>
2021-01-19 13:08:10 -0800

    Port some test infra from covidhub
    ### Description
    1. Database testing fixtures.
    2. Alembic single head test.
    3. Added test requirements.
    
    ### Test plan
    github actions!

---
+++ src/py/covidr/conftest.py
@@ -0,0 +1,8 @@
+from covidr.test_infra.fixtures import (  # noqa: F401
+    postgres_database,
+    postgres_database_with_schema,
+    postgres_instance,
+    session,
+    sqlalchemy_interface,
+    unused_tcp_port,
+)
