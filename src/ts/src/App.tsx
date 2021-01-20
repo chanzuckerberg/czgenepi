699f6be8233076326627da7c5862dcd0730dd764
lvreynoso <lvreynoso@protonmail.com>
2021-01-20 11:09:00 -0800

    Add prettified and linted files

--- src/ts/src/App.tsx
+++ src/ts/src/App.tsx
@@ -3,25 +3,26 @@ import logo from "./logo.svg";
 import "./App.scss";
 
 function App(): JSX.Element {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is COVIDr on Flask, saying<br/>
-          <code>Hello, world!</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    This is COVIDr on Flask, saying
+                    <br />
+                    <code>Hello, world!</code>
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
