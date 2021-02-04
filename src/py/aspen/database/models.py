37809a0a7a0c767c4ea970d67c6877ebeae942da
Tony Tung <ttung@chanzuckerberg.com>
2021-02-04 10:49:06 -0800

    Models for the physical sample (#43)
    ### Description
    It's still missing the fields from PHA4GE but I think it's easier to throw them in after people agree to the general concepts.
    
    #### Issue
    [ch64743](https://app.clubhouse.io/genepi/stories/space/64743)
    
    ### Test plan
    Ran migration.

--- src/py/aspen/database/models.py
+++ src/py/aspen/database/models.py
@@ -1,5 +1,15 @@
 # the main models in the database
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, MetaData, String
+from sqlalchemy import (
+    Boolean,
+    Column,
+    DateTime,
+    ForeignKey,
+    Integer,
+    JSON,
+    MetaData,
+    String,
+    UniqueConstraint,
+)
 from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
 from sqlalchemy.orm import backref, relationship
 
@@ -53,3 +63,84 @@ class User(base):
 
     def __repr__(self):
         return f"User <{self.name}>"
+
+
+########################################################################################
+# physical sample
+
+
+class PhysicalSample(base):
+    """A physical sample.  Multiple sequences can be taken of each physical sample."""
+
+    __tablename__ = "physical_samples"
+    __table_args__ = (UniqueConstraint("submitting_group_id", "private_identifier"),)
+
+    submitting_group_id = Column(
+        Integer,
+        ForeignKey(f"{Group.__tablename__}.id"),
+        nullable=False,
+    )
+    submitting_group = relationship(
+        Group, backref=backref("physical_samples", uselist=True)
+    )
+    private_identifier = Column(
+        String,
+        nullable=False,
+        comment=(
+            "This is the private identifier groups (DPHs) will use to map data back to "
+            "their internal databases."
+        ),
+    )
+    original_submission = Column(
+        JSON,
+        nullable=False,
+        comment="This is the original metadata submitted by the user.",
+    )
+
+    public_identifier = Column(
+        String,
+        nullable=False,
+        unique=True,
+        comment="This is the public identifier we assign to this sample.",
+    )
+
+    collection_date = Column(
+        DateTime,
+        nullable=False,
+        info={
+            "schema_mappings": {
+                "PHA4GE": "sample_collection_date",
+            }
+        },
+    )
+
+    # location
+    location = Column(String, nullable=False)
+    division = Column(
+        String,
+        nullable=False,
+        info={
+            "schema_mappings": {
+                "PHA4GE": "geo_loc_name_state_province_region",
+            }
+        },
+    )
+    country = Column(
+        String,
+        nullable=False,
+        info={
+            "schema_mappings": {
+                "PHA4GE": "geo_loc_name_country",
+            }
+        },
+    )
+
+    # TODO: (ttung) (sidneymbell) leaving out a lot of fields for now.
+    purpose_of_sampling = Column(
+        String,
+        info={
+            "schema_mappings": {
+                "PHA4GE": "purpose_of_sampling",
+            }
+        },
+    )
