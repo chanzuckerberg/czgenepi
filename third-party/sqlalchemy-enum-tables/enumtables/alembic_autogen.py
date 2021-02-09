fe145e2e2a750aa5aed51e54c825cdfa68cf1cf0
Tony Tung <ttung@chanzuckerberg.com>
2021-02-09 09:25:45 -0800

    Make enumtables schema aware (#47)
    ### Description
    This library was unaware of schemas and as such choked whenever it came across a schema.
    
    ### Test plan
    used in later PR.

--- third-party/sqlalchemy-enum-tables/enumtables/alembic_autogen.py
+++ third-party/sqlalchemy-enum-tables/enumtables/alembic_autogen.py
@@ -13,9 +13,9 @@ def get_declared_enums(metadatas, schema, default):
 		for column in table.columns if (isinstance(column.type, enum_column.EnumType) and table.schema == schema))
 	return {typ.__enum__.__tablename__ : frozenset(typ.__enum__.__enum__.__members__) for typ in types}
 
-def is_table_present(tablename, connection):
+def is_table_present(fully_qualified_table_name, connection):
 	try:
-		connection.execute("SELECT * FROM {} LIMIT 0;".format(tablename))
+		connection.execute("SELECT * FROM {} LIMIT 0;".format(fully_qualified_table_name))
 	except:
 		return False
 	else:
@@ -36,15 +36,19 @@ def compare_enums(autogen_context, upgrade_ops, schema_names):
 			metadatas = autogen_context.metadata
 		enums = get_declared_enums(metadatas, schema, default)
 		for table, values in enums.items():
-			if is_table_present(table, autogen_context.connection):
-				items = {r[0] for r in autogen_context.connection.execute("SELECT item_id FROM {}".format(table))}
+			fully_qualified_table_name = "%s.%s" % (schema, table) if schema else table
+			if is_table_present(fully_qualified_table_name, autogen_context.connection):
+				items = {
+					r[0]
+					for r in autogen_context.connection.execute(
+						"SELECT item_id FROM {}".format(fully_qualified_table_name)
+					)
+				}
 				to_add = values - items
 				to_remove = items - values
 				if to_add:
-					upgrade_ops.ops.append(alembic_ops.EnumInsertOp(table, list(to_add)))
+					upgrade_ops.ops.append(alembic_ops.EnumInsertOp(schema, table, list(to_add)))
 				if to_remove:
-					upgrade_ops.ops.append(alembic_ops.EnumDeleteOp(table, list(to_remove)))
+					upgrade_ops.ops.append(alembic_ops.EnumDeleteOp(schema, table, list(to_remove)))
 			else:
-				upgrade_ops.ops.append(alembic_ops.EnumInsertOp(table, list(values)))
-
-
+				upgrade_ops.ops.append(alembic_ops.EnumInsertOp(schema, table, list(values)))
