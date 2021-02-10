380d8d4066e5a60cebd0738e69baece1d5f651bf
Tony Tung <ttung@chanzuckerberg.com>
2021-02-10 13:33:41 -0800

    Models for gisaid dumps
    ### Description
    From [schema.ddl](https://github.com/chanzuckerberg/aspen/blob/cffddbd3836cc0c766d2a725947f2c84d63f1c44/docs/backend/schema.ddl)
    
    #### Issue
    [ch64743](https://app.clubhouse.io/genepi/stories/space/64743)
    
    ### Test plan
    ran migration, run tests.

--- src/py/aspen/database/models/__init__.py
+++ src/py/aspen/database/models/__init__.py
@@ -2,6 +2,11 @@ from .accessions import Accession, PublicRepository  # noqa: F401
 from .base import meta  # noqa: F401
 from .cansee import CanSee, DataType  # noqa: F401
 from .entity import Entity, EntityType  # noqa: F401
+from .gisaid_dump import (  # noqa: F401
+    GisaidDumpWorkflow,
+    ProcessedGisaidDump,
+    RawGisaidDump,
+)
 from .physical_sample import PhysicalSample  # noqa: F401
 from .usergroup import Group, User  # noqa: F401
 from .workflow import Workflow, WorkflowType  # noqa: F401
