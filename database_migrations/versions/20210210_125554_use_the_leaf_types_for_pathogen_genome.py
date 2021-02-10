ef23f6eed9d2480967d3ade43a4a06bb91750398
Tony Tung <ttung@chanzuckerberg.com>
2021-02-10 13:53:57 -0800

    Add the leaf types for *PathogenGenome
    ### Description
    SqlAlchemy [does not support multi-level inheritance](https://docs.sqlalchemy.org/en/14/orm/inheritance.html#joined-table-inheritance) so we have to specify all the leaf types at the top level.
    
    ### Test plan
    used in subsequent PR.

---
+++ database_migrations/versions/20210210_125554_use_the_leaf_types_for_pathogen_genome.py
@@ -0,0 +1,33 @@
+"""use the leaf types for pathogen_genome
+
+Revision ID: 20210210_125554
+Revises: 20210210_132846
+Create Date: 2021-02-10 12:55:54.816436
+
+"""
+import enumtables  # noqa: F401
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "20210210_125554"
+down_revision = "20210210_132846"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.enum_insert(
+        "entity_types",
+        ["CALLED_PATHOGEN_GENOME", "UPLOADED_PATHOGEN_GENOME"],
+        schema="aspen",
+    )
+    op.enum_delete("entity_types", ["PATHOGEN_GENOME"], schema="aspen")
+
+
+def downgrade():
+    op.enum_insert("['PATHOGEN_GENOME']", [], schema="entity_types")
+    op.enum_delete(
+        "entity_types",
+        ["CALLED_PATHOGEN_GENOME", "UPLOADED_PATHOGEN_GENOME"],
+        schema="aspen",
+    )
