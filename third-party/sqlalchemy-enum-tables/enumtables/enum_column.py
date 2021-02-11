17393c460669c48468117918b7b0fc41e48afc18
Tony Tung <ttung@chanzuckerberg.com>
2021-02-11 11:38:55 -0800

    Use the enum value instead of the enum name (#75)
    ### Description
    Using the enum value makes it easier to read the db contents.
    
    ### Test plan
    Used in subsequent PR.

--- third-party/sqlalchemy-enum-tables/enumtables/enum_column.py
+++ third-party/sqlalchemy-enum-tables/enumtables/enum_column.py
@@ -16,8 +16,8 @@ class EnumType(types.TypeDecorator):
 	def process_bind_param(self, value, dialect):
 		if value is None:
 			return None
-		return value.name
+		return value._value_
 	def process_result_value(self, value, dialect):
 		if value is None:
 			return None
-		return self.__enum__[value]
+		return self.__enum__(value)
