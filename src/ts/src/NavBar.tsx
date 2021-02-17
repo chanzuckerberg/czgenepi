f91d5d7a06bf0a077f1fc887504d1dca43b5e1e1
lvreynoso <lvreynoso@protonmail.com>
2021-02-17 10:57:22 -0800

    Rework theme structure and build new NavBar component

--- src/ts/src/NavBar.tsx
+++ src/ts/src/NavBar.tsx
@@ -1,33 +1,28 @@
-import React from "react";
-import { NavBar } from "cz-ui";
-import { Link } from "react-router-dom";
+import React, { FunctionComponent } from "react";
+import { Menu, Container, Dropdown, Image, Divider } from "semantic-ui-react";
 
-import ErrorBoundary from "common/components/ErrorBoundary";
+import { ReactComponent as AspenLogo } from "common/styles/logos/AspenLogo.svg";
 
-import style from "NavBar.module.scss";
+import style from "./NavBar.module.scss";
 
-export default function NavBarAspen(): JSX.Element {
-    const navLinks = [
-        <Link key="home" to="/">
-            Overview
-        </Link>,
-        <Link key="upload" to="/upload">
-            Upload
-        </Link>,
-    ];
-
-    return (
-        <div className={style.navbar}>
-            <ErrorBoundary>
-                <NavBar
-                    title={
-                        <Link className={style.titleLink} to="/">
-                            Aspen?
-                        </Link>
-                    }
-                    navLinks={navLinks}
-                />
-            </ErrorBoundary>
-        </div>
-    );
+type Props = {
+    user: string
 }
+
+const NavBar: FunctionComponent<Props> = ({ user, children }) => {
+    return (
+        <Menu className={style.menu} fixed='top' floated inverted borderless>
+          <Container fluid>
+            <Menu.Item as='a' header>
+                <Image size='tiny' style={{ marginRight: '.5em' }}>
+                    <AspenLogo />
+                </Image>
+            </Menu.Item>
+            <div className={style.verticalLine}/>
+            <Menu.Item as='a'><span className={style.user}>{user}</span></Menu.Item>
+          </Container>
+        </Menu>
+    )
+}
+
+export default NavBar;
