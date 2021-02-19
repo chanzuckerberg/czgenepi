b68d2dcbfa3877931f5aaf92a230964ac4162c97
Tony Tung <ttung@chanzuckerberg.com>
2021-02-18 17:41:50 -0800

    Add more types of data that access can be granted to.
    ### Description
    This is originally from #62.
    
    ### Test plan
    YOLO

---
+++ database_migrations/versions/20210218_104830_add_private_identifiers_as_an_option_.py
@@ -0,0 +1,23 @@
+"""add private identifiers as an option for cansee datatypes
+
+Revision ID: 20210218_104830
+Revises: 20210216_114729
+Create Date: 2021-02-18 10:48:31.834354
+
+"""
+import enumtables  # noqa: F401
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "20210218_104830"
+down_revision = "20210216_114729"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.enum_insert("data_types", ["PRIVATE_IDENTIFIERS"], schema="aspen")
+
+
+def downgrade():
+    op.enum_delete("data_types", ["PRIVATE_IDENTIFIERS"], schema="aspen")
