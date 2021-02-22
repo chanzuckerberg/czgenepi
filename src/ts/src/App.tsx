ed9cbf96128be548d287023fca6620264840a1fb
Lucia Reynoso <lreynoso@chanzuckerberg.com>
2021-02-22 13:54:11 -0800

    Update stylesheets to follow design system (#93)
    * Update stylesheets
    
    * Separate out type changes
    
    * Remove yet to be used prop and update margin
    
    * Fix linting issues
    
    * Update test

--- src/ts/src/App.tsx
+++ src/ts/src/App.tsx
@@ -1,23 +1,26 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { CZUI } from "cz-ui";
 
 import Landing from "Landing";
 import NavBar from "NavBar";
 
-import "App.scss";
+import style from "App.module.scss";
 
-export default function App(): JSX.Element {
+const App: FunctionComponent<React.ReactNode> = () => {
     return (
         <Router>
-            <div className="App">
-                <CZUI>
+            <div className={style.app}>
+                <div className={style.navBar}>
                     <NavBar />
+                </div>
+                <div className={style.view}>
                     <Switch>
                         <Route path="/" render={() => <Landing />} />
                     </Switch>
-                </CZUI>
+                </div>
             </div>
         </Router>
     );
-}
+};
+
+export default App;
