47168e3d9191dca70f1b29d754458c4f28cb5067
lvreynoso <lvreynoso@protonmail.com>
2021-02-22 15:26:29 -0800

    Fix linting

--- src/ts/src/NavBar.tsx
+++ src/ts/src/NavBar.tsx
@@ -13,92 +13,97 @@ type Props = {
 };
 
 const LINKS: Array<Record<string, string>> = [
-    { to: "/", text: "Overview"},
-    { to: "/data", text: "Data"}
+    { to: "/", text: "Overview" },
+    { to: "/data", text: "Data" },
 ];
 
 const DROPDOWN_LINKS: Array<Record<string, string>> = [
     { to: "/contact_us", text: "Contact Us" },
     { to: "/terms", text: "Terms of Use" },
     { to: "/privacy", text: "Privacy Policy" },
-    { to: "/logout", text: "Logout" }
-]
+    { to: "/logout", text: "Logout" },
+];
 
-const NavBar: FunctionComponent<Props> = ({ org, user }) => {
-
-    const navigationLinks = LINKS.map(link => {
+const NavBar: FunctionComponent<Props> = ({ org, user }: Props) => {
+    const navigationLinks = LINKS.map((link) => {
         return (
             <Link to={link.to} key={link.text}>
                 <div className={cx(style.item, style.link)}>{link.text}</div>
             </Link>
-        )
-    })
+        );
+    });
 
-    const dropdownLinks = DROPDOWN_LINKS.map(link => {
+    const dropdownLinks = DROPDOWN_LINKS.map((link) => {
         return (
             <Link to={link.to} key={link.text}>
-                <Dropdown.Item className={style.dropdownLink} text={link.text} />
+                <Dropdown.Item
+                    className={style.dropdownLink}
+                    text={link.text}
+                />
             </Link>
-        )
-    })
+        );
+    });
 
-    const orgElements = <React.Fragment>
-                            <div className={style.seperator} />
-                            <div className={cx(style.item, style.org)}>
-                                {org}
-                            </div>
-                        </React.Fragment>
+    const orgElements = (
+        <React.Fragment>
+            <div className={style.seperator} />
+            <div className={cx(style.item, style.org)}>{org}</div>
+        </React.Fragment>
+    );
 
     function hasOrg(): JSX.Element | null {
         if (org === undefined) {
-            return null
+            return null;
         } else {
-            return orgElements
+            return orgElements;
         }
     }
 
-    const orgSplash = hasOrg()
+    const orgSplash = hasOrg();
 
-    const userMenu =    <div className={cx(style.item, style.user)}>
-                            <Dropdown text={user} floating direction={"left"}>
-                                <Dropdown.Menu className={style.dropdownMenu}>
-                                    {dropdownLinks}
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </div>
+    const userMenu = (
+        <div className={cx(style.item, style.user)}>
+            <Dropdown text={user} floating direction={"left"}>
+                <Dropdown.Menu className={style.dropdownMenu}>
+                    {dropdownLinks}
+                </Dropdown.Menu>
+            </Dropdown>
+        </div>
+    );
 
-    const signInLink =  <Link to="/login">
-                            <div className={cx(style.item, style.link)}>Sign In</div>
-                        </Link>
+    const signInLink = (
+        <Link to="/login">
+            <div className={cx(style.item, style.link)}>Sign In</div>
+        </Link>
+    );
 
     function isLoggedIn(): JSX.Element {
         if (user === undefined) {
-            return signInLink
+            return signInLink;
         } else {
-            return userMenu
+            return userMenu;
         }
     }
 
-    const rightEdge = isLoggedIn()
+    const rightEdge = isLoggedIn();
 
     return (
         <div className={style.bar}>
             <div className={style.contentArea}>
                 <div className={style.left}>
                     <div className={cx(style.item, style.logo)}>
-                        <AspenLogo height={"60%"}/>
+                        <AspenLogo height={"60%"} />
                     </div>
                     {orgSplash}
                 </div>
-                <div className={style.center}>
-                </div>
+                <div className={style.center}></div>
                 <div className={style.right}>
                     {navigationLinks}
                     {rightEdge}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default NavBar;
