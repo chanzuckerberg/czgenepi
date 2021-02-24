07f299be94935417b5a66f6baf5e9b9f0437e3e9
Tony Tung <ttung@chanzuckerberg.com>
2021-02-24 10:50:26 -0800

    Accessors to go between SequencingRead <-> HostFilteredSequencingRead
    ### Description
    This makes it easier to walk between objects.
    
    #### Issue
    [ch64743](https://app.clubhouse.io/genepi/stories/space/64743)
    
    ### Test plan
    None.

--- src/py/aspen/database/models/host_filtering.py
+++ src/py/aspen/database/models/host_filtering.py
@@ -1,7 +1,14 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from sqlalchemy import Column, ForeignKey, Integer, String, UniqueConstraint
 
 from .entity import Entity, EntityType
 from .workflow import Workflow, WorkflowType
+
+if TYPE_CHECKING:
+    from .sequences import SequencingReadCollection
 
 
 class HostFilteredSequencingReadCollection(Entity):
@@ -13,6 +20,16 @@ class HostFilteredSequencingReadCollection(Entity):
 
     s3_bucket = Column(String, nullable=False)
     s3_key = Column(String, nullable=False)
+
+    @property
+    def sequencing_read(self) -> SequencingReadCollection:
+        """The raw gisaid dump this processed gisaid dump was generated from."""
+        # this import has to be here for circular dependencies reasons. :(
+        from .sequences import SequencingReadCollection
+
+        parents = self.get_parents(SequencingReadCollection)
+        assert len(parents) == 1
+        return parents[0]
 
 
 class FilterRead(Workflow):
