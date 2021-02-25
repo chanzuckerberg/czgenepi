42d9afc5f4f4c0281b1fee97379ebc85bc771646
phoenixAja <phoenix@phoenixlogan.net>
2021-02-25 08:41:36 -0800

    changed name of root view

--- src/py/aspen/main.py
+++ src/py/aspen/main.py
@@ -2,6 +2,6 @@
 
 from aspen.app.app import application  # noqa: F401
 from aspen.app.views.auth import callback_handling, login, logout  # noqa: F401
-from aspen.app.views.base import serve  # noqa: F401
+from aspen.app.views.index import serve  # noqa: F401
 from aspen.app.views.sample import samples  # noqa: F401
 from aspen.app.views.usergroup import usergroup  # noqa: F401
