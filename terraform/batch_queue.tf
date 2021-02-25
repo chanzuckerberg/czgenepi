2bbaa2a31e3a294e332a9bfb96994339519589b3
Andrey Kislyuk <kislyuk@gmail.com>
2021-02-25 14:21:32 -0800

    WIP

--- terraform/batch_queue.tf
+++ terraform/batch_queue.tf
@@ -3,113 +3,113 @@ locals {
   launch_template_user_data_hash = filemd5(local.launch_template_user_data_file)
 }
 
-data "aws_ssm_parameter" "mccloud_batch_ami" {
+data "aws_ssm_parameter" "aspen_batch_ami" {
   name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
 }
 
-resource "aws_iam_role" "mccloud_batch_service_role" {
-  name = "mccloud-${var.DEPLOYMENT_ENVIRONMENT}-batch-service"
+resource "aws_iam_role" "aspen_batch_service_role" {
+  name = "aspen-${var.DEPLOYMENT_ENVIRONMENT}-batch-service"
   assume_role_policy = templatefile("${path.module}/iam_policy_templates/trust_policy.json", {
     trust_services = ["batch"]
   })
   tags = local.common_tags
 }
 
-resource "aws_iam_role_policy_attachment" "mccloud_batch_service_role" {
-  role       = aws_iam_role.mccloud_batch_service_role.name
+resource "aws_iam_role_policy_attachment" "aspen_batch_service_role" {
+  role       = aws_iam_role.aspen_batch_service_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
 }
 
-resource "aws_iam_role" "mccloud_batch_spot_fleet_service_role" {
-  name = "mccloud-${var.DEPLOYMENT_ENVIRONMENT}-batch-spot-fleet-service"
+resource "aws_iam_role" "aspen_batch_spot_fleet_service_role" {
+  name = "aspen-${var.DEPLOYMENT_ENVIRONMENT}-batch-spot-fleet-service"
   assume_role_policy = templatefile("${path.module}/iam_policy_templates/trust_policy.json", {
     trust_services = ["spotfleet"]
   })
   tags = local.common_tags
 }
 
-resource "aws_iam_role_policy_attachment" "mccloud_batch_spot_fleet_service_role" {
-  role       = aws_iam_role.mccloud_batch_spot_fleet_service_role.name
+resource "aws_iam_role_policy_attachment" "aspen_batch_spot_fleet_service_role" {
+  role       = aws_iam_role.aspen_batch_spot_fleet_service_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2SpotFleetTaggingRole"
 }
 
-resource "aws_iam_role" "mccloud_batch_main_instance_role" {
-  name = "mccloud-${var.DEPLOYMENT_ENVIRONMENT}-batch-main-instance"
+resource "aws_iam_role" "aspen_batch_main_instance_role" {
+  name = "aspen-${var.DEPLOYMENT_ENVIRONMENT}-batch-main-instance"
   assume_role_policy = templatefile("${path.module}/iam_policy_templates/trust_policy.json", {
     trust_services = ["ec2"]
   })
   tags = local.common_tags
 }
 
-resource "aws_iam_role_policy_attachment" "mccloud_batch_main_instance_role_put_metric" {
-  role       = aws_iam_role.mccloud_batch_main_instance_role.name
+resource "aws_iam_role_policy_attachment" "aspen_batch_main_instance_role_put_metric" {
+  role       = aws_iam_role.aspen_batch_main_instance_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "mccloud_batch_main_instance_role_ecs" {
-  role       = aws_iam_role.mccloud_batch_main_instance_role.name
+resource "aws_iam_role_policy_attachment" "aspen_batch_main_instance_role_ecs" {
+  role       = aws_iam_role.aspen_batch_main_instance_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
-resource "aws_iam_role_policy_attachment" "mccloud_batch_main_instance_role_ssm" {
-  role       = aws_iam_role.mccloud_batch_main_instance_role.name
+resource "aws_iam_role_policy_attachment" "aspen_batch_main_instance_role_ssm" {
+  role       = aws_iam_role.aspen_batch_main_instance_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-resource "aws_iam_instance_profile" "mccloud_batch_main" {
-  name = "mccloud-${var.DEPLOYMENT_ENVIRONMENT}-batch-main"
-  role = aws_iam_role.mccloud_batch_main_instance_role.name
+resource "aws_iam_instance_profile" "aspen_batch_main" {
+  name = "aspen-${var.DEPLOYMENT_ENVIRONMENT}-batch-main"
+  role = aws_iam_role.aspen_batch_main_instance_role.name
 }
 
-resource "aws_launch_template" "mccloud_batch_main" {
+resource "aws_launch_template" "aspen_batch_main" {
   # AWS Batch pins a specific version of the launch template when a compute environment is created.
   # The CE does not support updating this version, and needs replacing (redeploying) if launch template contents change.
   # The launch template resource increments its version when contents change, but the compute environment resource does
   # not recognize this change. We bind the launch template name to user data contents here, so any changes to user data
   # will cause the whole launch template to be replaced, forcing the compute environment to pick up the changes.
-  name      = "mccloud-${var.DEPLOYMENT_ENVIRONMENT}-batch-main-${local.launch_template_user_data_hash}"
+  name      = "aspen-${var.DEPLOYMENT_ENVIRONMENT}-batch-main-${local.launch_template_user_data_hash}"
   user_data = filebase64(local.launch_template_user_data_file)
   tags      = local.common_tags
 }
 
-resource "aws_key_pair" "mccloud_batch" {
-  key_name   = "mccloud-${var.DEPLOYMENT_ENVIRONMENT}"
-  public_key = var.SSH_PUBLIC_KEY
+resource "aws_key_pair" "aspen_batch" {
+  key_name   = "aspen-${var.DEPLOYMENT_ENVIRONMENT}"
+  # public_key = var.SSH_PUBLIC_KEY
 }
 
 # See https://github.com/hashicorp/terraform-provider-aws/pull/16819 for Batch Fargate CE support
-resource "aws_batch_compute_environment" "mccloud_ec2_spot" {
-  compute_environment_name_prefix = "mccloud-${var.DEPLOYMENT_ENVIRONMENT}-ec2-spot-"
+resource "aws_batch_compute_environment" "aspen_ec2_spot" {
+  compute_environment_name_prefix = "aspen-${var.DEPLOYMENT_ENVIRONMENT}-ec2-spot-"
 
   compute_resources {
-    instance_role = aws_iam_instance_profile.mccloud_batch_main.arn
+    instance_role = aws_iam_instance_profile.aspen_batch_main.arn
     instance_type = ["r5d"]
-    image_id      = data.aws_ssm_parameter.mccloud_batch_ami.value
-    ec2_key_pair  = aws_key_pair.mccloud_batch.id
+    image_id      = data.aws_ssm_parameter.aspen_batch_ami.value
+    ec2_key_pair  = aws_key_pair.aspen_batch.id
     min_vcpus     = 0
     desired_vcpus = 16
     max_vcpus     = 256
     security_group_ids = [
-      aws_security_group.mccloud.id,
+      aws_security_group.aspen.id,
     ]
-    subnets             = [for subnet in aws_subnet.mccloud : subnet.id]
+    subnets             = [for subnet in aws_subnet.aspen : subnet.id]
     type                = "SPOT"
     allocation_strategy = "BEST_FIT"
     bid_percentage      = 100
-    spot_iam_fleet_role = aws_iam_role.mccloud_batch_spot_fleet_service_role.arn
+    spot_iam_fleet_role = aws_iam_role.aspen_batch_spot_fleet_service_role.arn
     tags = merge(local.common_tags, {
-      Name = "mccloud-${var.DEPLOYMENT_ENVIRONMENT}-batch-ec2-spot"
+      Name = "aspen-${var.DEPLOYMENT_ENVIRONMENT}-batch-ec2-spot"
     })
 
     launch_template {
-      launch_template_name = aws_launch_template.mccloud_batch_main.name
+      launch_template_name = aws_launch_template.aspen_batch_main.name
     }
   }
 
-  service_role = aws_iam_role.mccloud_batch_service_role.arn
+  service_role = aws_iam_role.aspen_batch_service_role.arn
   type         = "MANAGED"
   depends_on = [
-    aws_iam_role_policy_attachment.mccloud_batch_service_role
+    aws_iam_role_policy_attachment.aspen_batch_service_role
   ]
 
   lifecycle {
@@ -120,11 +120,11 @@ resource "aws_batch_compute_environment" "mccloud_ec2_spot" {
   }
 }
 
-resource "aws_batch_job_queue" "mccloud_main" {
-  name     = "mccloud-${var.DEPLOYMENT_ENVIRONMENT}"
+resource "aws_batch_job_queue" "aspen_main" {
+  name     = "aspen-${var.DEPLOYMENT_ENVIRONMENT}"
   state    = "ENABLED"
   priority = 10
   compute_environments = [
-    aws_batch_compute_environment.mccloud_ec2_spot.arn,
+    aws_batch_compute_environment.aspen_ec2_spot.arn,
   ]
 }
--- terraform/batch_vpc.tf
+++ terraform/batch_vpc.tf
@@ -2,41 +2,41 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-resource "aws_vpc" "mccloud" {
+resource "aws_vpc" "aspen" {
   cidr_block           = "10.20.0.0/16"
   enable_dns_hostnames = true
   tags = merge(local.common_tags, {
-    Name = "mccloud-${var.DEPLOYMENT_ENVIRONMENT}"
+    Name = "aspen-${var.DEPLOYMENT_ENVIRONMENT}"
   })
 }
 
-resource "aws_internet_gateway" "mccloud" {
-  vpc_id = aws_vpc.mccloud.id
+resource "aws_internet_gateway" "aspen" {
+  vpc_id = aws_vpc.aspen.id
   tags = merge(local.common_tags, {
-    Name = "mccloud-${var.DEPLOYMENT_ENVIRONMENT}"
+    Name = "aspen-${var.DEPLOYMENT_ENVIRONMENT}"
   })
 }
 
-resource "aws_route" "mccloud" {
-  route_table_id         = aws_vpc.mccloud.default_route_table_id
+resource "aws_route" "aspen" {
+  route_table_id         = aws_vpc.aspen.default_route_table_id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.mccloud.id
+  gateway_id             = aws_internet_gateway.aspen.id
 }
 
-resource "aws_subnet" "mccloud" {
+resource "aws_subnet" "aspen" {
   for_each                = toset(data.aws_availability_zones.available.names)
-  vpc_id                  = aws_vpc.mccloud.id
+  vpc_id                  = aws_vpc.aspen.id
   availability_zone       = each.key
-  cidr_block              = cidrsubnet(aws_vpc.mccloud.cidr_block, 8, index(data.aws_availability_zones.available.names, each.key))
+  cidr_block              = cidrsubnet(aws_vpc.aspen.cidr_block, 8, index(data.aws_availability_zones.available.names, each.key))
   map_public_ip_on_launch = true
   tags = merge(local.common_tags, {
-    Name = "mccloud-${var.DEPLOYMENT_ENVIRONMENT}"
+    Name = "aspen-${var.DEPLOYMENT_ENVIRONMENT}"
   })
 }
 
-resource "aws_security_group" "mccloud" {
-  name   = "mccloud-${var.DEPLOYMENT_ENVIRONMENT}"
-  vpc_id = aws_vpc.mccloud.id
+resource "aws_security_group" "aspen" {
+  name   = "aspen-${var.DEPLOYMENT_ENVIRONMENT}"
+  vpc_id = aws_vpc.aspen.id
   egress {
     from_port   = 0
     to_port     = 0
