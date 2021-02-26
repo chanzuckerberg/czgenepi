6f2c9b0beca2249be4c0108fe22caaa3b4150b31
Tony Tung <ttung@chanzuckerberg.com>
2021-02-26 09:37:23 -0800

    Merge alembic migration paths
    ### Description
    Whoops, we got a branch.
    
    ### Test plan
    upgraded to head

---
+++ database_migrations/versions/20210226_092430_merge_heads.py
@@ -0,0 +1,20 @@
+"""merge heads
+
+Create Date: 2021-02-26 09:24:30.678567
+
+"""
+import enumtables  # noqa: F401
+
+# revision identifiers, used by Alembic.
+revision = "20210226_092430"
+down_revision = ("20210225_221125", "20210225_180155")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
