e286d8fbdee90ff131b1bb3f7a836d4a7b48b37a
lvreynoso <lvreynoso@protonmail.com>
2021-03-01 12:14:46 -0800

    Touch up jsonToType<T> function

--- src/ts/src/common/utils/typeUtils.tsx
+++ src/ts/src/common/utils/typeUtils.tsx
@@ -5,14 +5,23 @@ export function get<T, K extends keyof T>(o: T, propertyName: K): T[K] {
     return o[propertyName]; // o[propertyName] is of type T[K]
 }
 
+
+// Take in a whole or subset of an API response that corresponds
+// to a defined type in the frontend, along with a mapping of
+// differing keys (e.g. if we're going from snake_case to camelCase)
+// and converts the data to an object of that type with the right keys.
+// Any keys not explicitly set in the mapping will be transferred to the
+// new object.
+type JSONPrimitive = string | number | boolean
+
 export function jsonToType<T>(
-    inputObject: Record<string, any>,
+    inputObject: Record<string, JSONPrimitive>,
     keyMap: Map<string, keyof T>
 ): T {
-    const entries: Array<Array<string | number | boolean>> = [];
+    const entries: Array<Array<JSONPrimitive>> = [];
     Object.keys(inputObject).forEach((key) => {
         if (keyMap.has(key)) {
-            entries.push([keyMap.get(key), inputObject[key]]);
+            entries.push([keyMap.get(key) as string, inputObject[key]]);
         } else {
             entries.push([key, inputObject[key]]);
         }
