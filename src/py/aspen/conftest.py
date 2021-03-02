dc050923c047e91d8ee64a8e6c01cbd1e65aceaa
Tony Tung <ttung@chanzuckerberg.com>
2021-03-02 11:41:13 -0800

    Convert the model fixtures to model factories (#156)
    ### Description
    We need factories to support tests that create multiple models.
    
    ### Test plan
    pytest

--- src/py/aspen/conftest.py
+++ src/py/aspen/conftest.py
@@ -1,7 +1,7 @@
 from aspen.test_infra.flask import app, client  # noqa: F401
-from aspen.test_infra.models.sample import sample  # noqa: F401
-from aspen.test_infra.models.sequences import sequencing_read  # noqa: F401
-from aspen.test_infra.models.usergroup import group, user  # noqa: F401
+from aspen.test_infra.models.sample import sample_factory  # noqa: F401
+from aspen.test_infra.models.sequences import sequencing_read_factory  # noqa: F401
+from aspen.test_infra.models.usergroup import group_factory, user_factory  # noqa: F401
 from aspen.test_infra.postgres import (  # noqa: F401
     postgres_database,
     postgres_database_with_schema,
