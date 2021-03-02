fbf7491cb93bdc0cc44ce7d48c6c6dd1f0e7b871
lvreynoso <lvreynoso@protonmail.com>
2021-03-02 15:38:13 -0800

    Add comments, fix linting

--- src/ts/src/common/api/index.tsx
+++ src/ts/src/common/api/index.tsx
@@ -3,34 +3,50 @@ import axios from "axios";
 
 import { jsonToType } from "common/utils";
 
-async function apiSingleResponse<T>(mapping: Map<string, keyof T>, endpoint: string): Promise<T> {
+/** Generic functions to interface with the backend API **/
+/* eslint-disable @typescript-eslint/no-explicit-any */
+async function apiSingleResponse<T>(
+    mapping: Map<string, keyof T>,
+    endpoint: string
+): Promise<T> {
     const response = await axios.get(endpoint);
-    const singleType = jsonToType<T>(response.data, mapping)
+    const singleType = jsonToType<T>(response.data, mapping);
     return singleType;
 }
-
-async function apiSplitResponse<T extends any[]>(mappings: Array<Map<string, any>>, endpoint: string): Promise<T> {
+async function apiSplitResponse<T extends any[]>(
+    mappings: Array<Map<string, any>>,
+    endpoint: string
+): Promise<T> {
     const response = await axios.get(endpoint);
-    const splitTypeArray = Object.keys(response.data).map(
-        (key: string, index: number) => jsonToType(response.data[key], mappings[index])
-    ) as T // this array maps to the tuple type T (e.g. [K, U])
-    return splitTypeArray
+    const splitTypeArray = Object.keys(
+        response.data
+    ).map((key: string, index: number) =>
+        jsonToType(response.data[key], mappings[index])
+    ) as T; // this array maps to the tuple type T (e.g. [K, U])
+    return splitTypeArray;
 }
 
-async function apiCollectionResponse<T>(mapping: Map<string, keyof T>, endpoint: string): Promise<Array<T>> {
+async function apiCollectionResponse<T>(
+    mapping: Map<string, keyof T>,
+    endpoint: string
+): Promise<Array<T>> {
     const response = await axios.get(endpoint);
     const collectionType: Array<T> = response.data.map(
         (entry: Record<string, JSONPrimitive>) => jsonToType<T>(entry, mapping)
     );
     return collectionType;
 }
+/* eslint-enable @typescript-eslint/no-explicit-any */
+
+/** Calls to specific API endpoints **/
 
 const USER_MAP = new Map<string, keyof User>([
     ["auth0_user_id", "auth0UserId"],
     ["group_admin", "groupAdmin"],
     ["system_admin", "systemAdmin"],
 ]);
-export const fetchUserData = async () => apiSplitResponse<[Group, User]>([new Map([]), USER_MAP], "/api/usergroup")
+export const fetchUserData = async (): [Group, User] =>
+    apiSplitResponse<[Group, User]>([new Map([]), USER_MAP], "/api/usergroup");
 
 const SAMPLE_MAP = new Map<string, keyof Sample>([
     ["collection_date", "collectionDate"],
@@ -39,11 +55,13 @@ const SAMPLE_MAP = new Map<string, keyof Sample>([
     ["public_identifier", "publicId"],
     ["upload_date", "uploadDate"],
 ]);
-export const fetchSamples = async () => apiCollectionResponse<Sample>(SAMPLE_MAP, "/api/samples")
+export const fetchSamples = async (): Sample[] =>
+    apiCollectionResponse<Sample>(SAMPLE_MAP, "/api/samples");
 
 const TREE_MAP = new Map<string, keyof Tree>([
     ["phylo_tree_id", "id"],
     ["pathogen_genome_count", "pathogenGenomeCount"],
     ["completed_date", "dateCompleted"],
 ]);
-export const fetchTrees = async () => apiCollectionResponse<Tree>(TREE_MAP, "/api/phylo_trees")
+export const fetchTrees = async (): Tree[] =>
+    apiCollectionResponse<Tree>(TREE_MAP, "/api/phylo_trees");
