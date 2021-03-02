5b70765f08a681950dc34741bfe710031ccfe71b
Tony Tung <ttung@chanzuckerberg.com>
2021-03-01 16:31:48 -0800

    Models to represent phylo trees and runs
    ### Description
    Self explanatory?
    
    #### Issue
    [ch65314](https://app.clubhouse.io/genepi/story/65314/standup-dummy-local-version-of-aspen-for-demo)
    
    ### Test plan
    Used in subsequent PR.

--- src/py/aspen/database/models/__init__.py
+++ src/py/aspen/database/models/__init__.py
@@ -15,6 +15,7 @@ from aspen.database.models.host_filtering import (  # noqa: F401
     FilterRead,
     HostFilteredSequencingReadsCollection,
 )
+from aspen.database.models.phylo_tree import PhyloRun, PhyloTree  # noqa: F401
 from aspen.database.models.sample import Sample  # noqa: F401
 from aspen.database.models.sequences import (  # noqa: F401
     CallConsensus,
