4f73bd0c0ccc82f751d273553e55f15c403ad3bb
Tony Tung <ttung@chanzuckerberg.com>
2021-03-10 21:19:59 -0800

    Support for a remote db config
    ### Description
    For the most part, accessing the DB is a function of AWS_PROFILE or AWS_ACCESS_KEY_ID/AWS_SECRET_KEY_ID.  We want to have an object that encompasses all the remote db config so we can access the DB from the CLI.
    
    If we didn't do this, then we would require the user to specify --staging or --prod (to initialize `StagingConfig` or `ProductionConfig`), when it actually doesn't matter.
    
    ### Test plan
    flask run

--- src/py/aspen/config/staging.py
+++ src/py/aspen/config/staging.py
@@ -1,5 +1,4 @@
 import logging
-import uuid
 
 from aspen import aws
 from aspen.config import config
@@ -7,14 +6,10 @@ from aspen.config import config
 logger = logging.getLogger(__name__)
 
 
-class StagingConfig(config.Config, descriptive_name="staging"):
+class StagingConfig(config.RemoteDatabaseConfig, descriptive_name="staging"):
     @config.flaskproperty
     def DEBUG(self) -> bool:
         return True
-
-    @config.flaskproperty
-    def SECRET_KEY(self) -> str:
-        return uuid.uuid4().hex
 
     @property
     def AUTH0_CALLBACK_URL(self) -> str:
@@ -23,7 +18,3 @@ class StagingConfig(config.Config, descriptive_name="staging"):
         return (
             f"http://aspen-{eb_env_name}.{aws.region()}.elasticbeanstalk.com/callback"
         )
-
-    @property
-    def DATABASE_URI(self) -> str:
-        return "postgresql://user_rw:password_rw@localhost:5432/aspen_db"
