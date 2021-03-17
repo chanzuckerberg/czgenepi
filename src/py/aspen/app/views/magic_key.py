25b31d7939e1ebff52be6e4d167c117003535364
lvreynoso <lvreynoso@protonmail.com>
2021-03-16 20:01:25 -0700

    Remove unused imports

--- src/py/aspen/app/views/magic_key.py
+++ src/py/aspen/app/views/magic_key.py
@@ -1,21 +1,15 @@
 import json
-from typing import Any, Iterable, Mapping, MutableSequence, Tuple
 from urllib.parse import urlparse, parse_qsl
 
 import boto3
 from flask import jsonify, session, request
-from sqlalchemy import func, or_
-from sqlalchemy.orm import joinedload
 
 from aspen.app.app import application, requires_auth
-from aspen.app.views.api_utils import format_datetime, get_usergroup_query
 from aspen.database.connection import session_scope
 from aspen.database.models import (
     PhyloRun,
     PhyloTree,
 )
-from aspen.database.models.usergroup import Group, User
-from aspen.error.recoverable import RecoverableError
 
 PRESIGNED_URL_KEY = "presigned_url"
 PRESIGNED_TYPES = { "phylo_tree": PhyloTree }
