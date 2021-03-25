e3d32bb7eb2b8d9b71164f8f6672cf7823f9d093
phoenixAja <phoenix@phoenixlogan.net>
2021-03-25 10:02:15 -0700

    tweak

--- src/py/aspen/test_infra/postgres.py
+++ src/py/aspen/test_infra/postgres.py
@@ -1,17 +1,11 @@
-import contextlib
-import socket
-import subprocess
-import time
 from dataclasses import dataclass
 from typing import Generator
-from uuid import uuid4
 import string
 import random
 
 import pytest
 
 from aspen.database import connection as aspen_connection
-from aspen.database import schema
 
 
 USERNAME = "user_rw"
@@ -20,7 +14,7 @@ INITIAL_DATABASE = "postgres"
 
 
 @dataclass
-class PostgresDatabase():
+class PostgresDatabase:
     database_name: str
     port: int
 
@@ -29,33 +23,28 @@ class PostgresDatabase():
 
 
 @pytest.fixture()
-def postgres_database():
-    """Starts a postgres instance with username/password cliadb/cliadb.  Returns a tuple
-    consisting of the container id, and the port the postgres instance can be reached
-    at."""
+def postgres_database() -> Generator[PostgresDatabase, None, None]:
+    """Creates a postgres test database named a random string with username/password user_rw/password_rw, yields it, and then drops it."""
+
+    # create admin sql connection to create test database
     admin_uri = "postgresql://postgres:password_postgres@database:5432/aspen_db"
     admin_sql_interface = aspen_connection.init_db(admin_uri)
-
     admin_session = admin_sql_interface.make_session()
+    # set isolation level to allow admin to create db
     admin_session.connection().connection.set_isolation_level(0)
+
+    # create random database name (for running tests in parallel)
     letters = string.ascii_lowercase
-    DATABASE_NAME = ( ''.join(random.choice(letters) for i in range(10)) )
+    database_name = (''.join(random.choice(letters) for i in range(10)))
 
-    admin_session.execute(f"create database {DATABASE_NAME}")
-    print(f"created db: {DATABASE_NAME}")
-    admin_session.execute(f"grant all privileges on database {DATABASE_NAME} to {USERNAME}")
+    admin_session.execute(f"create database {database_name}")
+    admin_session.execute(f"grant all privileges on database {database_name} to {USERNAME}")
 
-    postgres_test_db = PostgresDatabase(database_name=DATABASE_NAME, port=5432)
+    postgres_test_db = PostgresDatabase(
+        database_name=database_name,
+        port=5432
+    )
 
     yield postgres_test_db
 
-    admin_session.execute(f"drop database {DATABASE_NAME}")
-    print(f"cleaned {DATABASE_NAME}")
-
-
-
-@pytest.fixture()
-def postgres_database_with_schema(postgres_database) -> PostgresDatabase:
-    test_db = aspen_connection.init_db(postgres_database.as_uri())
-    schema.create_tables_and_schema(test_db)
-    yield test_db
+    admin_session.execute(f"drop database {database_name} with (force)")
