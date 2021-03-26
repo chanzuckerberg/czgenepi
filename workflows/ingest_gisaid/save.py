5d9f015f1db9a015528a669985db45cf199ac8d6
Tony Tung <ttung@chanzuckerberg.com>
2021-03-26 11:47:25 -0700

    Ingest and transform workflows for gisaid data
    ### Description
    This encompasses the workflows for both the ingest and transform workflows.
    
    Some interesting notes:
    1. I did not add an object to the db indicating workflow start (followed by an update to reflect workflow completion).  Rationale is that these workflows are so simple and mostly not-user-facing so that it's really not worth tracking failed workflows.
    1. To circumvent the issue where the db/codebase may have moved forward from the time we start the workflow and the time we are ready to write to the DB, we update the local checkout.  This implies that we should be running workflows against a git tag that accurately reflects the state of the database.  i.e., if we run a migration on the database, it is important to advance the tag.
    
    #### Issue
    [ch126776](https://app.clubhouse.io/genepi/story/126776/workflow-to-download-and-process-raw-gisaid-data)
    
    ### Test plan
    Successfully ran the workflows!

---
+++ workflows/ingest_gisaid/save.py
@@ -0,0 +1,47 @@
+import datetime
+
+import click
+
+from aspen.config.config import RemoteDatabaseConfig
+from aspen.database.connection import (
+    get_db_uri,
+    init_db,
+    session_scope,
+    SqlAlchemyInterface,
+)
+from aspen.database.models import RawGisaidDump
+
+
+@click.command("save")
+@click.option("--aspen-workflow-rev", type=str, required=True)
+@click.option("--aspen-creation-rev", type=str, required=True)
+@click.option("--start-time", type=int, required=True)
+@click.option("--end-time", type=int, required=True)
+@click.option("--gisaid-s3-bucket", type=str, required=True)
+@click.option("--gisaid-s3-key", type=str, required=True)
+def cli(
+    aspen_workflow_rev: str,
+    aspen_creation_rev: str,
+    start_time: int,
+    end_time: int,
+    gisaid_s3_bucket: str,
+    gisaid_s3_key: str,
+):
+    start_time_datetime = datetime.datetime.fromtimestamp(start_time)
+
+    interface: SqlAlchemyInterface = init_db(get_db_uri(RemoteDatabaseConfig()))
+
+    entity = RawGisaidDump(
+        download_date=start_time_datetime,
+        s3_bucket=gisaid_s3_bucket,
+        s3_key=gisaid_s3_key,
+    )
+
+    with session_scope(interface) as session:
+        session.add(entity)
+        session.flush()
+        print(entity.entity_id)
+
+
+if __name__ == "__main__":
+    cli()
