1a6f8cb447d9c2f18cae0405ab61a4b026e78866
Tony Tung <ttung@chanzuckerberg.com>
2021-03-26 13:59:12 -0700

    Export some symbols (#220)
    ### Description
    I forgot to include this asport of #210.
    
    #### Issue
    [ch126871](https://app.clubhouse.io/genepi/story/126871/schema-changes-to-support-cached-alignments)
    
    ### Test plan
    none

--- src/py/aspen/database/models/__init__.py
+++ src/py/aspen/database/models/__init__.py
@@ -7,6 +7,8 @@ from aspen.database.models.base import meta  # noqa: F401
 from aspen.database.models.cansee import CanSee, DataType  # noqa: F401
 from aspen.database.models.entity import Entity, EntityType  # noqa: F401
 from aspen.database.models.gisaid_dump import (  # noqa: F401
+    AlignedGisaidDump,
+    GisaidAlignmentWorkflow,
     GisaidDumpWorkflow,
     ProcessedGisaidDump,
     RawGisaidDump,
