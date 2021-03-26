d5a3703b76d175aadb321deef263f5c65f295109
Tony Tung <ttung@chanzuckerberg.com>
2021-03-26 11:04:31 -0700

    Docker image to run batch jobs
    ### Description
    This docker image is based on nextstrain/base and sets up a virtualenv with aspen's dependencies installed.  This can be used to invoke a variety of workflows.
    
    #### Issue
    [ch126778](https://app.clubhouse.io/genepi/story/126778)
    
    ### Test plan
    docker run -it --rm aspen/batch:latest trunk ls

---
+++ docker/aspen-batch/docker-entrypoint.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+if [ $# -lt 1 ]; then
+    echo "To activate virtual env, run: "
+    echo "% source /aspen/.venv/bin/activate\""
+
+    bash
+elif [ $# -lt 2 ]; then
+    # uh what?
+    echo "Usage: <revspec> <script> [<scriptarg> ...]"
+    echo "    Checks out <revspec> on aspen and run <script>"
+    echo "Usage:"
+    echo "    Interactive shell."
+    exit 1
+else
+    export ASPEN_GIT_REVSPEC="$1"
+
+    git init
+    git fetch git://github.com/chanzuckerberg/aspen "$ASPEN_GIT_REVSPEC"
+    git checkout FETCH_HEAD
+
+    /aspen/.venv/bin/pip install -U pip
+    /aspen/.venv/bin/pip install -e third-party/sqlalchemy-enum-tables/
+    /aspen/.venv/bin/pip install -e src/py/
+
+    shift
+    $*
+fi
