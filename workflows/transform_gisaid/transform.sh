77d9cf0ba9070a392c1b688e29027d30a73fb4c8
Tony Tung <ttung@chanzuckerberg.com>
2021-03-29 13:27:21 -0700

    align workflow (#218)
    ### Description
    This is the workflow to align the gisaid data.
    
    #### Issue
    [ch127651](https://app.clubhouse.io/genepi/story/127651/implement-caching-of-alignment-in-nexstrain)
    
    ### Test plan
    successfully invoked ingest -> transform -> align
    
    align crashes, but that's because I under-provisioned the EC2 instance.  Apparently mafft uses a ton of scratch space.

--- workflows/transform_gisaid/transform.sh
+++ workflows/transform_gisaid/transform.sh
@@ -75,3 +75,16 @@ entity_id=$(/aspen/.venv/bin/python workflows/transform_gisaid/save.py          
                                     --gisaid-sequences-s3-key "${sequences_key}" \
                                     --gisaid-metadata-s3-key "${metadata_key}"   \
          )
+
+# invoke the next workflow.
+# NOTE: when the number of cpus is modified, it would be prudent to modify workflows/align_gisaid/config.yaml.
+aws batch submit-job \
+    --job-name "align-gisaid"                \
+    --job-queue aspen-batch                      \
+    --job-definition aspen-batch-job-definition  \
+    --container-overrides "
+      {
+        \"command\": [\"${ASPEN_GIT_REVSPEC}\", \"workflows/align_gisaid/align.sh\", \"${entity_id}\"],
+        \"vcpus\": 32,
+        \"memory\": 420000
+      }"
