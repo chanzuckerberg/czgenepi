12b17e11361ec51f37d972aad8ff3cd7d4a1993a
Tony Tung <ttung@chanzuckerberg.com>
2021-03-31 09:41:58 -0700

    Extract policy allowing submissions of aspen batch jobs
    ### Description
    This is going to be applied to the role given to the cloudwatch event.
    
    #### Issue
    [ch126779](https://app.clubhouse.io/genepi/story/126779/cloudwatch-trigger-to-invoke-gisaid-workflow)
    
    ### Test plan
    make deploy-tf

--- terraform/aspen_batch_iam.tf
+++ terraform/aspen_batch_iam.tf
@@ -10,6 +10,30 @@ resource "aws_iam_role" "aspen-batch-jobs-role" {
 }
 
 
+# this policy is applied to any role who can submit a job to aspen batch.
+resource "aws_iam_policy" "can-submit-aspen-batch-job" {
+  name = "can-submit-aspen-batch-job"
+  policy = <<-POLICY
+  {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Effect": "Allow",
+              "Action": [
+                  "batch:SubmitJob"
+              ],
+              "Resource": [
+                  "arn:aws:batch:*:*:job-definition/${aws_batch_job_definition.aspen-batch-job-definition.name}",
+                  "arn:aws:batch:*:*:job-queue/${aws_batch_job_queue.aspen-batch-job-queue.name}"
+              ]
+          }
+      ]
+  }
+  POLICY
+}
+
+
+# these additional policies are applied to the role given to jobs running in the aspen batch pipeline.
 resource "aws_iam_policy" "aspen-batch-jobs-policies" {
   name = "aspen-batch-jobs-policies"
   policy = <<-POLICY
@@ -22,16 +46,6 @@ resource "aws_iam_policy" "aspen-batch-jobs-policies" {
               "Resource": [
                   "${data.aws_secretsmanager_secret.aspen_config.arn}",
                   "${data.aws_secretsmanager_secret.gisaid-download-credentials.arn}"
-              ]
-          },
-          {
-              "Effect": "Allow",
-              "Action": [
-                  "batch:SubmitJob"
-              ],
-              "Resource": [
-                  "arn:aws:batch:*:*:job-definition/${aws_batch_job_definition.aspen-batch-job-definition.name}",
-                  "arn:aws:batch:*:*:job-queue/${aws_batch_job_queue.aspen-batch-job-queue.name}"
               ]
           },
           {
@@ -56,6 +70,12 @@ resource "aws_iam_policy" "aspen-batch-jobs-policies" {
       ]
   }
   POLICY
+}
+
+
+resource "aws_iam_role_policy_attachment" "aspen-batch-job-can-submit-more-jobs" {
+  role       = aws_iam_role.aspen-batch-jobs-role.name
+  policy_arn = aws_iam_policy.can-submit-aspen-batch-job.arn
 }
 
 
