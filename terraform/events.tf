8c1663e950f244e52c7b275db8d57a847cfc108f
Tony Tung <ttung@chanzuckerberg.com>
2021-04-01 13:14:07 -0700

    Event trigger for ingest-gisaid (#229)
    ### Description
    event and target for triggering the ingest-gisaid workflow.
    
    #### Issue
    [ch126779](https://app.clubhouse.io/genepi/story/126779/cloudwatch-trigger-to-invoke-gisaid-workflow)
    
    ### Test plan
    Changed it to every 15 minutes, and watched it kick off a build.

---
+++ terraform/events.tf
@@ -0,0 +1,46 @@
+resource "aws_iam_role" "cloudwatch-batch-trigger-role" {
+  name = "cloudwatch-batch-trigger-role"
+  assume_role_policy = templatefile("${path.module}/iam_templates/trust_policy.json", {
+    trust_services = ["events"]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "cloudwatch-can-trigger-batch-jobs" {
+  role       = aws_iam_role.cloudwatch-batch-trigger-role.name
+  policy_arn = aws_iam_policy.can-submit-aspen-batch-job.arn
+}
+
+
+resource "aws_cloudwatch_event_rule" "ingest-gisaid-rule" {
+  name                = "ingest-gisaid"
+  description         = "Ingest GISAID data daily"
+  schedule_expression = "cron(0 0 ? * 1-5 *)"
+}
+
+
+resource "aws_cloudwatch_event_target" "ingest-gisaid-target" {
+  target_id = "ingest-gisaid-target"
+  rule      = aws_cloudwatch_event_rule.ingest-gisaid-rule.name
+  arn       = aws_batch_job_queue.aspen-batch-job-queue.arn
+  role_arn  = aws_iam_role.cloudwatch-batch-trigger-role.arn
+
+  batch_target {
+    job_name       = "ingest-gisaid"
+    job_definition = aws_batch_job_definition.aspen-batch-job-definition.name
+  }
+
+  # These are overrides to the defaults specified in the job definition.  See
+  # https://docs.aws.amazon.com/batch/latest/APIReference/API_ContainerOverrides.html
+  # for more details.
+  input = <<-CONTAINER_OVERRIDES
+  {
+    "ContainerOverrides": {
+      "Command": [
+        "trunk",
+        "workflows/ingest_gisaid/ingest.sh"
+      ]
+    }
+  }
+  CONTAINER_OVERRIDES
+}
