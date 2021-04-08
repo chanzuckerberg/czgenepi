e4e6eca29c41daf655c0fdf7499646ce82127add
Jessica Gadling <jgadling@chanzuckerberg.com>
2021-04-07 17:17:57 -0700

    Update src/py/aspen/app/views/health.py
    Co-authored-by: Phoenix Logan <phoenix@phoenixlogan.net>

--- src/py/aspen/app/views/health.py
+++ src/py/aspen/app/views/health.py
@@ -5,7 +5,9 @@ from aspen.app.app import application
 
 @application.route("/health", methods=["GET"])
 def health():
-    # Provide an endpoint that AWS Load Balancers can use to determine
-    # whether this service is healthy.
+    """
+    Provide an endpoint that AWS Load Balancers can use to determine
+    whether this service is healthy.
+    """
     health_check = Response(response="healthy", status=200)
     return health_check
