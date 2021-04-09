6c05f35839a1a3126d664cd23909a2da9c3edc34
Tony Tung <ttung@chanzuckerberg.com>
2021-04-09 09:01:40 -0700

    Migrate all test to use existing factories (#239)
    ### Description
    Remove redundant constructions of Group, Sample, and SequencingReadsCollection.
    
    ### Test plan
    `make unit-tests`

--- src/py/aspen/conftest.py
+++ src/py/aspen/conftest.py
@@ -1,8 +1,4 @@
 from aspen.test_infra.flask import app, client  # noqa: F401
-from aspen.test_infra.models.sample import sample_factory  # noqa: F401
-from aspen.test_infra.models.sequences import sequencing_read_factory  # noqa: F401
-from aspen.test_infra.models.usergroup import group_factory  # noqa: F401
-from aspen.test_infra.models.usergroup import user_factory  # noqa: F401
 from aspen.test_infra.postgres import postgres_database  # noqa: F401
 from aspen.test_infra.sqlalchemy import session  # noqa: F401
 from aspen.test_infra.sqlalchemy import sqlalchemy_interface  # noqa: F401
