24b06c3b8a6a07f8d165a418c85470c3ada51f10
lvreynoso <lvreynoso@protonmail.com>
2021-04-12 14:39:49 -0700

    Import auspice view into main.py

--- src/py/aspen/main.py
+++ src/py/aspen/main.py
@@ -1,6 +1,7 @@
 # this is where flask finds the entry point for the application.
 
 from aspen.app.app import application  # noqa: F401
+from aspen.app.views.auspice import auspice_view  # noqa: F401
 from aspen.app.views.auth import callback_handling, login, logout  # noqa: F401
 from aspen.app.views.health import health  # noqa: F401
 from aspen.app.views.index import serve  # noqa: F401
