d75cfb373f5a9e72531b2c6a8ad24562e150a3be
lvreynoso <lvreynoso@protonmail.com>
2021-04-12 13:55:14 -0700

    Greatly simplify code

--- src/py/aspen/app/views/auspice.py
+++ src/py/aspen/app/views/auspice.py
@@ -1,23 +1,16 @@
 import logging
 import os
-from typing import Iterable
+from typing import Optional
 
 import boto3
 from flask import make_response
-from sqlalchemy.orm import joinedload
 
 from aspen.app.app import application
 from aspen.database.connection import session_scope
-from aspen.database.models import PhyloRun, PhyloTree, WorkflowStatusType
+from aspen.database.models import PhyloTree
+from aspen.error.recoverable import RecoverableError
 
 logger = logging.getLogger(__name__)
-
-
-def tree_dive(phylo_runs: Iterable[PhyloRun], phylo_tree_id: int):
-    for phylo_run in phylo_runs:
-        for output in phylo_run.outputs:
-            if isinstance(output, PhyloTree) and output.entity_id == phylo_tree_id:
-                return output
 
 
 @application.route(
@@ -25,13 +18,14 @@ def tree_dive(phylo_runs: Iterable[PhyloRun], phylo_tree_id: int):
 )
 def auspice_view(phylo_tree_id: int):
     with session_scope(application.DATABASE_INTERFACE) as db_session:
-        phylo_runs: Iterable[PhyloRun] = (
-            db_session.query(PhyloRun)
-            .options(joinedload(PhyloRun.outputs))
-            .filter(PhyloRun.workflow_status == WorkflowStatusType.COMPLETED)
+        phylo_tree: Optional[PhyloTree] = (
+            db_session.query(PhyloTree)
+            .filter(PhyloTree.entity_id == phylo_tree_id)
+            .one_or_none()
         )
 
-        phylo_tree: PhyloTree = tree_dive(phylo_runs, phylo_tree_id)
+        if not phylo_tree:
+            raise RecoverableError(f"Phylo Tree {phylo_tree_id} not found.")
 
         s3_client = boto3.resource(
             "s3",
