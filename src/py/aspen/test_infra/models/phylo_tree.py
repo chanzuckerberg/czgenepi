5b6d1dbe72ec6017e52b148b6aa2b6517f6e8b04
Timmy Huang <thuang@chanzuckerberg.com>
2021-04-16 12:22:59 -0700

    remove src/py/aspen/test_infra/models/phylo_tree.py

--- src/py/aspen/test_infra/models/phylo_tree.py
+++
@@ -1,9 +0,0 @@
-from aspen.database.models import PhyloTree
-
-
-def phylotree_factory(
-        entity,
-        s3_bucket="test_bucket",
-        s3_key="test_tree",
-        constituent_samples
-):
