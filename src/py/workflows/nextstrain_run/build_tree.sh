d40436e8177773bd6fb6514ac52b46e93b9b4f02
Tony Tung <ttung@chanzuckerberg.com>
2021-04-15 20:49:03 -0700

    Workflow for nextstrain run (#256)
    ### Description
    When invoked with phylo run as its parameter, `build_tree.sh` invokes `export.py`.
    
    `export.py` does the following:
    1. exports out the aspen sequences and metadata to run nextstrain.
    1. dumps information about the linked aligned gisaid sequences, which is consumed by `build_tree.sh` to download the gisaid dataset.
    1. generates the build.yaml file from the template and the template args referenced in the phylo run.
    
    #### Duplicate sequences
    There may be sequences duplicated in aspen and gisaid.  In that case, nextstrain resolves metadata differences in the order files are specified.  Since we specify the gisaid data first, aspen data overrides that IFF aspen provides that field.
    
    Differences between Sequence data is normally treated by nextstrain as a failure.  As a result, we patch the script that merges sequences to ensure that we use the sequences in the order that we expect.
    
    #### Issue
    [ch126782](https://app.clubhouse.io/genepi/story/126782/workflow-to-pull-the-data-from-aspen-api-endpoint-and-launch-nextstrain-build)
    
    ### Test plan
    `aspen-cli db --remote create-phylo-run --group-name 'Orange County Public Health Lab'  --all-group-sequences --builds-template-file src/py/workflows/nextstrain_run/builds_templates/group.yaml --builds-template-args '{"division": "California", "location": "Orange County"}' --git-refspec ttung/setup-nextstrain-run`
    and watched the run.

---
+++ src/py/workflows/nextstrain_run/build_tree.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -Eeuxo pipefail
+shopt -s inherit_errexit
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <phylo_run_id>"
+    exit 1
+fi
+
+# install jq
+apt-get install -y jq
+
+build_id=$(date +%Y%m%d-%H%M)
+
+# set up ncov
+mkdir -p /ncov/my_profiles/aspen /ncov/results
+cd /ncov
+git init
+git fetch git://github.com/nextstrain/ncov.git
+git checkout FETCH_HEAD
+ncov_git_rev=$(git rev-parse HEAD)
+
+# patch ncov/scripts/combine-and-dedup-fastas.py
+patch -p1 < /aspen/src/py/workflows/nextstrain_run/combine-and-dedup-fastas.py.patch
+
+cp /aspen/src/py/workflows/nextstrain_run/nextstrain_profile/* /ncov/my_profiles/aspen/
+
+# dump the sequences, metadata, and builds.yaml for a run out to disk.
+aligned_gisaid_location=$(
+    /aspen/.venv/bin/python /aspen/src/py/workflows/nextstrain_run/export.py  \
+                            --phylo-run-id "$1"                               \
+                            --sequences /ncov/data/sequences_aspen.fasta      \
+                            --metadata /ncov/data/metadata_aspen.tsv          \
+                            --builds-file /ncov/my_profiles/aspen/builds.yaml \
+)
+aligned_gisaid_s3_bucket=$(echo "${aligned_gisaid_location}" | jq -r .bucket)
+aligned_gisaid_sequences_s3_key=$(echo "${aligned_gisaid_location}" | jq -r .sequences_key)
+aligned_gisaid_metadata_s3_key=$(echo "${aligned_gisaid_location}" | jq -r .metadata_key)
+
+# fetch the gisaid dataset
+aws s3 cp --no-progress s3://"${aligned_gisaid_s3_bucket}"/"${aligned_gisaid_sequences_s3_key}" - | xz -d > /ncov/results/aligned_gisaid.fasta
+aws s3 cp --no-progress s3://"${aligned_gisaid_s3_bucket}"/"${aligned_gisaid_metadata_s3_key}" /ncov/data/metadata_gisaid.tsv
+
+snakemake --printshellcmds auspice/ncov_aspen.json --profile my_profiles/aspen/  --resources=mem_mb=312320
+
+# upload the tree to S3
+bucket=aspen-db-data-"${DEPLOYMENT_ENVIRONMENT}"
+key=phylo_run/"${build_id}"/ncov_aspen.json
+aws s3 cp /ncov/auspice/ncov_aspen.json s3://"${bucket}"/"${key}"
+
+cd /aspen/
+
+# update aspen
+aspen_workflow_rev=$(git rev-parse HEAD)
+git fetch git://github.com/chanzuckerberg/aspen "$ASPEN_GIT_REVSPEC"
+git checkout FETCH_HEAD
+aspen_creation_rev=$(git rev-parse HEAD)
+
+if [ "${aspen_workflow_rev}" != "${aspen_creation_rev}" ]; then
+    # reinstall aspen
+    /aspen/.venv/bin/pip install -e src/py
+fi
+
+end_time=$(date +%s)
+
+# create the objects
+/aspen/.venv/bin/python /aspen/src/py/workflows/nextstrain_run/save.py               \
+                        --aspen-workflow-rev "${aspen_workflow_rev}"                 \
+                        --aspen-creation-rev "${aspen_creation_rev}"                 \
+                        --ncov-rev "${ncov_git_rev}"                                 \
+                        --aspen-docker-image-version "${ASPEN_DOCKER_IMAGE_VERSION}" \
+                        --end-time "${end_time}"                                     \
+                        --phylo-run-id "$1"                                          \
+                        --bucket "$bucket"                                           \
+                        --key "$key"                                                 \
+                        --tree-path /ncov/auspice/ncov_aspen.json                    \
---
+++ src/py/workflows/nextstrain_run/export.py
@@ -0,0 +1,246 @@
+import csv
+import io
+import json
+from pathlib import Path
+from typing import Any, Mapping, MutableMapping, Set, Tuple
+
+import click
+from sqlalchemy.orm import aliased, joinedload, with_polymorphic
+
+from aspen.config.config import RemoteDatabaseConfig
+from aspen.database.connection import (
+    get_db_uri,
+    init_db,
+    session_scope,
+    SqlAlchemyInterface,
+)
+from aspen.database.models import (
+    Accession,
+    AlignedGisaidDump,
+    CalledPathogenGenome,
+    Entity,
+    HostFilteredSequencingReadsCollection,
+    PathogenGenome,
+    PhyloRun,
+    PublicRepositoryType,
+    Sample,
+    SequencingReadsCollection,
+    UploadedPathogenGenome,
+)
+from aspen.database.models.workflow import Workflow
+
+METADATA_CSV_FIELDS = [
+    "strain",
+    "virus",
+    "gisaid_epi_isl",
+    "genbank_accession",
+    "date",
+    "region",
+    "country",
+    "division",
+    "location",
+    "region_exposure",
+    "country_exposure",
+    "division_exposure",
+    "segment",
+    "length",
+    "host",
+    "age",
+    "sex",
+    "pango_lineage",
+    "GISAID_clade",
+    "originating_lab",
+    "submitting_lab",
+    "authors",
+    "url",
+    "title",
+    "paper_url",
+    "date_submitted",
+]
+
+
+@click.command("save")
+@click.option("--phylo-run-id", type=int, required=True)
+@click.option("sequences_fh", "--sequences", type=click.File("w"), required=True)
+@click.option("metadata_fh", "--metadata", type=click.File("w"), required=True)
+@click.option("builds_file_fh", "--builds-file", type=click.File("w"), required=True)
+def cli(
+    phylo_run_id: int,
+    sequences_fh: io.TextIOBase,
+    metadata_fh: io.TextIOBase,
+    builds_file_fh: io.TextIOBase,
+):
+    interface: SqlAlchemyInterface = init_db(get_db_uri(RemoteDatabaseConfig()))
+
+    with session_scope(interface) as session:
+        # this allows us to load the secondary tables of a polymorphic type.  In this
+        # case, we want to load the inputs of a phylo run, provided the input is of type
+        # `PathogenGenome` and `AlignedGisaidDump`.
+        phylo_run_inputs = with_polymorphic(
+            Entity,
+            [PathogenGenome, AlignedGisaidDump],
+            flat=True,
+        )
+        phylo_run: PhyloRun = (
+            session.query(PhyloRun)
+            .filter(PhyloRun.workflow_id == phylo_run_id)
+            .options(
+                joinedload(PhyloRun.inputs.of_type(phylo_run_inputs)).undefer(
+                    phylo_run_inputs.PathogenGenome.sequence
+                )
+                # load the workflows that generated the called pathogen genomes
+                .subqueryload(phylo_run_inputs.producing_workflow)
+                # load the host-filtered sequencing reads that generated the called
+                # pathogen genomes.
+                .subqueryload(Workflow.inputs)
+                # load the workflows that generated the host-filtered sequencing
+                # reads.
+                .subqueryload(Entity.producing_workflow)
+                # load the sequencing reads that generated the host-filtered
+                # sequencing reads.
+                .subqueryload(Workflow.inputs)
+            )
+            .one()
+        )
+
+        # get all the children that are pathogen genomes
+        pathogen_genomes = [
+            inp for inp in phylo_run.inputs if isinstance(inp, PathogenGenome)
+        ]
+        # get the aligned gisaid run info.
+        aligned_gisaid = [
+            inp for inp in phylo_run.inputs if isinstance(inp, AlignedGisaidDump)
+        ][0]
+
+        aspen_root = Path(__file__).parent.parent.parent.parent.parent
+        with (aspen_root / phylo_run.template_file_path).open("r") as build_template_fh:
+            build_template = build_template_fh.read()
+        template_args = (
+            phylo_run.template_args
+            if isinstance(phylo_run.template_args, Mapping)
+            else {}
+        )
+        builds_file_fh.write(build_template.format(**template_args))
+
+        uploaded_pathogen_genomes = {
+            pathogen_genome
+            for pathogen_genome in pathogen_genomes
+            if isinstance(pathogen_genome, UploadedPathogenGenome)
+        }
+        sequencing_reads_collections = {
+            pathogen_genome.get_parents(HostFilteredSequencingReadsCollection)[
+                0
+            ].get_parents(SequencingReadsCollection)[0]
+            for pathogen_genome in pathogen_genomes
+            if isinstance(pathogen_genome, CalledPathogenGenome)
+        }
+
+        sample_ids = {
+            uploaded_pathogen_genome.sample_id
+            for uploaded_pathogen_genome in uploaded_pathogen_genomes
+        }
+        sample_ids.update(
+            {
+                sequencing_reads_collection.sample_id
+                for sequencing_reads_collection in sequencing_reads_collections
+            }
+        )
+
+        sample_id_to_sample: Mapping[int, Sample] = {
+            sample.id: sample
+            for sample in session.query(Sample).filter(Sample.id.in_(sample_ids))
+        }
+        accession_input_alias = aliased(Entity)
+        pathogen_genome_id_repository_type_to_accession_names: Mapping[
+            Tuple[int, PublicRepositoryType], str
+        ] = {
+            (
+                accession.get_parents(PathogenGenome)[0].entity_id,
+                accession.repository_type,
+            ): accession.public_identifier
+            for accession in session.query(Accession)
+            .join(Accession.producing_workflow)
+            .join(accession_input_alias, Workflow.inputs)
+            .filter(
+                accession_input_alias.id.in_(
+                    {pathogen_genome.entity_id for pathogen_genome in pathogen_genomes}
+                )
+            )
+        }
+
+        aspen_samples: Set[str] = set()
+        metadata_csv_fh = csv.DictWriter(
+            metadata_fh, METADATA_CSV_FIELDS, delimiter="\t"
+        )
+        metadata_csv_fh.writeheader()
+        for pathogen_genome in pathogen_genomes:
+            # find the corresponding sample
+            if isinstance(pathogen_genome, UploadedPathogenGenome):
+                sample_id = pathogen_genome.sample_id
+            elif isinstance(pathogen_genome, CalledPathogenGenome):
+                sample_id = (
+                    pathogen_genome.get_parents(HostFilteredSequencingReadsCollection)[
+                        0
+                    ]
+                    .get_parents(SequencingReadsCollection)[0]
+                    .sample_id
+                )
+            else:
+                raise ValueError("pathogen genome of unknown type")
+            sample = sample_id_to_sample[sample_id]
+            aspen_samples.add(sample.public_identifier)
+
+            sequence = "".join(
+                [
+                    line
+                    for line in pathogen_genome.sequence.splitlines()
+                    if not (line.startswith(">") or line.startswith(";"))
+                ]
+            )
+            sequence = sequence.strip("Nn")
+
+            aspen_metadata_row: MutableMapping[str, Any] = {
+                "strain": sample.public_identifier,
+                "virus": "ncov",
+                "gisaid_epi_isl": pathogen_genome_id_repository_type_to_accession_names.get(
+                    (pathogen_genome.entity_id, PublicRepositoryType.GISAID), ""
+                ),
+                "genbank_accession": pathogen_genome_id_repository_type_to_accession_names.get(
+                    (pathogen_genome.entity_id, PublicRepositoryType.GENBANK), ""
+                ),
+                "date": sample.collection_date.strftime("%Y-%m-%d"),
+                "region": sample.region.value,
+                "country": sample.country,
+                "division": sample.division,
+                "location": sample.location,
+                "region_exposure": sample.region.value,
+                "country_exposure": sample.country,
+                "division_exposure": sample.division,
+                "segment": "genome",
+                "length": len(sequence),
+                "host": "Human",
+                "age": "?",
+                "sex": "?",
+                "originating_lab": sample.sample_collected_by,
+                "submitting_lab": sample.submitting_group.name,
+                "authors": ", ".join(sample.authors),
+            }
+
+            metadata_csv_fh.writerow(aspen_metadata_row)
+            sequences_fh.write(f">{sample.public_identifier}\n")
+            sequences_fh.write(sequence)
+            sequences_fh.write("\n")
+
+        print(
+            json.dumps(
+                {
+                    "bucket": aligned_gisaid.s3_bucket,
+                    "metadata_key": aligned_gisaid.metadata_s3_key,
+                    "sequences_key": aligned_gisaid.sequences_s3_key,
+                }
+            )
+        )
+
+
+if __name__ == "__main__":
+    cli()
