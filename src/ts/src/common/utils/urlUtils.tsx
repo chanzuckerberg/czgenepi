282886102234662459d9d924858ca376a3de672a
Timmy Huang <thuang@chanzuckerberg.com>
2021-04-19 08:27:22 -0700

    thuang-update-http-protocol-removal-regex

--- src/ts/src/common/utils/urlUtils.tsx
+++ src/ts/src/common/utils/urlUtils.tsx
@@ -2,6 +2,6 @@ export function stripProtocol(url: string | undefined): string | undefined {
   if (url === undefined) {
     return undefined;
   }
-  const re = /^.*:\/\//i;
+  const re = /^[^:]*:\/\//i;
   return url.replace(re, "");
 }
