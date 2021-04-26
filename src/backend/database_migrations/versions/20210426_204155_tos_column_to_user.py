a1a97d09c548674c9aa77b25cbd2f4b11ce50281
phoenixAja <phoenix@phoenixlogan.net>
2021-04-26 14:12:26 -0700

    removed alembic autogenerated comment

--- src/backend/database_migrations/versions/20210426_204155_tos_column_to_user.py
+++ src/backend/database_migrations/versions/20210426_204155_tos_column_to_user.py
@@ -8,7 +8,6 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 from alembic import op
 
-# revision identifiers, used by Alembic.
 revision = "20210426_204155"
 down_revision = "20210420_231414"
 branch_labels = None
