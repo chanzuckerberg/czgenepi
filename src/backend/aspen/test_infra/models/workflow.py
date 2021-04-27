4ba8f3158cdf348ef75798114bb987824181729f
phoenixAja <phoenix@phoenixlogan.net>
2021-04-27 16:01:51 -0700

    pr feedback

--- src/backend/aspen/test_infra/models/workflow.py
+++ src/backend/aspen/test_infra/models/workflow.py
@@ -5,7 +5,6 @@ from aspen.database.models.workflow import WorkflowStatusType, WorkflowType
 
 
 def call_consensus_factory(
-    workflow_type=WorkflowType.CALL_CONSENSUS,
     start_datetime=datetime.now() - timedelta(days=1),
     end_datetime=datetime.now(),
     workflow_status=WorkflowStatusType.COMPLETED,
@@ -14,7 +13,6 @@ def call_consensus_factory(
     outputs=[],
 ):
     return CallConsensus(
-        workflow_type=workflow_type,
         start_datetime=start_datetime,
         end_datetime=end_datetime,
         workflow_status=workflow_status,
