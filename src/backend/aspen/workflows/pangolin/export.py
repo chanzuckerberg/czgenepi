c8d91a45751f16861777842e57bf9c3c7e71ffaa
phoenixAja <phoenix@phoenixlogan.net>
2021-04-27 19:22:08 -0700

    adding test

---
+++ src/backend/aspen/workflows/pangolin/export.py
@@ -0,0 +1,63 @@
+from typing import Iterable, MutableSequence, Optional, Sequence, Type
+import io
+import click
+from sqlalchemy.orm import aliased, joinedload, undefer
+
+from aspen.config.config import RemoteDatabaseConfig
+from aspen.database.connection import (
+    get_db_uri,
+    init_db,
+    session_scope,
+    SqlAlchemyInterface,
+)
+from aspen.database.models import (
+    CalledPathogenGenome,
+    Entity,
+    HostFilteredSequencingReadsCollection,
+    PathogenGenome,
+    Sample,
+    SequencingReadsCollection,
+    UploadedPathogenGenome,
+)
+
+@click.command("export")
+@click.option("--sample-public-identifier", type=str, required=True, multiple=True)
+@click.option("sequences_fh", "--sequences", type=click.File("w"), required=True)
+def cli(
+    sample_public_identifier: Sequence[str],
+    sequences_fh: io.TextIOBase
+):
+    interface: SqlAlchemyInterface = init_db(get_db_uri(RemoteDatabaseConfig()))
+
+    with session_scope(interface) as session:
+        all_samples: Iterable[Sample] = (
+            session.query(Sample)
+            .filter(
+                Sample.public_identifier.in_(sample_public_identifier)
+            ).options(
+                joinedload(Sample.uploaded_pathogen_genome).undefer(
+                    UploadedPathogenGenome.sequence
+                ),
+            )
+        )
+
+        for sample in all_samples:
+            pathogen_genome = sample.uploaded_pathogen_genome
+
+            sequence = "".join(
+                [
+                    line
+                    for line in pathogen_genome.sequence.splitlines()
+                    if not (line.startswith(">") or line.startswith(";"))
+                ]
+            )
+
+            sequence = sequence.strip("Nn")
+            sequences_fh.write(f">{pathogen_genome.entity_id}\n")
+            sequences_fh.write(sequence)
+
+
+
+if __name__ == "__main__":
+    cli(["--sample-public-identifier", "USA/CA-CZB-23525/2021", "--sample-public-identifier", "USA/CA-CZB-23526/2021"])
+    cli.export()
---
+++ src/backend/aspen/workflows/pangolin/tests/test_pangolin_workflow.py
@@ -0,0 +1,43 @@
+from click.testing import CliRunner
+
+from aspen.workflows.pangolin.export import cli
+from aspen.test_infra.models.usergroup import group_factory, user_factory
+from aspen.test_infra.models.sample import sample_factory
+from aspen.test_infra.models.sequences import uploaded_pathogen_genome_factory
+
+
+def test_pangolin_export(mocker, session, postgres_database):
+    group = group_factory()
+    user = user_factory(group)
+
+    for i in range(0,2):
+        sample = sample_factory(
+            private_identifier=f"private_identifier_{i}",
+            public_identifier=f"public_identifier_{i}"
+        )
+        session.add(sample)
+        pathogen_genome = uploaded_pathogen_genome_factory(sample)
+        session.add(pathogen_genome)
+        session.commit()
+
+    mocker.patch(
+        "aspen.config.RemoteDatabaseConfig.DATABASE_URI",
+        return_value=postgres_database.as_uri()
+    )
+
+    runner = CliRunner()
+    runner.invoke(
+        cli,
+        [
+            "--sequences",
+            "test.fa",
+            "--sample-public-identifier",
+            "public_identifier_1",
+            "--sample-public-identifier",
+            "public_identifier_2"
+        ]
+    )
+
+
+
+
