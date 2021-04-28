fe385801bbdba8d667d6d3f3a2c1f61188f9b10a
Tony Tung <ttung@chanzuckerberg.com>
2021-04-27 19:20:43 -0700

    Bucket for auspice data.
    ### Description
    This is the bucket for temporary objects that are shared via presigned urls to auspice.
    
    #### Issue
    [ch133611](https://app.clubhouse.io/genepi/story/133611/rewrite-tree-data-with-private-identifiers)
    
    ### Test plan
    `make deploy-tf`

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,3 +2,18 @@ resource "aws_s3_bucket" "aspen-db-data" {
   bucket = join("", ["aspen-db-data-", var.DEPLOYMENT_ENVIRONMENT])
   acl = "private"
 }
+
+
+resource "aws_s3_bucket" "aspen-external-auspice-data" {
+  bucket = join("", ["aspen-external-auspice-data-", var.DEPLOYMENT_ENVIRONMENT])
+  acl = "private"
+
+  lifecycle_rule {
+    id      = "expire"
+    enabled = true
+
+    expiration {
+      days = 30
+    }
+  }
+}
