07d47c296e50ca378b952c91f36f84efc5186846
Phoenix Logan <phoenix@phoenixlogan.net>
2021-04-28 18:57:08 -0700

    usergroup PUT  (#308)

--- src/backend/aspen/app/views/tests/test_views.py
+++ src/backend/aspen/app/views/tests/test_views.py
@@ -1,9 +1,10 @@
 import json
 
+from aspen.database.models.usergroup import User
 from aspen.test_infra.models.usergroup import group_factory, user_factory
 
 
-def test_usergroup_view(session, app, client):
+def test_usergroup_view_get(session, app, client):
     group = group_factory()
     user = user_factory(group)
     session.add(group)
@@ -13,6 +14,41 @@ def test_usergroup_view(session, app, client):
     res = client.get("/api/usergroup")
     expected = {"user": user.to_dict(), "group": group.to_dict()}
     assert expected == json.loads(res.get_data(as_text=True))
+
+
+def test_usergroup_view_put_pass(session, app, client):
+    group = group_factory()
+    user = user_factory(group, agreed_to_tos=False)
+    session.add(group)
+    session.commit()
+    with client.session_transaction() as sess:
+        sess["profile"] = {"name": user.name, "user_id": user.auth0_user_id}
+
+    data = {"agreed_to_tos": True}
+    res = client.put("/api/usergroup", json=json.dumps(data))
+
+    # start a new transaction
+    session.close()
+    session.begin()
+    updated_user = (
+        session.query(User).filter(User.auth0_user_id == user.auth0_user_id).one()
+    )
+    assert updated_user.agreed_to_tos
+    assert res.status == "200 OK"
+
+
+def test_usergroup_view_put_fail(session, app, client):
+    group = group_factory()
+    user = user_factory(group, agreed_to_tos=False)
+    session.add(group)
+    session.commit()
+    with client.session_transaction() as sess:
+        sess["profile"] = {"name": user.name, "user_id": user.auth0_user_id}
+
+    data = {"fake_field": "even faker"}
+    res = client.put("/api/usergroup", json=json.dumps(data))
+
+    assert res.status == "400 BAD REQUEST"
 
 
 def test_redirect(app, client):
--- src/backend/aspen/app/views/usergroup.py
+++ src/backend/aspen/app/views/usergroup.py
@@ -1,17 +1,44 @@
-from flask import jsonify, session
+import json
+from typing import Dict, List, Union
+
+from flask import jsonify, request, Response, session
 
 from aspen.app.app import application, requires_auth
 from aspen.app.views.api_utils import get_usergroup_query
 from aspen.database.connection import session_scope
+from aspen.database.models.usergroup import User
 
 
-@application.route("/api/usergroup", methods=["GET"])
+@application.route("/api/usergroup", methods=["GET", "PUT"])
 @requires_auth
 def usergroup():
     with session_scope(application.DATABASE_INTERFACE) as db_session:
         # since authentication is required to access view this information is guaranteed
         # to be in session
-        profile = session["profile"]
-        user = get_usergroup_query(db_session, profile["user_id"]).one()
-        user_groups = {"user": user.to_dict(), "group": user.group.to_dict()}
-        return jsonify(user_groups)
+        profile: Dict[str, str] = session["profile"]
+        user: User = get_usergroup_query(db_session, profile["user_id"]).one()
+
+        if request.method == "GET":
+            user_groups: Dict[str, Dict[str, Union[str, bool]]] = {
+                "user": user.to_dict(),
+                "group": user.group.to_dict(),
+            }
+            return jsonify(user_groups)
+
+        if request.method == "PUT":
+            update_allowed_fields: List[str] = ["name", "email", "agreed_to_tos"]
+            fields_to_update: Dict[str, Union[str, bool]] = json.loads(
+                request.get_json()
+            )
+            for key, value in fields_to_update.items():
+                if hasattr(user, key) and key in update_allowed_fields:
+                    setattr(user, key, value)
+                else:
+                    return Response(
+                        f"only the following fields can be updated on the User object: {update_allowed_fields}, you provided the attribute <{key}>",
+                        400,
+                    )
+
+            # all fields have updated successfully
+            db_session.flush()
+            return jsonify(success=True)
