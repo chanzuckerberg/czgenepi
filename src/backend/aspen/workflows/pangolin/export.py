003d1e9ebddad832b39b66485565e43887959103
phoenixAja <phoenix@phoenixlogan.net>
2021-04-29 14:00:09 -0700

    lint

--- src/backend/aspen/workflows/pangolin/export.py
+++ src/backend/aspen/workflows/pangolin/export.py
@@ -1,7 +1,8 @@
-from typing import Iterable, MutableSequence, Optional, Sequence, Type
 import io
+from typing import Iterable, Sequence
+
 import click
-from sqlalchemy.orm import aliased, joinedload, undefer
+from sqlalchemy.orm import joinedload
 
 from aspen.config.config import RemoteDatabaseConfig
 from aspen.database.connection import (
@@ -10,31 +11,20 @@ from aspen.database.connection import (
     session_scope,
     SqlAlchemyInterface,
 )
-from aspen.database.models import (
-    CalledPathogenGenome,
-    Entity,
-    HostFilteredSequencingReadsCollection,
-    PathogenGenome,
-    Sample,
-    SequencingReadsCollection,
-    UploadedPathogenGenome,
-)
+from aspen.database.models import Sample, UploadedPathogenGenome
+
 
 @click.command("export")
 @click.option("--sample-public-identifier", type=str, required=True, multiple=True)
 @click.option("sequences_fh", "--sequences", type=click.File("w"), required=True)
-def cli(
-    sample_public_identifier: Sequence[str],
-    sequences_fh: io.TextIOBase
-):
+def cli(sample_public_identifier: Sequence[str], sequences_fh: io.TextIOBase):
     interface: SqlAlchemyInterface = init_db(get_db_uri(RemoteDatabaseConfig()))
 
     with session_scope(interface) as session:
         all_samples: Iterable[Sample] = (
             session.query(Sample)
-            .filter(
-                Sample.public_identifier.in_(sample_public_identifier)
-            ).options(
+            .filter(Sample.public_identifier.in_(sample_public_identifier))
+            .options(
                 joinedload(Sample.uploaded_pathogen_genome).undefer(
                     UploadedPathogenGenome.sequence
                 ),
@@ -57,7 +47,5 @@ def cli(
             sequences_fh.write(sequence)
 
 
-
 if __name__ == "__main__":
-    cli(["--sample-public-identifier", "USA/CA-CZB-23525/2021", "--sample-public-identifier", "USA/CA-CZB-23526/2021"])
-    cli.export()
+    cli()
--- src/backend/aspen/workflows/pangolin/tests/test_pangolin_workflow.py
+++ src/backend/aspen/workflows/pangolin/tests/test_pangolin_workflow.py
@@ -1,19 +1,19 @@
 from click.testing import CliRunner
 
-from aspen.workflows.pangolin.export import cli
-from aspen.test_infra.models.usergroup import group_factory
 from aspen.test_infra.models.sample import sample_factory
 from aspen.test_infra.models.sequences import uploaded_pathogen_genome_factory
+from aspen.test_infra.models.usergroup import group_factory
+from aspen.workflows.pangolin.export import cli
 
 
 def test_pangolin_export(mocker, session, postgres_database):
     group = group_factory()
 
-    for i in range(1,3):
+    for i in range(1, 3):
         sample = sample_factory(
             group,
             private_identifier=f"private_identifier_{i}",
-            public_identifier=f"public_identifier_{i}"
+            public_identifier=f"public_identifier_{i}",
         )
         session.add(sample)
         pathogen_genome = uploaded_pathogen_genome_factory(sample)
@@ -21,9 +21,9 @@ def test_pangolin_export(mocker, session, postgres_database):
         session.commit()
 
     mocker.patch(
-        'aspen.config.config.RemoteDatabaseConfig.DATABASE_URI',
+        "aspen.config.config.RemoteDatabaseConfig.DATABASE_URI",
         new_callable=mocker.PropertyMock,
-        return_value=postgres_database.as_uri()
+        return_value=postgres_database.as_uri(),
     )
 
     runner = CliRunner()
@@ -35,13 +35,10 @@ def test_pangolin_export(mocker, session, postgres_database):
             "--sample-public-identifier",
             "public_identifier_1",
             "--sample-public-identifier",
-            "public_identifier_2"
-        ]
+            "public_identifier_2",
+        ],
     )
     assert result.exit_code == 0
     with open("test.fa", "r") as fh:
         lines = fh.read()
-        assert lines == '>1\nTCGGCG>2\nTCGGCG'
-
-
-
+        assert lines == ">1\nTCGGCG>2\nTCGGCG"
