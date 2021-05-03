ade8bcf417e0d1af04f780f77f667ba8066e5fda
phoenixAja <phoenix@phoenixlogan.net>
2021-05-03 10:47:56 -0700

    add basic test

--- src/backend/aspen/workflows/pangolin/tests/test_pangolin_workflow.py
+++ src/backend/aspen/workflows/pangolin/tests/test_pangolin_workflow.py
@@ -1,12 +1,16 @@
+from pathlib import Path
+
 from click.testing import CliRunner
 
+from aspen.database.models.sequences import UploadedPathogenGenome
 from aspen.test_infra.models.sample import sample_factory
 from aspen.test_infra.models.sequences import uploaded_pathogen_genome_factory
 from aspen.test_infra.models.usergroup import group_factory
-from aspen.workflows.pangolin.export import cli
+from aspen.workflows.pangolin.export import cli as export_cli
+from aspen.workflows.pangolin.save import cli as save_cli
 
 
-def test_pangolin_export(mocker, session, postgres_database):
+def create_test_data(session):
     group = group_factory()
 
     for i in range(1, 3):
@@ -20,6 +24,11 @@ def test_pangolin_export(mocker, session, postgres_database):
         session.add(pathogen_genome)
         session.commit()
 
+
+def test_pangolin_export(mocker, session, postgres_database):
+
+    create_test_data(session)
+
     mocker.patch(
         "aspen.config.config.RemoteDatabaseConfig.DATABASE_URI",
         new_callable=mocker.PropertyMock,
@@ -28,7 +37,7 @@ def test_pangolin_export(mocker, session, postgres_database):
 
     runner = CliRunner()
     result = runner.invoke(
-        cli,
+        export_cli,
         [
             "--sequences",
             "test.fa",
@@ -42,3 +51,38 @@ def test_pangolin_export(mocker, session, postgres_database):
     with open("test.fa", "r") as fh:
         lines = fh.read()
         assert lines == ">1\nTCGGCG>2\nTCGGCG"
+
+
+def test_pangolin_save(mocker, session, postgres_database):
+
+    create_test_data(session)
+
+    mocker.patch(
+        "aspen.config.config.RemoteDatabaseConfig.DATABASE_URI",
+        new_callable=mocker.PropertyMock,
+        return_value=postgres_database.as_uri(),
+    )
+
+    pangolin_csv = Path(
+        Path(__file__).parent, "data", "lineage_report.csv"
+    )
+
+    runner = CliRunner()
+    result = runner.invoke(
+        save_cli,
+        [
+            "--pangolin-csv",
+            pangolin_csv
+        ],
+    )
+    assert result.exit_code == 0
+    
+    # start new transaction
+    session.stop()
+    session.begin()
+
+    for i in range(1, 3):
+        pathogen_genome = session.query(UploadedPathogenGenome).filter(UploadedPathogenGenome.entity_id==i).one()
+        assert pathogen_genome.pangolin_lineage == "B.1.590"
+        assert pathogen_genome.pangolin_probability == 1
+
