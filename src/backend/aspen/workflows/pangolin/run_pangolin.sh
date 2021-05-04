2db1c052a066c88e5b9cafba28ee6546c033140c
phoenixAja <phoenix@phoenixlogan.net>
2021-05-04 11:50:37 -0700

    install miniconda in silent mode

--- src/backend/aspen/workflows/pangolin/run_pangolin.sh
+++ src/backend/aspen/workflows/pangolin/run_pangolin.sh
@@ -5,7 +5,7 @@ set -Eeuxo pipefail
 # install miniconda
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
-./Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
 source ~/.bashrc
 
 # install pangolin
