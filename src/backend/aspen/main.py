3cc0ead07465a4423c074a782a95521380bcc667
Tony Tung <ttung@chanzuckerberg.com>
2021-05-10 16:13:00 -0700

    presigned url link for auspice with tree rewrite
    ### Description
    Rewrite the tree data and save to S3.  Then return a presigned url to that key.
    
    Modified existing docker-compose infrastructure to set up the bucket.
    
    #### Issue
    [ch133611](https://app.clubhouse.io/genepi/story/133611/rewrite-tree-data-with-private-identifiers)
    
    ### Test plan
    Modified test to retrieve the data from the presigned url

--- src/backend/aspen/main.py
+++ src/backend/aspen/main.py
@@ -1,10 +1,9 @@
 # this is where flask finds the entry point for the application.
 
 from aspen.app.app import application  # noqa: F401
-from aspen.app.views.auspice import auspice_view  # noqa: F401
 from aspen.app.views.auth import callback_handling, login, logout  # noqa: F401
 from aspen.app.views.health import health  # noqa: F401
 from aspen.app.views.index import serve  # noqa: F401
-from aspen.app.views.phylo_trees import phylo_trees  # noqa: F401
+from aspen.app.views.phylo_trees import auspice_view, phylo_trees  # noqa: F401
 from aspen.app.views.sample import samples  # noqa: F401
 from aspen.app.views.usergroup import usergroup  # noqa: F401
