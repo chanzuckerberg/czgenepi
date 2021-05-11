01ca3e0d39759445fd7bcaae99371608de9966b2
Tony Tung <ttung@chanzuckerberg.com>
2021-05-11 10:44:28 -0700

    Don't import VRDL trees (#377)
    ### Description
    They don't exist!
    
    #### Issue
    [ch136615](https://app.clubhouse.io/genepi/story/136615/vrdl-trees-do-not-port)
    
    ### Test plan
    removed all but vrdl and tulare and ran import.

--- scripts/migrate-all.sh
+++ scripts/migrate-all.sh
@@ -16,7 +16,7 @@ COUNTY_INFO='{
         "san_luis_obispo": {"external_project_id": "RR073e", "internal_project_ids": ["RR073i"]},
         "ventura": {"external_project_id": "RR078e"},
         "humboldt": {"external_project_id": "RR075e"},
-        "vrdl": {"external_project_id": "RR096e"},
+        "vrdl": {"external_project_id": "RR096e", "skip_tree_import": true},
         "tuolumne": {"internal_project_ids": ["RR095i"]},
         "fresno": {"external_project_id": "RR097e"},
         "sfpdh": {"external_project_id": "RR083e"},
@@ -38,14 +38,14 @@ done
 
 for county in "${COUNTIES[@]}"; do
     aspen_group_id=$(echo "$COUNTY_INFO" | jq -r ".$county".aspen_group_id)
-    if $(echo "$COUNTY_INFO" | jq ".$county | has(\"internal_project_ids\")") = "true"; then
+    if [ "$(echo "$COUNTY_INFO" | jq ".$county | has(\"internal_project_ids\")")" = "true" ]; then
         echo "Importing internal samples for $county..."
         for internal_project_id in $(echo "$COUNTY_INFO" | jq -r ".$county".internal_project_ids[]); do
             aspen-cli db --local import-covidhub-project --rr-project-id "$internal_project_id" --covidhub-db-secret cliahub/cliahub_rds_read_prod  --covidhub-aws-profile biohub --aspen-group-id "$aspen_group_id"
         done
     fi
 
-    if $(echo "$COUNTY_INFO" | jq ".$county | has(\"external_project_id\")") = "true"; then
+    if [ "$(echo "$COUNTY_INFO" | jq ".$county | has(\"external_project_id\")")" = "true" ]; then
         echo "Importing external samples for $county..."
         external_project_id=$(echo "$COUNTY_INFO" | jq -r ".$county".external_project_id)
         aspen-cli db --local import-covidhub-project --rr-project-id "$external_project_id" --covidhub-db-secret cliahub/cliahub_rds_read_prod  --covidhub-aws-profile biohub --aspen-group-id "$aspen_group_id"
@@ -53,6 +53,8 @@ for county in "${COUNTIES[@]}"; do
 done
 
 for county in "${COUNTIES[@]}"; do
-    aspen_group_id=$(echo "$COUNTY_INFO" | jq -r ".$county".aspen_group_id)
-    aspen-cli db --local import-covidhub-trees --covidhub-aws-profile biohub --s3-src-prefix s3://covidtracker-datasets/cdph/"$county" --s3-key-prefix /imported/phylo_trees/"$county" --aspen-group-id "$aspen_group_id"
+    if [ "$(echo "$COUNTY_INFO" | jq ".$county.skip_tree_import")" != "true" ]; then
+        aspen_group_id=$(echo "$COUNTY_INFO" | jq -r ".$county".aspen_group_id)
+        aspen-cli db --local import-covidhub-trees --covidhub-aws-profile biohub --s3-src-prefix s3://covidtracker-datasets/cdph/"$county" --s3-key-prefix /imported/phylo_trees/"$county" --aspen-group-id "$aspen_group_id"
+    fi
 done
