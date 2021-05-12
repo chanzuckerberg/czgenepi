c62f2722aefe5afeb150e136885905f11a6327d7
phoenixAja <phoenix@phoenixlogan.net>
2021-05-12 16:23:17 -0700

    with load_only

--- src/backend/aspen/app/views/api_utils.py
+++ src/backend/aspen/app/views/api_utils.py
@@ -1,18 +1,20 @@
 import datetime
 from typing import Optional
 
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import joinedload, load_only
 from sqlalchemy.orm.query import Query
 from sqlalchemy.orm.session import Session
 
-from aspen.database.models.usergroup import User
+from aspen.database.models.usergroup import User, Group
 
 
 def get_usergroup_query(session: Session, user_id: str) -> Query:
     return (
         session.query(User)
-        .options(joinedload(User.group))
-        .filter(User.auth0_user_id == user_id)
+            .options(
+            load_only("auth0_user_id", "agreed_to_tos"),
+            joinedload(User.group).load_only("name")
+        ).filter(User.auth0_user_id == user_id)
     )
 
 
