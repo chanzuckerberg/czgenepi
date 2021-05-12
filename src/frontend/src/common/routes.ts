d83585ed393b184ff17ea92b647164d3e27a48e0
Shannon Axelrod <shannon.axelrod@chanzuckerberg.com>
2021-05-12 10:47:40 -0700

    updating the contact us email

--- src/frontend/src/common/routes.ts
+++ src/frontend/src/common/routes.ts
@@ -3,7 +3,7 @@ export enum ROUTES {
   DATA = "/data",
   TERMS = "/terms",
   PRIVACY = "/privacy",
-  CONTACT_US_EMAIL = "mailto:covidtracker-data-requests-ext@chanzuckerberg.com",
+  CONTACT_US_EMAIL = "mailto:helloaspen@chanzuckerberg.com",
   AGREE_TERMS = "/agreeTerms",
   FAQ = "/faq",
 }
