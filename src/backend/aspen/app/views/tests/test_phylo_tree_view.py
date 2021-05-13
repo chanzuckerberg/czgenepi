a8a1a1e08a3ae077076e1174bfdc9e417f73eca9
phoenixAja <phoenix@phoenixlogan.net>
2021-05-13 10:42:30 -0700

    phylotree test with cansee

--- src/backend/aspen/app/views/tests/test_phylo_tree_view.py
+++ src/backend/aspen/app/views/tests/test_phylo_tree_view.py
@@ -2,6 +2,10 @@ import json
 import random
 
 from aspen.app.views.phylo_trees import PHYLO_TREE_KEY
+from aspen.database.models import (
+    CanSee,
+    DataType,
+)
 from aspen.test_infra.models.phylo_tree import phylorun_factory, phylotree_factory
 from aspen.test_infra.models.sample import sample_factory
 from aspen.test_infra.models.sequences import uploaded_pathogen_genome_factory
@@ -40,6 +44,54 @@ def test_phylo_tree_view(
     session.commit()
     with client.session_transaction() as sess:
         sess["profile"] = {"name": user.name, "user_id": user.auth0_user_id}
+    results = json.loads(client.get("/api/phylo_trees").get_data(as_text=True))
+
+    for tree in trees:
+        for result_tree in results[PHYLO_TREE_KEY]:
+            if tree.entity_id == result_tree["phylo_tree_id"]:
+                # found it!
+                break
+        else:
+            raise ValueError(f"Could not find {tree} in results")
+
+        assert result_tree["pathogen_genome_count"] == len(tree.constituent_samples)
+
+
+def test_phylo_trees_can_see(session, client):
+    owner_group = group_factory()
+    viewer_group = group_factory("CADPH")
+    user = user_factory(viewer_group)
+    samples = [
+        sample_factory(
+            owner_group,
+            public_identifier=f"public_identifier_{ix}",
+            private_identifier=f"private_identifier_{ix}",
+        )
+        for ix in range(5)
+    ]
+    _ = [uploaded_pathogen_genome_factory(sample, accessions=()) for sample in samples]
+
+    # make up to 3 trees, each with a random sample of uploaded pathogen genomes.
+    trees = [
+        phylotree_factory(
+            phylorun_factory(owner_group),
+            random.sample(samples, k=random.randint(0, len(samples))),
+            key=f"key_{ix}",
+        )
+        for ix in range(5)
+    ]
+
+    CanSee(
+        viewer_group=viewer_group,
+        owner_group=owner_group,
+        data_type=DataType.TREES
+    )
+    session.add_all((owner_group, viewer_group))
+    session.commit()
+
+    with client.session_transaction() as sess:
+        sess["profile"] = {"name": user.name, "user_id": user.auth0_user_id}
+
     results = json.loads(client.get("/api/phylo_trees").get_data(as_text=True))
 
     for tree in trees:
