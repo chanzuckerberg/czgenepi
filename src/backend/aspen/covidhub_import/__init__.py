b62e5dc29c18c1b2057b2c69241b1a9a57fa1dba
Tony Tung <ttung@chanzuckerberg.com>
2021-05-13 13:59:16 -0700

    Refactor a generic utils module for shared code (#387)
    ### Description
    `covidhub_interface_from_secret` is already used in multiple places.  `get_or_make_group` and `retrieve_auth0_users` will be used by `import_users.py` and the future admin import.
    
    #### Issue
    [ch135517](https://app.clubhouse.io/genepi/story/135517/port-over-account-info-for-admin-czb-czi-users)
    
    ### Test plan
    mechanical refactor.

--- src/backend/aspen/covidhub_import/__init__.py
+++ src/backend/aspen/covidhub_import/__init__.py
@@ -5,4 +5,5 @@ except ImportError:
 else:
     from .import_projects import import_project  # noqa: F401
     from .import_trees import import_trees  # noqa: F401
-    from .import_users import import_project_users, retrieve_auth0_users  # noqa: F401
+    from .import_users import import_project_users  # noqa: F401
+    from .utils import retrieve_auth0_users  # noqa: F401
