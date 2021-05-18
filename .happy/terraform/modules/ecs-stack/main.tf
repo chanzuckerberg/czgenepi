0cdec106cc8a17a517b59b714f9101b5c68e71c0
Tony Tung <ttung@chanzuckerberg.com>
2021-05-18 11:46:57 -0700

    Set up nextstrain runs for scc (#451)
    ### Description
    I plan to do this for all the counties we want to generate nightly trees, but want to try this first.
    
    ### Test plan
    deploy and test?

--- .happy/terraform/modules/ecs-stack/main.tf
+++ .happy/terraform/modules/ecs-stack/main.tf
@@ -209,7 +209,7 @@ module pangolin_sfn_config {
   event_role_arn        = local.ecs_role_arn
 }
 
-module nextstrain_sfn_config {
+module nextstrain_template_sfn_config {
   source   = "../sfn_config"
   app_name = "nextstrain-sfn"
   image    = "${local.nextstrain_image_repo}:${local.image_tag}"
@@ -221,9 +221,67 @@ module nextstrain_sfn_config {
   stack_resource_prefix = local.stack_resource_prefix
   swipe_comms_bucket    = local.swipe_comms_bucket
   swipe_wdl_bucket      = local.swipe_wdl_bucket
-  extra_args            =  {
+  sfn_arn               = module.swipe_sfn.step_function_arn
+  event_role_arn        = local.ecs_role_arn
+   extra_args            =  {
     aspen_config_secret_name = "${local.deployment_stage}/aspen-config"
-    remote_dev_prefix = local.remote_dev_prefix
+    remote_dev_prefix        = local.remote_dev_prefix
+  }
+}
+
+module nextstrain_scc_local_sfn_config {
+  source   = "../sfn_config"
+  app_name = "nextstrain-scc-local-sfn"
+  image    = "${local.nextstrain_image_repo}:${local.image_tag}"
+  memory   = 400000
+  wdl_path = "workflows/nextstrain.wdl"
+  custom_stack_name     = local.custom_stack_name
+  deployment_stage      = local.deployment_stage
+  remote_dev_prefix     = local.remote_dev_prefix
+  stack_resource_prefix = local.stack_resource_prefix
+  swipe_comms_bucket    = local.swipe_comms_bucket
+  swipe_wdl_bucket      = local.swipe_wdl_bucket
+  sfn_arn               = module.swipe_sfn.step_function_arn
+  schedule_expressions  = ["prod", "staging"].contains(local.deployment_stage) ? ["cron(0 0 ? * 1-5 *)"] : []
+  event_role_arn        = local.ecs_role_arn
+   extra_args            =  {
+    aspen_config_secret_name = "${local.deployment_stage}/aspen-config"
+    remote_dev_prefix        = local.remote_dev_prefix
+    group_name               = "Santa Clara County Public Health"
+    s3_filestem              = "Santa Clara Local"
+    template_filename        = "group.yaml"
+    template_args            = {
+     division = "California"
+     location = "Santa Clara County"
+    }
+  }
+}
+
+module nextstrain_scc_contextual_sfn_config {
+  source   = "../sfn_config"
+  app_name = "nextstrain-scc-contextual-sfn"
+  image    = "${local.nextstrain_image_repo}:${local.image_tag}"
+  memory   = 400000
+  wdl_path = "workflows/nextstrain.wdl"
+  custom_stack_name     = local.custom_stack_name
+  deployment_stage      = local.deployment_stage
+  remote_dev_prefix     = local.remote_dev_prefix
+  stack_resource_prefix = local.stack_resource_prefix
+  swipe_comms_bucket    = local.swipe_comms_bucket
+  swipe_wdl_bucket      = local.swipe_wdl_bucket
+  sfn_arn               = module.swipe_sfn.step_function_arn
+  schedule_expressions  = ["prod", "staging"].contains(local.deployment_stage) ? ["cron(0 0 ? * 1-5 *)"] : []
+  event_role_arn        = local.ecs_role_arn
+   extra_args            =  {
+    aspen_config_secret_name = "${local.deployment_stage}/aspen-config"
+    remote_dev_prefix        = local.remote_dev_prefix
+    group_name               = "Santa Clara County Public Health"
+    s3_filestem              = "Santa Clara Contextual"
+    template_filename        = "group_plus_context.yaml"
+    template_args            = {
+     division = "California"
+     location = "Santa Clara County"
+    }
   }
 }
 
