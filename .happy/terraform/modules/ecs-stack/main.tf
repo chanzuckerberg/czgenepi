4014ac09f0765825cd34fbbc56985e0260bfdcb2
Tony Tung <ttung@chanzuckerberg.com>
2021-05-18 16:27:57 -0700

    Cut memory ask for nextstrain runs
    ### Description
    It only needs 32GB, in my experience.
    
    ### Test plan
    Kicked off a [run](https://us-west-2.console.aws.amazon.com/states/home?region=us-west-2#/executions/details/arn:aws:states:us-west-2:473004499091:execution:aspen-staging-stagingstack-swipe-spot-sfn:scc-contextual-32gb)

--- .happy/terraform/modules/ecs-stack/main.tf
+++ .happy/terraform/modules/ecs-stack/main.tf
@@ -213,7 +213,7 @@ module nextstrain_template_sfn_config {
   source   = "../sfn_config"
   app_name = "nextstrain-sfn"
   image    = "${local.nextstrain_image_repo}:${local.image_tag}"
-  memory   = 400000
+  memory   = 32000
   wdl_path = "workflows/nextstrain.wdl"
   custom_stack_name     = local.custom_stack_name
   deployment_stage      = local.deployment_stage
