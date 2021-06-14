b8bba2b14752b533c07093890432c969145cfa9a
Timmy Huang <thuang@chanzuckerberg.com>
2021-06-14 10:54:16 -0700

    upload-119808

--- src/frontend/jest/config.js
+++ src/frontend/jest/config.js
@@ -3,6 +3,7 @@ const config = require(__dirname + "/common.config.js");
 module.exports = {
   ...config,
   setupFilesAfterEnv: ["<rootDir>/jest/setup.ts"],
+  testEnvironment: "jsdom",
   testMatch: ["<rootDir>/**/**/*.{spec,test}.{js,jsx,ts,tsx,scss}"],
   testPathIgnorePatterns: [
     ...config.testPathIgnorePatterns,
