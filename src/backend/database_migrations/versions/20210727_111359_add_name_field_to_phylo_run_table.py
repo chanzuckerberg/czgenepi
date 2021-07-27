a358f8c0f17ac80110cf11d85c27951946a0fc31
lvreynoso <lreynoso@chanzuckerberg.com>
2021-07-27 11:27:08 -0700

    Add alembic migration - add name to phylo run

---
+++ src/backend/database_migrations/versions/20210727_111359_add_name_field_to_phylo_run_table.py
@@ -0,0 +1,26 @@
+"""Add name field to phylo run table
+
+Create Date: 2021-07-27 11:13:59.925534
+
+"""
+import enumtables  # noqa: F401
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "20210727_111359"
+down_revision = "20210617_201052"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        "phylo_runs",
+        sa.Column("name", sa.String(), nullable=True),
+        schema="aspen",
+    )
+
+
+def downgrade():
+    op.drop_column("phylo_runs", "name", schema="aspen")
