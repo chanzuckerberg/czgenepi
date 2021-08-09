491a4432421762d13a2a2ef37a06a01cf93dffdf
Jessica Gadling <jgadling@chanzuckerberg.com>
2021-08-09 13:04:56 -0700

    Support device authorization flow in local dev. (#624)

--- src/backend/aspen/app/app.py
+++ src/backend/aspen/app/app.py
@@ -1,11 +1,14 @@
 import os
+import time
 from functools import wraps
 from pathlib import Path
 from typing import Optional
 
+import requests
 import sentry_sdk
 from auth0.v3.authentication.token_verifier import (
     AsymmetricSignatureVerifier,
+    JwksFetcher,
     TokenVerifier,
 )
 from auth0.v3.exceptions import TokenValidationError
@@ -81,6 +84,28 @@ auth0 = oauth.register(
 )
 
 
+class InsecureJwksFetcher(JwksFetcher):
+    def _fetch_jwks(self, force=False):
+        has_expired = self._cache_date + self._cache_ttl < time.time()
+
+        if not force and not has_expired:
+            # Return from cache
+            self._cache_is_fresh = False
+            return self._cache_value
+
+        # Invalidate cache and fetch fresh data
+        self._cache_value = {}
+        response = requests.get(self._jwks_url, verify=False)
+
+        if response.ok:
+            # Update cache
+            jwks = response.json()
+            self._cache_value = self._parse_jwks(jwks)
+            self._cache_is_fresh = True
+            self._cache_date = time.time()
+        return self._cache_value
+
+
 def validate_auth_header(auth_header):
     parts = auth_header.split()
 
@@ -96,11 +121,18 @@ def validate_auth_header(auth_header):
     domain = application.aspen_config.AUTH0_DOMAIN
     client_id = application.aspen_config.AUTH0_CLIENT_ID
 
-    jwks_url = f"https://{domain}/.well-known/jwks.json"
-    issuer = f"https://{domain}/"
+    # TODO, this should probably be a part of aspen config.
+    if "genepinet.local" in domain:
+        jwks_url = f"https://{domain}/.well-known/openid-configuration/jwks"
+        issuer = f"https://{domain}"
+        # Adapted from https://github.com/auth0/auth0-python#id-token-validation
+        sv = AsymmetricSignatureVerifier(jwks_url)
+        sv._fetcher = InsecureJwksFetcher(jwks_url)
+    else:
+        jwks_url = f"https://{domain}/.well-known/jwks.json"
+        issuer = f"https://{domain}/"
+        sv = AsymmetricSignatureVerifier(jwks_url)
 
-    # Adapted from https://github.com/auth0/auth0-python#id-token-validation
-    sv = AsymmetricSignatureVerifier(jwks_url)  # Reusable instance
     payload = sv.verify_signature(id_token)
     tv = TokenVerifier(signature_verifier=sv, issuer=issuer, audience=client_id)
     tv._verify_payload(payload)
