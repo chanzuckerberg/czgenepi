1ab3e655442536e87c17748774a88862a6977cca
Phoenix Logan <phoenix@phoenixlogan.net>
2021-08-18 09:48:17 -0700

    tree build button/ modal / api request (#638)

---
+++ src/frontend/src/common/components/library/data_subview/components/IconButton/style.ts
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+import { getSpacings } from "czifui";
+
+export const StyledSpan = styled.span`
+  display: flex;
+  ${(props) => {
+    const spacings = getSpacings(props);
+    return `
+      margin-left: ${spacings?.m}px;
+    `;
+  }}
+`;
