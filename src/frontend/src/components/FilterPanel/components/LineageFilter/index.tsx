553c69e63152191a0d77f14c363ddabda49f80c9
maya <mlila@chanzuckerberg.com>
2021-08-30 10:55:54 -0700

    [ch150285] lineage filter (#648)
    * use new version of czifui in order to pull in menuselect
    
    * new lineage filter component
    
    * add multi filter type to filter panel; add lineage filter logic
    
    * determine lineage list from data
    
    * filter by lineage
    
    * types
    
    * move component to proper location

---
+++ src/frontend/src/components/FilterPanel/components/LineageFilter/index.tsx
@@ -0,0 +1,121 @@
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Popper from "@material-ui/core/Popper";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { AutocompleteCloseReason } from "@material-ui/lab";
+import { Chip, MenuSelect } from "czifui";
+import React, { useEffect, useState } from "react";
+import { DefaultMenuSelectOption } from "../../index";
+
+interface Props {
+  options: DefaultMenuSelectOption[];
+  updateLineageFilter: (lineages: string[]) => void;
+}
+
+const LineageFilter = (props: Props): JSX.Element => {
+  const { options = [], updateLineageFilter } = props;
+
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const [value, setValue] = useState<DefaultMenuSelectOption[] | undefined>([]);
+  const [pendingValue, setPendingValue] = useState<
+    DefaultMenuSelectOption[] | undefined
+  >([]);
+
+  useEffect(() => {
+    if (value) {
+      updateLineageFilter(value.map((d) => d.name));
+    }
+  }, [updateLineageFilter, value]);
+
+  const open = Boolean(anchorEl);
+  const id = open ? "lineage-filter" : undefined;
+
+  return (
+    <>
+      <div>
+        <ButtonBase disableRipple aria-describedby={id} onClick={handleClick}>
+          <span>Lineage</span>
+          <ExpandMoreIcon />
+        </ButtonBase>
+
+        <Chips value={value} onDelete={handleDelete} />
+      </div>
+      <Popper id={id} open={open} anchorEl={anchorEl}>
+        <MenuSelect
+          disableCloseOnSelect
+          multiple
+          open
+          search
+          onClose={handleClose}
+          value={pendingValue}
+          onChange={handleChange}
+          options={options}
+        />
+      </Popper>
+    </>
+  );
+
+  function handleClick(event: React.MouseEvent<HTMLElement>) {
+    setPendingValue(value as DefaultMenuSelectOption[]);
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose(
+    _: React.ChangeEvent<unknown>,
+    reason: AutocompleteCloseReason
+  ) {
+    if (reason === "toggleInput") {
+      return;
+    }
+
+    setValue(pendingValue);
+
+    if (anchorEl) {
+      anchorEl.focus();
+    }
+
+    setAnchorEl(null);
+  }
+
+  function handleChange(
+    _: React.ChangeEvent<unknown>,
+    newValue: DefaultMenuSelectOption[] | undefined
+  ) {
+    return setPendingValue(newValue as DefaultMenuSelectOption[]);
+  }
+
+  function handleDelete(option: DefaultMenuSelectOption) {
+    const newValue = (value as DefaultMenuSelectOption[]).filter(
+      (item) => item !== option
+    );
+
+    setValue(newValue);
+  }
+};
+
+interface ChipsProps {
+  value?: DefaultMenuSelectOption[];
+  onDelete: (option: DefaultMenuSelectOption) => void;
+}
+
+function Chips({ value, onDelete }: ChipsProps): JSX.Element | null {
+  if (!value) return null;
+
+  return (
+    <div>
+      {(value as DefaultMenuSelectOption[]).map((item) => {
+        const { name } = item;
+
+        return (
+          <Chip
+            size="medium"
+            key={name}
+            label={name}
+            onDelete={() => onDelete(item)}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export { LineageFilter };
--- src/frontend/src/components/FilterPanel/index.tsx
+++ src/frontend/src/components/FilterPanel/index.tsx
@@ -1,4 +1,4 @@
-import { filter, forEach } from "lodash";
+import { filter, forEach, isEqual } from "lodash";
 import React, {
   Dispatch,
   FC,
@@ -7,6 +7,7 @@ import React, {
   useState,
 } from "react";
 import { CollectionDateFilter } from "./components/CollectionDateFilter";
+import { LineageFilter } from "./components/LineageFilter";
 import { UploadDateFilter } from "./components/UploadDateFilter";
 import { StyledFilterPanel } from "./style";
 
@@ -16,7 +17,12 @@ enum TypeFilterType {
   Single = "single",
   Multiple = "multiple",
 }
+export interface DefaultMenuSelectOption {
+  name: string;
+}
+
 interface Props {
+  lineages: DefaultMenuSelectOption[];
   setDataFilterFunc: Dispatch<
     SetStateAction<(data: TableItem[]) => TableItem[]>
   >;
@@ -24,11 +30,13 @@ interface Props {
 
 interface FilterParamsType {
   end?: DateType;
+  selected?: string[];
   start?: DateType;
 }
 interface FilterType {
   key: string;
   params: FilterParamsType;
+  transform?: (d: any) => any;
   type: TypeFilterType;
 }
 
@@ -46,6 +54,14 @@ const DATA_FILTER_INIT = {
     },
     type: TypeFilterType.Date,
   },
+  lineage: {
+    key: "lineage",
+    params: {
+      selected: [],
+    },
+    transform: (d: Sample) => d.lineage?.lineage,
+    type: TypeFilterType.Multiple,
+  },
   uploadDate: {
     key: "uploadDate",
     params: {
@@ -59,26 +75,32 @@ const DATA_FILTER_INIT = {
 const applyFilter = (data: TableItem[], dataFilter: FilterType) => {
   if (!data) return [];
 
-  const { key, params, type } = dataFilter;
+  const { key, params, transform, type } = dataFilter;
   if (!key || !params || !type) return data;
+  const { end, start, selected = [] } = params;
 
   switch (type) {
     case TypeFilterType.Date:
       return filter(data, (d) => {
-        const doesPassFilterCheckStart =
-          !params.start || d[key] >= params.start;
-        const doesPassFilterCheckEnd = !params.end || d[key] <= params.end;
+        const doesPassFilterCheckStart = !start || d[key] >= start;
+        const doesPassFilterCheckEnd = !end || d[key] <= end;
 
         return doesPassFilterCheckStart && doesPassFilterCheckEnd;
       });
-    case "single":
-    case "multiple":
+    case TypeFilterType.Multiple:
+      if (selected.length === 0) return data;
+
+      return filter(data, (d) => {
+        const value = transform ? transform(d) : d;
+        return selected.includes(value);
+      });
+    case TypeFilterType.Single:
     default:
       return data;
   }
 };
 
-const FilterPanel: FC<Props> = ({ setDataFilterFunc }) => {
+const FilterPanel: FC<Props> = ({ lineages, setDataFilterFunc }) => {
   const [dataFilters, setDataFilters] = useState<FiltersType>(DATA_FILTER_INIT);
 
   useEffect(() => {
@@ -99,12 +121,13 @@ const FilterPanel: FC<Props> = ({ setDataFilterFunc }) => {
   }, [dataFilters, setDataFilterFunc]);
 
   const updateDataFilter = (filterKey: string, params: FilterParamsType) => {
-    const type = dataFilters[filterKey]?.type as TypeFilterType;
+    const { transform, type } = dataFilters[filterKey];
     const newFilters = {
       ...dataFilters,
       [filterKey]: {
         key: filterKey,
         params,
+        transform,
         type,
       },
     };
@@ -120,12 +143,27 @@ const FilterPanel: FC<Props> = ({ setDataFilterFunc }) => {
     updateDataFilter("uploadDate", { end, start });
   };
 
+  const updateLineageFilter = (selected: string[]) => {
+    const prevSelected = dataFilters.lineage?.params.selected;
+
+    // * (mlila): need to do a comparison here, or else the component gets into
+    // * an infinite state loop (because arrays are compared by identity rather
+    // * than content, by default)
+    if (!isEqual(prevSelected, selected)) {
+      updateDataFilter("lineage", { selected });
+    }
+  };
+
   return (
     <StyledFilterPanel>
       <CollectionDateFilter
         updateCollectionDateFilter={updateCollectionDateFilter}
       />
       <UploadDateFilter updateUploadDateFilter={updateUploadDateFilter} />
+      <LineageFilter
+        options={lineages}
+        updateLineageFilter={updateLineageFilter}
+      />
     </StyledFilterPanel>
   );
 };
