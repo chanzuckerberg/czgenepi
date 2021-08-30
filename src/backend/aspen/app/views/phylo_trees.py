c613093a92e8f900362e40c2a441ddb3cad6d2e2
lvreynoso <lreynoso@chanzuckerberg.com>
2021-08-30 11:04:19 -0700

    Add proper output for trees not yet completed

--- src/backend/aspen/app/views/phylo_trees.py
+++ src/backend/aspen/app/views/phylo_trees.py
@@ -11,7 +11,7 @@ from sqlalchemy import or_
 from sqlalchemy.orm import aliased, joinedload, Session
 
 from aspen.app.app import application, requires_auth
-from aspen.app.views.api_utils import format_datetime
+from aspen.app.views.api_utils import format_datetime, format_date
 from aspen.database.models import (
     DataType,
     PhyloRun,
@@ -39,6 +39,12 @@ def humanize_tree_name(s3_key: str):
     return title_case
 
 
+def generate_tree_name_from_template(phylo_run: PhyloRun) -> str:
+    template_data = json.loads(phylo_run.template_args)
+    if template_data['run_name']:
+        return template_data['run_name']
+    return f"{phylo_run.group.name} Tree {format_date(phylo_run.start_datetime)}"
+
 @application.route("/api/phylo_trees", methods=["GET"])
 @requires_auth
 def phylo_trees():
@@ -54,7 +60,6 @@ def phylo_trees():
             phylo_run_alias,
         )
         .options(
-            joinedload(phylo_run_alias.outputs),
             joinedload(phylo_run_alias.outputs.of_type(PhyloTree)),
         )
         .filter(
@@ -64,30 +69,32 @@ def phylo_trees():
                 phylo_run_alias.group_id.in_(cansee_owner_group_ids),
             )
         )
-        .filter(phylo_run_alias.workflow_status == WorkflowStatusType.COMPLETED)
     )
 
     # filter for only information we need in sample table view
     results: MutableSequence[Mapping[str, Any]] = list()
     for phylo_run in phylo_runs:
-        phylo_tree: PhyloTree
+        phylo_tree: Optional[PhyloTree] = None
+        result: Mapping[str, Any] = {}
         for output in phylo_run.outputs:
             if isinstance(output, PhyloTree):
                 phylo_tree = output
-                break
-        else:
-            raise RecoverableError(
-                f"phylo run (workflow id={phylo_run.workflow_id}) does not have a"
-                " phylo tree output."
-            )
-        results.append(
-            {
-                "phylo_tree_id": phylo_tree.entity_id,
-                "name": humanize_tree_name(phylo_tree.s3_key),
-                "pathogen_genome_count": 0,  # Leaving this field in place temporarily for reverse-compatibility
-                "completed_date": format_datetime(phylo_run.end_datetime),
-            }
-        )
+                result = result | {
+                        "phylo_tree_id": phylo_tree.entity_id,
+                        "name": phylo_tree.name or humanize_tree_name(phylo_tree.s3_key),
+                    }
+        if not phylo_tree:
+            result = result | {
+                    "phylo_tree_id": None,
+                    "name": phylo_run.name or generate_tree_name_from_template(phylo_run),
+                }
+        result = result | {
+                    "started_date": format_datetime(phylo_run.start_datetime),
+                    "completed_date": format_datetime(phylo_run.end_datetime),
+                    "status": phylo_run.workflow_status.value,
+                    "workflow_id": phylo_run.workflow_id,
+                }
+        results.append(result)
 
     return jsonify({PHYLO_TREE_KEY: results})
 
