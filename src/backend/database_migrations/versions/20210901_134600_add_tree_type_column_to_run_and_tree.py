7161cf65987234bce2f99f9b60e0d16fc374ec03
lvreynoso <lreynoso@chanzuckerberg.com>
2021-09-02 12:19:27 -0700

    Add backfill code to migration

--- src/backend/database_migrations/versions/20210901_134600_add_tree_type_column_to_run_and_tree.py
+++ src/backend/database_migrations/versions/20210901_134600_add_tree_type_column_to_run_and_tree.py
@@ -21,14 +21,19 @@ def upgrade():
         sa.PrimaryKeyConstraint("item_id", name=op.f("pk_tree_types")),
         schema="aspen",
     )
+    op.enum_insert(
+        "tree_types",
+        ["OVERVIEW", "TARGETED", "NON_CONTEXTUALIZED", "UNKNOWN"],
+        schema="aspen",
+    )
     op.add_column(
         "phylo_runs",
-        sa.Column("tree_type", enumtables.enum_column.EnumType(), nullable=False),
+        sa.Column("tree_type", enumtables.enum_column.EnumType(), nullable=False, server_default="UNKNOWN"),
         schema="aspen",
     )
     op.add_column(
         "phylo_trees",
-        sa.Column("tree_type", enumtables.enum_column.EnumType(), nullable=False),
+        sa.Column("tree_type", enumtables.enum_column.EnumType(), nullable=False, server_default="UNKNOWN"),
         schema="aspen",
     )
     op.create_foreign_key(
@@ -49,21 +54,32 @@ def upgrade():
         source_schema="aspen",
         referent_schema="aspen",
     )
-    op.enum_insert(
-        "tree_types",
-        ["OVERVIEW", "TARGETED", "NON_CONTEXTUALIZED"],
-        schema="aspen",
-    )
 
+    # update the trees
     ancestors_to_overview_sql = sa.sql.text(
-        "UPDATE aspen.phylo_trees SET tree_type = 'OVERVIEW' WHERE phylo_trees.s3_key LIKE '%ancestors%';"
+        "UPDATE aspen.phylo_trees SET tree_type = 'OVERVIEW' WHERE phylo_trees.s3_key LIKE '%_ancestors_%' OR phylo_trees.s3_key LIKE '% Contextual.json%'"
     )
     local_to_non_contextualized_sql = sa.sql.text(
-        "UPDATE aspen.phylo_trees SET tree_type = 'NON_CONTEXTUALIZED' WHERE phylo_trees.s3_key LIKE '%local%';"
+        "UPDATE aspen.phylo_trees SET tree_type = 'NON_CONTEXTUALIZED' WHERE phylo_trees.s3_key LIKE '%_local_%' OR phylo_trees.s3_key LIKE '% Local.json%'"
     )
     conn = op.get_bind()
     conn.execute(ancestors_to_overview_sql)
     conn.execute(local_to_non_contextualized_sql)
+
+    # update the runs with phylo trees
+    copy_tree_tree_type_to_run_sql = sa.sql.text(
+        "UPDATE aspen.phylo_runs SET tree_type = subquery.tree_type FROM (SELECT producing_workflow_id, tree_type FROM aspen.entities INNER JOIN aspen.phylo_trees ON entities.id = phylo_trees.entity_id) AS subquery WHERE phylo_runs.workflow_id = subquery.producing_workflow_id"
+    )
+    # update remaining runs without phylo trees but with some contextual information
+    assign_overview_to_group_plus_context_sql = sa.sql.text(
+        "UPDATE aspen.phylo_runs SET tree_type = 'OVERVIEW' WHERE phylo_runs.tree_type = 'UNKNOWN' AND phylo_runs.template_file_path LIKE '%group_plus_context.yaml%';"
+    )
+    assign_non_contextualized_to_group_sql = sa.sql.text(
+        "UPDATE aspen.phylo_runs SET tree_type = 'NON_CONTEXTUALIZED' WHERE phylo_runs.tree_type = 'UNKNOWN' AND phylo_runs.template_file_path LIKE '%group.yaml%';"
+    )
+    conn.execute(copy_tree_tree_type_to_run_sql)
+    conn.execute(assign_overview_to_group_plus_context_sql)
+    conn.execute(assign_non_contextualized_to_group_sql)
 
 
 def downgrade():
