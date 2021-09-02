88e4686475d0072bedf2494637ecf3469cb87425
maya <mlila@chanzuckerberg.com>
2021-09-02 10:46:53 -0700

    Maya/filter styles (#655)

--- src/frontend/src/views/Data/components/FilterPanelToggle/index.tsx
+++ src/frontend/src/views/Data/components/FilterPanelToggle/index.tsx
@@ -3,14 +3,18 @@ import { IconButtonBubble } from "src/common/styles/support/style";
 import { StyledDiv, StyledIconFilters } from "./style";
 
 interface Props {
+  activeFilterCount: number;
   onClick: () => void;
 }
 
-export const FilterPanelToggle: FC<Props> = ({ onClick }): JSX.Element => {
+export const FilterPanelToggle: FC<Props> = ({
+  activeFilterCount,
+  onClick,
+}): JSX.Element => {
   return (
     <StyledDiv>
       <IconButtonBubble onClick={onClick}>
-        <StyledIconFilters />
+        <StyledIconFilters /> {activeFilterCount}
       </IconButtonBubble>
     </StyledDiv>
   );
