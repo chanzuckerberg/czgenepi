dd1286b810b3bbb1bb25c66ddc19007dff77ed74
Phoenix Logan <phoenix@phoenixlogan.net>
2021-09-13 15:50:14 -0700

    👹 fix guide on tree type link 👹 (#666)

--- src/frontend/src/common/components/library/data_subview/components/createTreeModal/index.tsx
+++ src/frontend/src/common/components/library/data_subview/components/createTreeModal/index.tsx
@@ -184,6 +184,7 @@ export const CreateTreeModal = ({
                 <FieldTitle>Tree Type: </FieldTitle>
                 <StyledTooltip
                   arrow
+                  leaveDelay={1000}
                   title={TREE_TYPE_TOOLTIP_TEXT}
                   placement="top"
                 >
