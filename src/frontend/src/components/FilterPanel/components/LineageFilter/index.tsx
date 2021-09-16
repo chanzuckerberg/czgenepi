abe183cbf40f4aa607858dd4a0b3ac429073cf33
vincent-czi <89553795+vincent-czi@users.noreply.github.com>
2021-09-16 11:48:14 -0700

    Fix selections in multi-select for lineage filtering  (#670)
    Co-authored-by: Timmy Huang <tihuan@users.noreply.github.com>

--- src/frontend/src/components/FilterPanel/components/LineageFilter/index.tsx
+++ src/frontend/src/components/FilterPanel/components/LineageFilter/index.tsx
@@ -1,32 +1,48 @@
-import { ComplexFilter } from "czifui";
+import {
+  ComplexFilter,
+  ComplexFilterValue,
+  DefaultMenuSelectOption,
+} from "czifui";
 import React, { useEffect, useState } from "react";
-import { DefaultMenuSelectOption } from "../../index";
 
 interface Props {
   options: DefaultMenuSelectOption[];
   updateLineageFilter: (lineages: string[]) => void;
 }
 
+// ComplexFilter defaults to checking if option is selected (is a value) by equality.
+// However, because our options are objects that are dynamically generated, object
+// equality is always false because they're not the same object, even if same content.
+// Instead, we must create a custom selection checker to check underlying value.
+const getOptionSelected = (
+  option: DefaultMenuSelectOption,
+  value: DefaultMenuSelectOption
+) => {
+  return option.name === value.name;
+};
+// ComplexFilter doesn't directly do the check, it's done by its child MenuSelect
+const optionCheckingMenuSelectProps = {
+  getOptionSelected,
+};
+
 const LineageFilter = (props: Props): JSX.Element => {
   const { options = [], updateLineageFilter } = props;
-
-  // FIXME (Vince) -- TypeScript has slayed me. Or maybe the intereaction with czifui?
-  // Types break because the `onChange` value must accept single DefaultMenuSelectOption
-  // and null as possible input args. But if we do that, then `value` is implied to possibly
-  // be those, which it can't be because of required `.map` below.
-  // Not sure how to go about fixing this. Probably some forced assertion? But where?
-  const [value, setValue] = useState<DefaultMenuSelectOption[]>([]);
+  const [value, setValue] = useState<ComplexFilterValue>([]);
 
   useEffect(() => {
     if (value) {
-      updateLineageFilter(value.map((d: DefaultMenuSelectOption) => d.name));
+      updateLineageFilter(
+        (value as DefaultMenuSelectOption[]).map((d) => d.name)
+      );
     }
   }, [updateLineageFilter, value]);
 
+  // TODO -- With czifui 0.0.55, the `InputDropdownComponent` was exposed so it
+  // can be directly styled and passed in as we do with others. Once we've upgraded
+  // to 0.0.55+, this should be swapped over so it matches the approach we do elsewhere.
   // (vince) To tweak the internal style of dropdown in ComplexFilter, need to
   // create a specialized set of props to insert CSS via raw `style` put into
-  // underlying HTML tag.
-  // TODO -- This approach is very different. Talk to SDS team about ways to avoid.
+  // underlying HTML tag. [As of czifui 0.0.53. See above for newer.]
   const PROPS_FOR_INPUT_DROPDOWN = {
     sdsStyle: "minimal", // Would be defaulted, but must set everything now.
     // This `style` gets directly put in as HTML style and interpolated to CSS.
@@ -36,17 +52,12 @@ const LineageFilter = (props: Props): JSX.Element => {
     },
   } as const;
 
-  // TODO (vince) -- BUG -- Need to talk to Timmy
-  // The ComplexFilter is not displaying previously selected options as I'd expect.
-  // It shouldn't need an explicit `value` passed in (this would make it isControlled
-  // in the czifui ComplexFilter interally), the non-controlled versions in Storybook
-  // maintain state of selected options on open and close. But here it's not...
   return (
     <ComplexFilter
       label="Lineage"
       options={options}
-      /* @ts-expect-error -- TODO look into czifui ComplexFilter type for onChange*/
       onChange={setValue}
+      MenuSelectProps={optionCheckingMenuSelectProps}
       multiple
       search
       InputDropdownProps={PROPS_FOR_INPUT_DROPDOWN}
