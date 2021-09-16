3c4f415fa0d75acab1ad15051d50083bb2c1ebe0
lvreynoso <lreynoso@chanzuckerberg.com>
2021-09-16 16:40:16 -0700

    Properly install & update pangolin

--- src/backend/aspen/workflows/pangolin/install_pangolin.sh
+++ src/backend/aspen/workflows/pangolin/install_pangolin.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# activate miniconda
 eval "$($HOME/miniconda/bin/conda shell.bash hook)"
 conda init
+mkdir /pangolin
 cd /pangolin
+git clone git://github.com/cov-lineages/pangolin.git --depth 1 .
+conda env create -f environment.yml
 conda activate pangolin
-git pull
-conda env update -f environment.yml
 pip install .
 
 # check pangolin is present:
