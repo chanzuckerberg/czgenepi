d579cfe68b8e6117121010d6555abc96923aacfc
maya <mlila@chanzuckerberg.com>
2021-09-17 17:52:39 -0700

    [ch153503] phylo tree help link (#675)
    * add icons
    
    * add help link to tree page
    
    * open link in new tab

---
+++ src/frontend/src/common/components/library/data_subview/components/TreeCreateHelpLink/styles.ts
@@ -0,0 +1,49 @@
+import styled from "@emotion/styled";
+import {
+  fontBodyXs,
+  getColors,
+  getFontWeights,
+  getSpacings,
+  Link,
+} from "czifui";
+
+export const StyledDiv = styled.div`
+  ${(props) => {
+    const colors = getColors(props);
+    const spacings = getSpacings(props);
+
+    return `
+      margin-left: ${spacings?.l}px;
+
+      &:hover {
+        path {
+          fill: ${colors?.primary[500]};
+        }
+      }
+
+      &:active {
+        path {
+          fill: ${colors?.primary[600]};
+        }
+      }
+    `;
+  }}
+`;
+
+export const StyledLink = styled(Link)`
+  display: flex;
+  align-items: center;
+`;
+
+export const StyledSpan = styled.span`
+  ${fontBodyXs}
+  ${(props) => {
+    const fontWeights = getFontWeights(props);
+    const spacings = getSpacings(props);
+
+    return `
+      font-weight: ${fontWeights?.semibold};
+      margin: 0 ${spacings?.xxs}px;
+    `;
+  }}
+`;
