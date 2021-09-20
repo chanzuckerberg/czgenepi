450197012539d74d70f774997a75579a9360e76e
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2021-09-20 12:27:31 -0700

    WIP: Ingest list of possible date options for menu

--- src/frontend/src/components/FilterPanel/components/UploadDateFilter/index.tsx
+++ src/frontend/src/components/FilterPanel/components/UploadDateFilter/index.tsx
@@ -1,6 +1,25 @@
 import React, { FC } from "react";
 import { FormattedDateType } from "src/components/DateField";
-import { DateFilter } from "../DateFilter";
+import { DateFilter, DateMenuOption } from "../DateFilter";
+
+
+const MENU_OPTIONS_UPLOAD_DATE: DateMenuOption[] = [
+  {
+    name: "Today",
+    numDaysStartOffset: 0,
+  },
+  {
+    name: "Yesterday",
+    numDaysStartOffset: 1,
+    numDaysEndOffset: 1
+  },
+  {
+    name: "Last 7 Days",
+    numDaysStartOffset: 7
+  },
+];
+
+
 interface Props {
   updateUploadDateFilter: (
     start: FormattedDateType,
@@ -15,6 +34,7 @@ const UploadDateFilter: FC<Props> = ({ updateUploadDateFilter }) => {
       fieldKeyStart="uploadDateStart"
       inputLabel="Upload Date"
       updateDateFilter={updateUploadDateFilter}
+      menuOptions={MENU_OPTIONS_UPLOAD_DATE}
     />
   );
 };
