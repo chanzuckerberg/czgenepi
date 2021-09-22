227b3015eb43ba4d1ce578cea6ed5b6f3940a7b0
maya <mlila@chanzuckerberg.com>
2021-09-22 10:13:57 -0700

    [sc161296] disable view download failed (#679)
    * disable json download if tree not completed
    
    * disable auspice link and modal when tree build not complete

--- src/frontend/src/common/constants/types.ts
+++ src/frontend/src/common/constants/types.ts
@@ -2,3 +2,9 @@ export enum VIEWNAME {
   SAMPLES = "Samples",
   TREES = "Phylogenetic Trees",
 }
+
+export enum TREE_STATUS {
+  Completed = "COMPLETED",
+  Failed = "FAILED",
+  Started = "STARTED",
+}
