91d3877eb5fa4b0e5bc58d5130641903aa5b1de3
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2021-09-22 17:22:49 -0700

    Styling tweaks for ComplexFilter

--- src/frontend/src/components/FilterPanel/components/GenomeRecoveryFilter/index.tsx
+++ src/frontend/src/components/FilterPanel/components/GenomeRecoveryFilter/index.tsx
@@ -1,5 +1,6 @@
-import { ComplexFilter, DefaultMenuSelectOption, ComplexFilterValue } from "czifui";
+import { ComplexFilterValue, DefaultMenuSelectOption } from "czifui";
 import React from "react";
+import { StyledComplexFilter } from "../../style";
 
 interface Props {
   updateGenomeRecoveryFilter: (selected?: string) => void;
@@ -14,10 +15,16 @@ const GENOME_RECOVERY_OPTIONS = [
   },
 ];
 
-// Because czifui ComplexFilter expects callback that handles both single
-// and multi case at same time, we sidestep type issue with this.
+// HACK Because czifui ComplexFilter expects callback that handles both
+// single and multi case at same time, we sidestep type issue with this.
 type CallbackTypeWorkaround = (options: ComplexFilterValue) => void;
 
+// TODO -- With czifui 0.0.55, the `InputDropdownComponent` was exposed so it
+// can be directly styled and passed in as we do with others.
+// This should be swapped over so it matches the approach we do elsewhere.
+// (vince) To tweak the internal style of dropdown in ComplexFilter, need to
+// create a specialized set of props to insert CSS via raw `style` put into
+// underlying HTML tag. Was done when we were on czifui 0.0.53.
 const PROPS_FOR_INPUT_DROPDOWN = {
   sdsStyle: "minimal", // Would be defaulted, but must set everything now.
   // This `style` gets directly put in as HTML style and interpolated to CSS.
@@ -30,20 +37,12 @@ const PROPS_FOR_INPUT_DROPDOWN = {
 const GenomeRecoveryFilter = ({
   updateGenomeRecoveryFilter,
 }: Props): JSX.Element => {
-
   const onChange = (selectedOption: DefaultMenuSelectOption | null) => {
     updateGenomeRecoveryFilter(selectedOption?.name);
   };
 
-  // TODO (mlila): replace with sds complex filter when complete
-  // (vince): For the most part, will be a simple drop-in replacement, but there
-  // are some styling difficulties right now. See notes in LineageFilter about
-  // czifui 0.0.55, but also, even with that version I think it won't work
-  // immediately because of the `Wrapper` component in how ComplexFilter is
-  // implemented locking it to 150px width. Whenever we make the change over,
-  // expect to need to figure that out as part of it.
   return (
-    <ComplexFilter
+    <StyledComplexFilter
       label="Genome Recovery"
       options={GENOME_RECOVERY_OPTIONS}
       onChange={onChange as CallbackTypeWorkaround}
