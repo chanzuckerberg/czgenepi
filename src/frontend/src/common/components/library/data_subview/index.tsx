a3b3367b02a89fd5318a95ebdaa2b6f80427e19e
Phoenix Logan <phoenix@phoenixlogan.net>
2021-09-23 11:38:52 -0700

    Update src/frontend/src/common/components/library/data_subview/index.tsx
    add period
    
    Co-authored-by: maya <mlila@chanzuckerberg.com>

--- src/frontend/src/common/components/library/data_subview/index.tsx
+++ src/frontend/src/common/components/library/data_subview/index.tsx
@@ -416,7 +416,7 @@ const DataSubview: FunctionComponent<Props> = ({
                     lightText={
                       <CreateTreeModalDiv>
                         Your tree is being created. It may take up to 12 hours
-                        to process To check your tree’s status, visit the
+                        to process. To check your tree’s status, visit the
                         Phylogenetic Tree page.
                         <NextLink href={ROUTES.PHYLO_TREES} passHref>
                           <a href="passRef">
