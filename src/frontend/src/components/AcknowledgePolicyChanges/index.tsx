cd126316badf970d87052a54a19d9f1c06fa53e2
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2021-09-27 16:24:52 -0700

    WIP: Majority of styling. Copy in place.

--- src/frontend/src/components/AcknowledgePolicyChanges/index.tsx
+++ src/frontend/src/components/AcknowledgePolicyChanges/index.tsx
@@ -1,17 +1,45 @@
 import React from "react";
+import { ROUTES } from "src/common/routes";
 import {
   Container,
   StyledIconInfo,
+  DummyCenteringSpacer,
+  MainText,
+  StyledCloseIcon,
+  StyledLink,
 } from "./style";
+import { B } from "src/common/styles/support/style";
 
 const AcknowledgePolicyChanges = () => {
   return (
     <Container>
-      <p>hi</p>
-      <p>
+      <DummyCenteringSpacer />
+      <MainText>
         <StyledIconInfo />
-        there</p>
-      <p>world</p>
+        <B>
+        We are no longer supporting automatic GISAID submissions. {" "}
+        </B>
+        This change is reflected in our updated{" "}
+          <StyledLink
+            href={ROUTES.TERMS}
+            target="_blank"
+            rel="noopener"
+          >
+            Terms of Service
+          </StyledLink>
+          {" "}and{" "}
+          <StyledLink
+            href={ROUTES.PRIVACY}
+            target="_blank"
+            rel="noopener"
+          >
+            Privacy Policy
+          </StyledLink>
+          {" "}effective September XX.
+      </MainText>
+      <StyledCloseIcon
+        onClick={() => console.log('ALIVE')}
+      />
     </Container>
   );
 };
--- src/frontend/src/components/AcknowledgePolicyChanges/style.ts
+++ src/frontend/src/components/AcknowledgePolicyChanges/style.ts
@@ -1,9 +1,12 @@
 import styled from "@emotion/styled";
-import { fontHeaderXs, getColors, getSpacings, getIconSizes } from "czifui";
+import { fontBodyS, Link, getColors, getSpacings, getIconSizes } from "czifui";
 import IconInfo from "src/common/icons/IconInfo.svg"
+import CloseIcon from "@material-ui/icons/Close";
 
+// Note the use of `DummyCenteringSpacer`. Explanation with it.
 export const Container = styled.div`
   width: 100%;
+  min-height: 40px;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -12,7 +15,7 @@ export const Container = styled.div`
     const spacings = getSpacings(props);
     return `
       background-color: ${colors?.primary[400]};
-      padding: 0 ${spacings?.l}px;
+      padding: ${spacings?.s}px ${spacings?.l}px;
     `;
   }}
 `;
@@ -23,11 +26,59 @@ export const StyledIconInfo = styled(IconInfo)`
   ${(props) => {
     const iconSizes = getIconSizes(props);
     const spacings = getSpacings(props);
-    console.log(iconSizes);
     return `
       width: ${iconSizes?.l.width}px;
       height: ${iconSizes?.l.height}px;
       margin-right: ${spacings?.m}px;
+    `;
+  }}
+`;
+
+// This exists to help center up the central block of text in the banner.
+// Accomplishing all the styling in the spec was kind of difficult, so this
+// is being used as a mild hack: we have a central block of text and a right-aligned
+// "Close" X icon. But we don't have a left-aligned block to even things out.
+// This is a fake block so we can use justify `space-between` on flex and
+// accomplish the other two above requirements accordingly.
+export const DummyCenteringSpacer = styled.div`
+  width: 0px;
+`;
+
+// Both the margin-block and margin are here just to override defaults
+// (margin-block is coming in from user agent, plain margin is an upstream CSS)
+export const MainText = styled.p`
+  margin-block: 0;
+  margin: 0;
+  ${fontBodyS}
+  text-align: center;
+  color: white;
+`;
+
+export const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: underline;
+  text-decoration-style: dashed;
+  &:hover {
+    color: white;
+    text-decoration-line: underline;
+    text-decoration-style: solid;
+  }
+`;
+
+export const StyledCloseIcon = styled(CloseIcon)`
+  color: white;
+  ${(props) => {
+    const colors = getColors(props);
+    const iconSizes = getIconSizes(props);
+    const spacings = getSpacings(props);
+    return `
+      width: ${iconSizes?.l.width}px;
+      height: ${iconSizes?.l.height}px;
+      margin-left: ${spacings?.m}px;
+      &:hover {
+        color: ${colors?.primary[300]};
+        cursor: pointer;
+      }
     `;
   }}
 `;
