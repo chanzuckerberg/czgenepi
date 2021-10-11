8b7ffc9e914a5904a83409fc2a06fe613df51a4b
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2021-10-11 13:58:21 -0700

    Run black style

--- src/backend/aspen/app/views/usher.py
+++ src/backend/aspen/app/views/usher.py
@@ -1,6 +1,8 @@
 """Views for handling anything related to UShER"""
-from typing import Iterable, Sequence, Mapping, Any
+from typing import Any, Iterable, Mapping, Sequence
+
 from flask import g, jsonify
+
 from aspen.app.app import application, requires_auth
 from aspen.database.models.usher import UsherOption
 
@@ -9,11 +11,14 @@ from aspen.database.models.usher import UsherOption
 @requires_auth
 def get_usher_tree_options():
     """DOCME"""
-    print('this works, wacky')
-
-    usher_options: Iterable[UsherOption] = g.db_session.query(UsherOption).order_by(
-        # Lowest priority is most important, lessens as priority ascends
-        UsherOption.priority.asc()).all()
+    usher_options: Iterable[UsherOption] = (
+        g.db_session.query(UsherOption)
+        .order_by(
+            # Lowest priority is most important, lessens as priority ascends
+            UsherOption.priority.asc()
+        )
+        .all()
+    )
 
     serializable_options: Sequence[Mapping[str, Any]]
     serializable_options = [option.to_dict() for option in usher_options]
