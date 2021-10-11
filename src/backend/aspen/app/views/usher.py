108ee874250ac60249068364ae982e315311fd76
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2021-10-11 15:26:28 -0700

    Two thumbs, loves docs

--- src/backend/aspen/app/views/usher.py
+++ src/backend/aspen/app/views/usher.py
@@ -10,7 +10,12 @@ from aspen.database.models.usher import UsherOption
 @application.route("/api/usher/tree_options", methods=["GET"])
 @requires_auth
 def get_usher_tree_options():
-    """DOCME"""
+    """Gets all options user can pick from when creating tree via UShER.
+
+    Transparent view of all info on each option in database.
+    Returned in order of priority. First in options list is highest priority,
+    which is what should be the default selection and first offered option.
+    """
     usher_options: Iterable[UsherOption] = (
         g.db_session.query(UsherOption)
         .order_by(
--- src/backend/aspen/database/models/usher.py
+++ src/backend/aspen/database/models/usher.py
@@ -6,12 +6,13 @@ from aspen.database.models.mixins import DictMixin
 
 
 class UsherOption(idbase, DictMixin):  # type: ignore
-    """DOCME"""
+    """A possible tree creation option when using UShER."""
 
     __tablename__ = "usher_options"
 
     description = Column(String, unique=True, nullable=False)
     value = Column(String, unique=True, nullable=False)
+    # `priority` is order we display options to user. LOWEST number is max priority.
     priority = Column(Integer, unique=True)
 
     def __repr__(self):
