5c5c346982c90f3d2e2b89292631d6713de68c71
Phoenix Logan <phoenix@phoenixlogan.net>
2021-10-15 10:29:45 -0700

    sample identifier validation endpoint (#736)

--- src/backend/aspen/app/views/api_utils.py
+++ src/backend/aspen/app/views/api_utils.py
@@ -1,13 +1,21 @@
 import datetime
 from collections import Counter
-from typing import Collection, List, Mapping, Optional, Set, Tuple, Union
+from typing import Any, Collection, Iterable, List, Mapping, Optional, Set, Tuple, Union
 
 from sqlalchemy.orm import joinedload
 from sqlalchemy.orm.query import Query
 from sqlalchemy.orm.session import Session
 from sqlalchemy.sql.expression import and_, or_
 
-from aspen.database.models import DataType, Group, PhyloRun, PhyloTree, Sample, User
+from aspen.database.models import (
+    DataType,
+    GisaidMetadata,
+    Group,
+    PhyloRun,
+    PhyloTree,
+    Sample,
+    User,
+)
 
 
 def filter_usergroup_dict(
@@ -60,7 +68,7 @@ def get_all_identifiers_in_request(data: Mapping) -> Tuple[list[str], list[str]]
 
 
 def get_existing_private_ids(
-    private_ids: list[str], session, group_id=None
+    private_ids: list[str], session: Session, group_id=None
 ) -> list[str]:
     samples = session.query(Sample).filter(Sample.private_identifier.in_(private_ids))
 
@@ -70,7 +78,9 @@ def get_existing_private_ids(
     return [i.private_identifier for i in samples.all()]
 
 
-def get_existing_public_ids(public_ids: list[str], session, group_id=None) -> list[str]:
+def get_existing_public_ids(
+    public_ids: list[str], session: Session, group_id=None
+) -> list[str]:
     samples = session.query(Sample).filter(Sample.public_identifier.in_(public_ids))
 
     if group_id:
@@ -80,7 +90,7 @@ def get_existing_public_ids(public_ids: list[str], session, group_id=None) -> li
 
 
 def check_duplicate_samples(
-    data: Mapping, session
+    data: Mapping, session: Session
 ) -> Union[None, Mapping[str, list[str]]]:
     private_ids, public_ids = get_all_identifiers_in_request(data)
 
@@ -220,3 +230,63 @@ def authz_phylo_tree_filters(query: Query, tree_ids: Set[int], user: User) -> Qu
         ),
     )
     return query
+
+
+def get_missing_and_found_sample_ids(
+    sample_ids: Iterable[str], all_samples: Query
+) -> Tuple[Set[str], Set[Any]]:
+    """
+    Check a list of sample identifiers against Sample table public and private identifiers
+
+    Parameters:
+        sample_ids Iterable[str]: A list of identifiers (usually submitted by a user)
+                                  that need to be checked if they exist as either public
+                                  or private identifiers in the aspen database Sample table
+        all_samples (Query): Query consisting of all samples that a user has been allowed access to see.
+
+    Returns:
+            missing_sample_ids (Set[str]): Set of idenitifiers that did not match against any sample public or private identifiers
+            found_sample_ids (Set[str]): Set of idenitifiers found as either public or private identifiers
+
+    """
+    found_sample_ids = set()
+    for sample in all_samples:
+        found_sample_ids.add(sample.private_identifier)
+        found_sample_ids.add(sample.public_identifier)
+
+    # These are the sample ID's that don't match the aspen db
+    missing_sample_ids = set(sample_ids) - found_sample_ids
+    return missing_sample_ids, found_sample_ids
+
+
+def get_matching_gisaid_ids(sample_ids: Iterable[str], session: Session) -> Set[str]:
+    """
+    Check if a list of identifiers exist as gisaid strain names,
+    strip identifier (hCoV-19/) before proceeding with check against GisaidMetadata table
+
+    Parameters:
+        sample_ids Iterable[str]: A list of identifiers (usually submitted by a user)
+                                  that need to be checked if they exist as gisaid identifiers ( as strain names)
+        session (Session): An open sql alchemy session
+
+    Returns:
+            gisaid_ids (Set[str]): Set of idenitifiers that matched against GisaidMetadata table as strain names
+
+    """
+
+    gisaid_ids = set()
+
+    # we need to strip off hCoV-19/ before checking against gisaid strain name
+    # (Gisaid data is prepped by Nextstrain which strips off this prefix)
+
+    # first create a mapping of ids that were stripped (so we can return unstripped id later)
+    stripped_mapping: Mapping[str, str] = {s.strip("hCoV-19/"): s for s in sample_ids}
+
+    gisaid_matches: Iterable[GisaidMetadata] = session.query(GisaidMetadata).filter(
+        GisaidMetadata.strain.in_(stripped_mapping.keys())
+    )
+    for gisaid_match in gisaid_matches:
+        # add back in originally submitted identifier (unstripped)
+        gisaid_ids.add(stripped_mapping[gisaid_match.strain])
+
+    return gisaid_ids
