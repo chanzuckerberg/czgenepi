732b226762f73c85a1afb26ffbf812ea2eb223ea
maya <mlila@chanzuckerberg.com>
2021-10-18 13:33:08 -0700

    allow users to click through preparing modal if they want to (#743)
    * allow users to click through preparing modal if they want to
    
    * types
    
    * clarity

--- src/frontend/src/common/components/library/data_subview/components/UsherTreeFlow/components/UsherPreparingModal/index.tsx
+++ src/frontend/src/common/components/library/data_subview/components/UsherTreeFlow/components/UsherPreparingModal/index.tsx
@@ -1,5 +1,5 @@
 import { LoadingIndicator } from "czifui";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { StyledDialog, StyledP, StyledTitle } from "./style";
 
 interface Props {
@@ -8,7 +8,9 @@ interface Props {
   onClose(): void;
 }
 
-const TIME_MODAL_SHOWN = 8000; // 8 seconds
+type TimeoutType = ReturnType<typeof setTimeout> | undefined;
+
+const TIME_MODAL_SHOWN = 5000; // 5 seconds
 const USHER_URL =
   "https://genome.ucsc.edu/cgi-bin/hgPhyloPlace?db=wuhCor1&remoteFile=";
 
@@ -18,32 +20,53 @@ const UsherPreparingModal = ({
   onClose,
 }: Props): JSX.Element => {
   const [hasOpenedUrl, setHasOpenedUrl] = useState<boolean>(false);
+  const [timer, setTimer] = useState<TimeoutType>();
+
+  const openUsher = useCallback(() => {
+    if (!fastaUrl) return;
+
+    const link = document.createElement("a");
+    link.href = `${USHER_URL + fastaUrl}`;
+    link.target = "_blank";
+    link.rel = "noopener";
+    link.click();
+    link.remove();
+
+    onClose();
+    if (timer) clearTimeout(timer);
+    setTimer(undefined);
+  }, [fastaUrl, onClose, timer]);
 
   useEffect(() => {
+    // The s3 link was cleared by the parent, so future renders of this component
+    // should know that any fasta link set in the future has not been opened yet.
     if (!fastaUrl) {
       setHasOpenedUrl(false);
       return;
     }
 
+    // only set a timer to open an usher link if this modal is open.
+    // otherwise, we risk opening a link when someone clicks "cancel"
+    // from the previous modal.
+    // Also, only open the link once :)
     if (isOpen && !hasOpenedUrl) {
       setHasOpenedUrl(true);
-      setTimeout(() => {
-        if (!fastaUrl) return;
 
-        const link = document.createElement("a");
-        link.href = `${USHER_URL + fastaUrl}`;
-        link.target = "_blank";
-        link.rel = "noopener";
-        link.click();
-        link.remove();
-
-        onClose();
+      const newTimer = setTimeout(() => {
+        openUsher();
       }, TIME_MODAL_SHOWN);
+
+      setTimer(newTimer);
     }
-  }, [fastaUrl, hasOpenedUrl, isOpen, onClose]);
+  }, [fastaUrl, hasOpenedUrl, isOpen, onClose, openUsher]);
 
   return (
-    <StyledDialog open={isOpen} maxWidth="xs">
+    <StyledDialog
+      open={isOpen}
+      maxWidth="xs"
+      onClose={openUsher}
+      disableEscapeKeyDown={false}
+    >
       <div>
         <LoadingIndicator sdsStyle="tag" />
       </div>
