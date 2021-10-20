9a728ef7821b9eab7915439bb015b49053b46318
Maya Lila <mlila@chanzuckerberg.com>
2021-10-20 09:24:27 -0700

    replace existing usher confirm modal with new generic modal

--- src/frontend/src/common/components/library/data_subview/components/UsherTreeFlow/components/UsherConfirmationModal/index.tsx
+++ src/frontend/src/common/components/library/data_subview/components/UsherTreeFlow/components/UsherConfirmationModal/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { NewTabLink } from "src/common/components/library/NewTabLink";
 import usherLogo from "src/common/images/usher.png";
-import ConfirmDialog from "src/components/ConfirmDialog";
-import { StyledHeader, StyledImg, StyledP } from "./style";
+import { RedirectConfirmationModal } from "../../../RedirectConfirmationModal";
 
 interface Props {
   isOpen: boolean;
@@ -15,37 +14,28 @@ const UsherConfirmationModal = ({
   onClose,
   onConfirm,
 }: Props): JSX.Element => {
-  const title = (
-    <>
-      <StyledImg src={usherLogo as string} />
-      <StyledHeader>You are now leaving Aspen.</StyledHeader>
-    </>
-  );
-
   const content = (
-    <div>
-      <StyledP>
-        By clicking “Continue” you agree to send a copy of your genomic data to
-        UCSC’s UShER visualization service, and that you understand this may
-        make the data accessible to others. UShER is a separate service from
-        Aspen. Your data will be subject to their{" "}
-        <NewTabLink href="https://genome.ucsc.edu/conditions.html">
-          Conditions of Use
-        </NewTabLink>
-        .
-      </StyledP>
-    </div>
+    <>
+      By clicking “Continue” you agree to send a copy of your genomic data to
+      UCSC’s UShER visualization service, and that you understand this may make
+      the data accessible to others. UShER is a separate service from Aspen.
+      Your data will be subject to their{" "}
+      <NewTabLink href="https://genome.ucsc.edu/conditions.html">
+        Conditions of Use
+      </NewTabLink>
+      .
+    </>
   );
 
   return (
     <div>
-      <ConfirmDialog
-        open={isOpen}
-        onClose={onClose}
-        onConfirm={onConfirm}
-        title={title}
+      <RedirectConfirmationModal
         content={content}
         footer="It may take a few minutes to process your samples."
+        img={usherLogo as string}
+        isOpen={isOpen}
+        onClose={onClose}
+        onConfirm={onConfirm}
       />
     </div>
   );
