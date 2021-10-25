6ba806e3dbc408b99273d02028f30787ba3ee84e
Jessica Gadling <jgadling@chanzuckerberg.com>
2021-10-25 11:12:16 -0700

    Add comment.

--- src/backend/aspen/uvicorn_worker.py
+++ src/backend/aspen/uvicorn_worker.py
@@ -9,6 +9,11 @@ from typing import Any
 from uvicorn.workers import UvicornWorker
 
 
+# This file is here to make sure our application automatically reloads when
+# the python files it's serving have changed. This is *supposed* to be handled
+# automatically by gunicorn, but there's a bug in the interaction between
+# gunicorn (our web process manager) and uvicorn (our application server) that
+# we're working around for now.
 # Code from https://github.com/encode/uvicorn/pull/1193/files
 class ReloaderThread(threading.Thread):
     def __init__(self, worker: "AspenUvicornWorker", sleep_interval: float = 1.0):
