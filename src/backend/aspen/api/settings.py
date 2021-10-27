afe05d703d30ec18ac83944bfb551c313cb216c4
Jessica Gadling <jgadling@chanzuckerberg.com>
2021-10-27 10:54:37 -0700

    Fix state management for settings & oauth (#757)
    * Fix state management for settings & oauth

--- src/backend/aspen/api/settings.py
+++ src/backend/aspen/api/settings.py
@@ -1,71 +1,129 @@
 import json
 import os
-from functools import cached_property, lru_cache
-from typing import Any, Mapping
+from functools import cached_property
+from typing import Any, Dict
 
 from boto3 import Session
 from botocore.exceptions import ClientError
 from pydantic import BaseSettings
 
 
+def aws_secret_settings(settings) -> Dict[str, Any]:
+    """Loads settings from an aws secret whose value is JSON encoded."""
+    session = Session(region_name=os.environ["AWS_REGION"])
+    secret_name = os.environ.get("ASPEN_CONFIG_SECRET_NAME", "aspen-config")
+    client = session.client(
+        service_name="secretsmanager",
+        endpoint_url=os.environ.get("BOTO_ENDPOINT_URL"),
+    )
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        raise e
+    else:
+        secret = get_secret_value_response["SecretString"]
+    response: Dict = {}
+    secrets = json.loads(secret)
+
+    # Pydantic will complain if we populate keys on the Settings object that it
+    # doesn't know about -- so if there are any keys in the AWS secret that
+    # don't have matching fields in our Settings class, our app will fail to start.
+    # So we're careful here to only load specific keys into our Settings object.
+    keys = settings.__config__.aws_secret_keys
+    response = {key: secrets[key] for key in keys if secrets.get(key)}
+
+    # Some of the keys in our AWS Secret don't match the names we're using in
+    # our Settings object. This maps the key name from the AWS secret to the
+    # key name in our Settings object.
+    remap_keys = settings.__config__.aws_secrets_remap
+    response.update(
+        {val: secrets[key] for key, val in remap_keys.items() if secrets.get(key)}
+    )
+    return response
+
+
+def aws_ssm_settings(settings) -> Dict[str, Any]:
+    """Fetch settings from AWS Systems Manager Parameter Store"""
+    session = Session(region_name=os.environ["AWS_REGION"])
+
+    dev_prefix = os.environ.get("REMOTE_DEV_PREFIX")
+    deployment_stage = os.environ.get("DEPLOYMENT_STAGE")
+    stack_prefix = dev_prefix if dev_prefix else f"/{deployment_stage}stack"
+
+    parameters = settings.__config__.aws_ssm_params
+
+    client = session.client(
+        service_name="ssm", endpoint_url=os.environ.get("BOTO_ENDPOINT_URL")
+    )
+
+    response: Dict[str, Any] = {}
+    for param, key_name in parameters.items():
+        parameter_path = f"/aspen/{deployment_stage}{stack_prefix}/{param}"
+        try:
+            get_parameter = client.get_parameter(Name=parameter_path)
+        except ClientError as e:
+            raise e
+        else:
+            param_value = get_parameter["Parameter"]["Value"]
+        response[key_name] = json.loads(param_value)
+    return response
+
+
 class Settings(BaseSettings):
     """Pydantic Settings object - do not instantiate it directly, please use get_settings() as a dependency where possible"""
 
+    # Hardcoded vars"
     SERVICE_NAME: str = "Aspen"
-
     DB_DRIVER: str = "postgresql+asyncpg"
-
     DB_POOL_SIZE: int = 5
     DB_MAX_OVERFLOW: int = 0
     DB_ECHO: bool = False
+    DEBUG: bool = True
+
+    # Pydantic automatically tries to load settings with matching names from the environment if available, and then
+    # goes down its list of "magic-settings-getters" to find more data to populate this settings object with. For
+    # some of these settings we're relying on env vars, others get loaded from AWS Secrets, and others are loaded
+    # from AWS SSM parameter store.
+
+    # Env vars usually read from env vars
     AWS_REGION: str
+    FLASK_ENV: str
+    API_URL: str
+    REMOTE_DEV_PREFIX: str = ""
+    DEPLOYMENT_STAGE: str = ""
+
+    # Env vars usually pulled from AWS Secrets Manager
+    SENTRY_BACKEND_DSN: str = ""
+    EXTERNAL_AUSPICE_BUCKET: str
+    DB_BUCKET: str = ""
+    DB_rw_username: str
+    DB_rw_password: str
+    DB_address: str
+    AUTH0_USERINFO_URL: str = "userinfo"
+    FLASK_SECRET: str
+    AUTH0_CLIENT_ID: str
+    AUTH0_CLIENT_SECRET: str
+    AUTH0_MANAGEMENT_CLIENT_ID: str = ""
+    AUTH0_MANAGEMENT_CLIENT_SECRET: str = ""
+    AUTH0_DOMAIN: str
+    AUTH0_CLIENT_KWARGS: Dict[str, Any] = {
+        "scope": "openid profile email",
+    }
+
+    # These are same-name'd keys in an AWS secret, so they get remapped when
+    # we load the secrets, and then we use getter methods to handle special cases.
+    SECRET_AUTH0_AUTHORIZE_URL: str
+    SECRET_AUTH0_BASE_URL: str
+    SECRET_AUTH0_ACCESS_TOKEN_URL: str
+
+    # Env vars usually pulled from AWS SSM Parameters
+    AWS_NEXTSTRAIN_SFN_PARAMETERS: Dict
 
     ####################################################################################
     # Stack name
     @cached_property
-    def STACK_PREFIX(self) -> str:
-        remote_prefix = os.environ.get("REMOTE_DEV_PREFIX")
-        deployment_stage = os.environ.get("DEPLOYMENT_STAGE")
-        stack_name = remote_prefix if remote_prefix else f"/{deployment_stage}stack"
-        return stack_name
-
-    ####################################################################################
-    # AWS secrets
-    @cached_property
-    def AWS_SECRET(self) -> Mapping[str, Any]:
-        session = Session(region_name=self.AWS_REGION)
-
-        secret_name = os.environ.get("ASPEN_CONFIG_SECRET_NAME", "aspen-config")
-        client = session.client(
-            service_name="secretsmanager",
-            endpoint_url=os.environ.get("BOTO_ENDPOINT_URL"),
-        )
-
-        try:
-            get_secret_value_response = client.get_secret_value(SecretId=secret_name)
-        except ClientError as e:
-            raise e
-        else:
-            secret = get_secret_value_response["SecretString"]
-            return json.loads(secret)
-
-    @cached_property
-    def DEBUG(self) -> bool:
-        return False
-
-    @cached_property
-    def SECRET_KEY(self) -> str:
-        return self.AWS_SECRET["FLASK_SECRET"]
-
-    ####################################################################################
-    # auth0 properties
-    @cached_property
-    def AUTH0_CLIENT_ID(self) -> str:
-        return self.AWS_SECRET["AUTH0_CLIENT_ID"]
-
-    @cached_property
     def AUTH0_LOGOUT_URL(self) -> str:
-        flask_env = os.environ.get("FLASK_ENV")
+        flask_env = self.FLASK_ENV
         if flask_env == "production":
             return f"{self.AUTH0_BASE_URL}/v2/logout"
         else:
@@ -73,66 +131,29 @@ class Settings(BaseSettings):
 
     @cached_property
     def AUTH0_CALLBACK_URL(self) -> str:
-        flask_env = os.environ.get("FLASK_ENV")
-        if flask_env != "production":
-            return "http://backend.genepinet.localdev:3000/v2/auth/callback"
-        api_url = os.environ.get("API_URL")
-        if not api_url:
-            raise Exception("Missing API_URL in config!")
+        api_url = self.API_URL
         return f"{api_url}/v2/auth/callback"
-
-    @cached_property
-    def AUTH0_CLIENT_SECRET(self) -> str:
-        return self.AWS_SECRET["AUTH0_CLIENT_SECRET"]
-
-    @cached_property
-    def AUTH0_MANAGEMENT_CLIENT_ID(self) -> str:
-        return self.AWS_SECRET["AUTH0_MANAGEMENT_CLIENT_ID"]
-
-    @cached_property
-    def AUTH0_MANAGEMENT_CLIENT_SECRET(self) -> str:
-        return self.AWS_SECRET["AUTH0_MANAGEMENT_CLIENT_SECRET"]
-
-    @cached_property
-    def AUTH0_DOMAIN(self) -> str:
-        return self.AWS_SECRET["AUTH0_DOMAIN"]
 
     @cached_property
     def AUTH0_BASE_URL(self) -> str:
         try:
-            return self.AWS_SECRET["AUTH0_BASE_URL"]
+            return self.SECRET_AUTH0_BASE_URL
         except KeyError:
             return f"https://{self.AUTH0_DOMAIN}"
 
     @cached_property
     def AUTH0_ACCESS_TOKEN_URL(self) -> str:
         try:
-            return self.AWS_SECRET["AUTH0_ACCESS_TOKEN_URL"]
+            return self.SECRET_AUTH0_ACCESS_TOKEN_URL
         except KeyError:
             return f"{self.AUTH0_BASE_URL}/oauth/token"
 
     @cached_property
     def AUTH0_AUTHORIZE_URL(self) -> str:
         try:
-            return self.AWS_SECRET["AUTH0_AUTHORIZE_URL"]
+            return self.SECRET_AUTH0_AUTHORIZE_URL
         except KeyError:
             return f"{self.AUTH0_BASE_URL}/authorize"
-
-    @cached_property
-    def AUTH0_CLIENT_KWARGS(self) -> Mapping[str, Any]:
-        try:
-            return self.AWS_SECRET["AUTH0_CLIENT_KWARGS"]
-        except KeyError:
-            return {
-                "scope": "openid profile email",
-            }
-
-    @cached_property
-    def AUTH0_USERINFO_URL(self) -> str:
-        try:
-            return self.AWS_SECRET["AUTH0_USERINFO_URL"]
-        except KeyError:
-            return "userinfo"
 
     ####################################################################################
     # database properties
@@ -143,10 +164,10 @@ class Settings(BaseSettings):
             return os.environ["DB_DSN"]
         if os.getenv("DB_URI"):
             return os.environ["DB_URI"]
-        username = self.AWS_SECRET["DB_rw_username"]
-        password = self.AWS_SECRET["DB_rw_password"]
-        instance_address = self.AWS_SECRET["DB_address"]
-        db_name = os.getenv("REMOTE_DEV_PREFIX") or "/aspen_db"
+        username = self.DB_rw_username
+        password = self.DB_rw_password
+        instance_address = self.DB_address
+        db_name = self.REMOTE_DEV_PREFIX or "/aspen_db"
         return f"{self.DB_DRIVER}://{username}:{password}@{instance_address}{db_name}"
 
     @cached_property
@@ -154,58 +175,11 @@ class Settings(BaseSettings):
         raise NotImplementedError()
 
     ####################################################################################
-    # s3 properties
-    @cached_property
-    def DB_BUCKET(self) -> str:
-        return self.AWS_SECRET["S3_db_bucket"]
-
-    @cached_property
-    def EXTERNAL_AUSPICE_BUCKET(self) -> str:
-        return self.AWS_SECRET["S3_external_auspice_bucket"]
-
-    ####################################################################################
-    # sentry properties
-    @cached_property
-    def SENTRY_BACKEND_DSN(self) -> str:
-        try:
-            return self.AWS_SECRET["SENTRY_BACKEND_DSN"]
-        except KeyError:
-            return ""
-
-    ####################################################################################
-    # SSM Parameter properties
-    # @lru_cache
-    def _AWS_SSM_PARAMETER(self, parameter_suffix: str) -> Mapping[str, Any]:
-        session = Session(region_name=self.AWS_REGION)
-
-        deployment_stage = os.environ.get("DEPLOYMENT_STAGE")
-        parameter_name = (
-            f"/aspen/{deployment_stage}{self.STACK_PREFIX}/{parameter_suffix}"
-        )
-        client = session.client(
-            service_name="ssm", endpoint_url=os.environ.get("BOTO_ENDPOINT_URL")
-        )
-
-        try:
-            get_parameter = client.get_parameter(Name=parameter_name)
-        except ClientError as e:
-            raise e
-        else:
-            parameter = get_parameter["Parameter"]["Value"]
-            return json.loads(parameter)
-
-    @cached_property
-    def AWS_NEXTSTRAIN_SFN_PARAMETERS(self) -> Mapping[str, Any]:
-        return self._AWS_SSM_PARAMETER("nextstrain-ondemand-sfn")
-
-    ####################################################################################
-    # SFN runtime input properties
+    # SFN batch config properties
     @cached_property
     def NEXTSTRAIN_DOCKER_IMAGE_ID(self) -> str:
         return self.AWS_NEXTSTRAIN_SFN_PARAMETERS["Input"]["Run"]["docker_image_id"]
 
-    ####################################################################################
-    # SFN batch config properties
     @cached_property
     def NEXTSTRAIN_OUTPUT_PREFIX(self) -> str:
         return self.AWS_NEXTSTRAIN_SFN_PARAMETERS["OutputPrefix"]
@@ -236,9 +210,43 @@ class Settings(BaseSettings):
         keep_untouched = (
             cached_property,
         )  # https://github.com/samuelcolvin/pydantic/issues/1241
+        aws_secrets_remap = {
+            "S3_db_bucket": "DB_BUCKET",
+            "S3_external_auspice_bucket": "EXTERNAL_AUSPICE_BUCKET",
+            "AUTH0_BASE_URL": "SECRET_AUTH0_BASE_URL",
+            "AUTH0_ACCESS_TOKEN_URL": "SECRET_AUTH0_ACCESS_TOKEN_URL",
+            "AUTH0_AUTHORIZE_URL": "SECRET_AUTH0_AUTHORIZE_URL",
+        }
+        aws_secret_keys = [
+            "SENTRY_BACKEND_DSN",
+            "DB_rw_username",
+            "DB_rw_password",
+            "DB_address",
+            "AUTH0_USERINFO_URL",
+            "FLASK_SECRET",
+            "AUTH0_CLIENT_ID",
+            "AUTH0_CLIENT_SECRET",
+            "AUTH0_MANAGEMENT_CLIENT_ID",
+            "AUTH0_MANAGEMENT_CLIENT_SECRET",
+            "AUTH0_DOMAIN",
+            "AUTH0_CLIENT_KWARGS",
+        ]
+        aws_ssm_params = {"nextstrain-ondemand-sfn": "AWS_NEXTSTRAIN_SFN_PARAMETERS"}
 
-
-@lru_cache
-def get_settings() -> Settings:
-    settings = Settings()
-    return settings
+        @classmethod
+        def customise_sources(
+            cls,
+            init_settings,
+            env_settings,
+            file_secret_settings,
+        ):
+            # Here we're telling pydantic to go ahead and load settings from
+            # its default loaders first, and then try reading them from the
+            # custom loaders we've defined at the top of this file.
+            return (
+                init_settings,
+                env_settings,
+                file_secret_settings,
+                aws_secret_settings,
+                aws_ssm_settings,
+            )
--- src/backend/aspen/api/views/auth.py
+++ src/backend/aspen/api/views/auth.py
@@ -1,41 +1,20 @@
 import os
-from functools import lru_cache
 from urllib.parse import urlencode
 
 from authlib.integrations.base_client.errors import OAuthError
-from authlib.integrations.starlette_client import OAuth, StarletteRemoteApp
+from authlib.integrations.starlette_client import StarletteRemoteApp
 from fastapi import APIRouter, Depends
 from fastapi.responses import RedirectResponse
 from starlette.requests import Request
 from starlette.responses import Response
 
 import aspen.api.error.http_exceptions as ex
-from aspen.api.settings import get_settings, Settings
+from aspen.api.deps import get_auth0_client, get_settings
+from aspen.api.settings import Settings
 
 # From the example here:
 # https://github.com/authlib/demo-oauth-client/tree/master/fastapi-google-login
 router = APIRouter()
-
-
-@lru_cache
-def get_auth0_client() -> StarletteRemoteApp:
-    # TODO - settings is an unhashable type, so we can't make it a Dependency
-    # *and* use the lru_cache decorator on this getter. We should probably use
-    # fastapi's built-in dependency caching instead of lru_cache here, but
-    # it needs a little more thought first.
-    # see: https://github.com/tiangolo/fastapi/issues/1985
-    settings: Settings = get_settings()
-    oauth = OAuth()
-    auth0 = oauth.register(
-        "auth0",
-        client_id=settings.AUTH0_CLIENT_ID,
-        client_secret=settings.AUTH0_CLIENT_SECRET,
-        api_base_url=settings.AUTH0_BASE_URL,
-        access_token_url=settings.AUTH0_ACCESS_TOKEN_URL,
-        authorize_url=settings.AUTH0_AUTHORIZE_URL,
-        client_kwargs=settings.AUTH0_CLIENT_KWARGS,
-    )
-    return auth0
 
 
 @router.get("/login")
