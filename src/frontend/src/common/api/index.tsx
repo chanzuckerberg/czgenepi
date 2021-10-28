2fceab3518587a23ee22025e5d31895b2d30b7ff
lvreynoso <lreynoso@chanzuckerberg.com>
2021-10-28 16:41:57 -0700

    Remove eslint

--- src/frontend/src/common/api/index.tsx
+++ src/frontend/src/common/api/index.tsx
@@ -119,4 +119,3 @@ const TREE_MAP = new Map<string, keyof Tree>([
 ]);
 export const fetchTrees = (): Promise<TreeResponse> =>
   apiResponse<TreeResponse>(["phylo_trees"], [TREE_MAP], API.PHYLO_TREES);
-  
