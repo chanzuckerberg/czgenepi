247899168a6e8706ddb8dca8e6567b5827d81125
Maya Lila <mlila@chanzuckerberg.com>
2021-11-05 11:06:02 -0700

    types

--- src/frontend/src/common/components/library/data_subview/components/CreateNSTreeModal/components/MissingSampleAlert/style.ts
+++ src/frontend/src/common/components/library/data_subview/components/CreateNSTreeModal/components/MissingSampleAlert/style.ts
@@ -1,9 +1,9 @@
 import styled from "@emotion/styled";
-import { getIconSizes } from "czifui";
+import { getIconSizes, Props } from "czifui";
 import ArrowDownIcon from "src/common/icons/IconArrowDownSmall.svg";
 import ArrowUpIcon from "src/common/icons/IconArrowUpSmall.svg";
 
-const smallIcon = (props) => {
+const smallIcon = (props: Props) => {
   const iconSizes = getIconSizes(props);
   return `
     height: ${iconSizes?.s.height}px;
