6383514d1ee1ce06d5c19280aca3a87d5b44e260
Jessica Gadling <jgadling@chanzuckerberg.com>
2021-11-09 14:41:47 -0800

    Attach the ID of the user initiating an ondemand run to a workflow.

--- src/backend/aspen/main.py
+++ src/backend/aspen/main.py
@@ -4,7 +4,6 @@ from aspen.app.app import application  # noqa: F401
 from aspen.app.views.auth import callback_handling, login, logout  # noqa: F401
 from aspen.app.views.health import health  # noqa: F401
 from aspen.app.views.index import serve  # noqa: F401
-from aspen.app.views.phylo_runs import start_phylo_run  # noqa: F401
 from aspen.app.views.phylo_trees import auspice_view, phylo_trees  # noqa: F401
 from aspen.app.views.sample import samples  # noqa: F401
 from aspen.app.views.usergroup import usergroup  # noqa: F401
