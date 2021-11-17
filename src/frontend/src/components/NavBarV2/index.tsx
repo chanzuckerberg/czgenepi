68ca2c769cb392e4975e018d56ba27cb3d14e0b6
Seth Feingold <sfeingold1104@gmail.com>
2021-11-17 11:16:27 -0600

    eslint fixes

--- src/frontend/src/components/NavBarV2/index.tsx
+++ src/frontend/src/components/NavBarV2/index.tsx
@@ -2,113 +2,112 @@ import React, { useState } from "react";
 import CloseIcon from "src/common/images/close-icon.svg";
 import HeaderLogo from "src/common/images/logo.svg";
 import {
-    Bar,
-    ButtonLink,
-    HeaderContainer,
-    HeaderLogoContainer,
-    HeaderMaxWidthContainer,
-    HeaderTopContainer,
-    HeaderTopLinks,
-    MobileNavClose,
-    MobileNavCloseContainer,
-    MobileNavLink,
-    MobileNavLinkContainer,
-    MobileNavSeparator,
-    MobileNavToggle,
-    MobileNavTray,
-    TextLink
+  Bar,
+  ButtonLink,
+  HeaderContainer,
+  HeaderLogoContainer,
+  HeaderMaxWidthContainer,
+  HeaderTopContainer,
+  HeaderTopLinks,
+  MobileNavClose,
+  MobileNavCloseContainer,
+  MobileNavLink,
+  MobileNavLinkContainer,
+  MobileNavSeparator,
+  MobileNavToggle,
+  MobileNavTray,
+  TextLink,
 } from "./style";
 
 export default function NavBarV2(): JSX.Element {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const [menuOpen, setMenuOpen] = useState(false);
+  function toggleMobileNav() {
+    setMenuOpen(!menuOpen);
+  }
 
-    function toggleMobileNav() {
-        setMenuOpen(!menuOpen);
-    }
-
-    return (
-        <HeaderContainer>
-            <HeaderMaxWidthContainer>
-                <HeaderTopContainer>
-                    <HeaderLogoContainer href="/">
-                        <HeaderLogo />
-                    </HeaderLogoContainer>
-                    <HeaderTopLinks>
-                        <TextLink href="/">Resources</TextLink>
-                        <ButtonLink href="/">Request Access</ButtonLink>
-                        <ButtonLink href="/">Sign in</ButtonLink>
-                    </HeaderTopLinks>
-                    <MobileNavToggle
-                        onClick={toggleMobileNav}
-                        onKeyDown={toggleMobileNav}
-                        >
-                        <Bar></Bar>
-                        <Bar></Bar>
-                        <Bar></Bar>
-                    </MobileNavToggle>
-                    <MobileNavTray style={menuOpen ? { width: "100%" } : { width: "0" }}>
-                        <MobileNavCloseContainer>
-                            <MobileNavClose
-                                onClick={toggleMobileNav}
-                                onKeyDown={toggleMobileNav}
-                            >
-                                <CloseIcon />
-                            </MobileNavClose>
-                        </MobileNavCloseContainer>
-                        <MobileNavLinkContainer>
-                            <MobileNavLink
-                                href="/"
-                                style={menuOpen ? { opacity: "1" } : { opacity: "0" }}
-                                target="_blank"
-                                rel="noreferrer"
-                                aria-label="View the Aspen GitHub page (opens in new window)"
-                                >
-                                Github
-                            </MobileNavLink>
-                            <MobileNavLink
-                                href="/"
-                                style={menuOpen ? { opacity: "1" } : { opacity: "0" }}
-                                target="_blank"
-                                rel="noreferrer"
-                                aria-label="View the Aspen Careers page (opens in new window)"
-                                >
-                                Careers
-                            </MobileNavLink>
-                            <MobileNavLink
-                                href="/"
-                                style={menuOpen ? { opacity: "1" } : { opacity: "0" }}
-                                target="_blank"
-                                rel="noreferrer"
-                                aria-label="View the Aspen Help page (opens in new window)"
-                                >
-                                Resources
-                            </MobileNavLink>
-                            <MobileNavSeparator
-                            style={menuOpen ? { opacity: "1" } : { opacity: "0" }}
-                            ></MobileNavSeparator>
-                            <MobileNavLink
-                                href="/"
-                                style={menuOpen ? { opacity: "1" } : { opacity: "0" }}
-                                target="_blank"
-                                rel="noreferrer"
-                                aria-label="View the Aspen login page (opens in new window)"
-                                >
-                                Sign In
-                            </MobileNavLink>
-                            <MobileNavLink
-                                href="/"
-                                style={menuOpen ? { opacity: "1" } : { opacity: "0" }}
-                                target="_blank"
-                                rel="noreferrer"
-                                aria-label="Request access to Aspen (opens in new window)"
-                                >
-                                Request Access
-                            </MobileNavLink>
-                        </MobileNavLinkContainer>
-                    </MobileNavTray>
-                </HeaderTopContainer>
-            </HeaderMaxWidthContainer>
-        </HeaderContainer>
-    );
+  return (
+    <HeaderContainer>
+      <HeaderMaxWidthContainer>
+        <HeaderTopContainer>
+          <HeaderLogoContainer href="/">
+            <HeaderLogo />
+          </HeaderLogoContainer>
+          <HeaderTopLinks>
+            <TextLink href="/">Resources</TextLink>
+            <ButtonLink href="/">Request Access</ButtonLink>
+            <ButtonLink href="/">Sign in</ButtonLink>
+          </HeaderTopLinks>
+          <MobileNavToggle
+            onClick={toggleMobileNav}
+            onKeyDown={toggleMobileNav}
+          >
+            <Bar></Bar>
+            <Bar></Bar>
+            <Bar></Bar>
+          </MobileNavToggle>
+          <MobileNavTray style={menuOpen ? { width: "100%" } : { width: "0" }}>
+            <MobileNavCloseContainer>
+              <MobileNavClose
+                onClick={toggleMobileNav}
+                onKeyDown={toggleMobileNav}
+              >
+                <CloseIcon />
+              </MobileNavClose>
+            </MobileNavCloseContainer>
+            <MobileNavLinkContainer>
+              <MobileNavLink
+                href="/"
+                style={menuOpen ? { opacity: "1" } : { opacity: "0" }}
+                target="_blank"
+                rel="noreferrer"
+                aria-label="View the Aspen GitHub page (opens in new window)"
+              >
+                Github
+              </MobileNavLink>
+              <MobileNavLink
+                href="/"
+                style={menuOpen ? { opacity: "1" } : { opacity: "0" }}
+                target="_blank"
+                rel="noreferrer"
+                aria-label="View the Aspen Careers page (opens in new window)"
+              >
+                Careers
+              </MobileNavLink>
+              <MobileNavLink
+                href="/"
+                style={menuOpen ? { opacity: "1" } : { opacity: "0" }}
+                target="_blank"
+                rel="noreferrer"
+                aria-label="View the Aspen Help page (opens in new window)"
+              >
+                Resources
+              </MobileNavLink>
+              <MobileNavSeparator
+                style={menuOpen ? { opacity: "1" } : { opacity: "0" }}
+              ></MobileNavSeparator>
+              <MobileNavLink
+                href="/"
+                style={menuOpen ? { opacity: "1" } : { opacity: "0" }}
+                target="_blank"
+                rel="noreferrer"
+                aria-label="View the Aspen login page (opens in new window)"
+              >
+                Sign In
+              </MobileNavLink>
+              <MobileNavLink
+                href="/"
+                style={menuOpen ? { opacity: "1" } : { opacity: "0" }}
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Request access to Aspen (opens in new window)"
+              >
+                Request Access
+              </MobileNavLink>
+            </MobileNavLinkContainer>
+          </MobileNavTray>
+        </HeaderTopContainer>
+      </HeaderMaxWidthContainer>
+    </HeaderContainer>
+  );
 }
--- src/frontend/src/components/NavBarV2/style.ts
+++ src/frontend/src/components/NavBarV2/style.ts
@@ -1,136 +1,135 @@
 import styled from "@emotion/styled";
 
 export const Bar = styled.div`
-    width: 17px;
-    height: 1px;
-    background-color: #ffffff;
-    margin: 5px 0;
+  width: 17px;
+  height: 1px;
+  background-color: #ffffff;
+  margin: 5px 0;
 `;
 
 export const ButtonLink = styled.a`
-    display: inline-block;
-    margin: 0 8px;
-    padding: 7px 14px 6px 14px;
-    border: 1px solid #ffffff;
-    border-radius: 24px;
-    width: 140px;
-    text-align: center;
-    font-size: 13px;
-    font-weight: 600;
-    line-height: 20px;
-    letter-spacing: 0.3px;
+  display: inline-block;
+  margin: 0 8px;
+  padding: 7px 14px 6px 14px;
+  border: 1px solid #ffffff;
+  border-radius: 24px;
+  width: 140px;
+  text-align: center;
+  font-size: 13px;
+  font-weight: 600;
+  line-height: 20px;
+  letter-spacing: 0.3px;
 
-    &:hover {
-        background: #ffffff;
-        color: #000000;
-    }
+  &:hover {
+    background: #ffffff;
+    color: #000000;
+  }
 `;
 
 export const HeaderTopLinks = styled.div`
-    position: relative;
-    z-index: 2;
+  position: relative;
+  z-index: 2;
 
-    @media (max-width: 768px) {
-        display: none;
-    }
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 export const HeaderContainer = styled.div`
-    background: black;
-    color: white;
+  background: black;
+  color: white;
 
-    & a  {
-        color: white;
-        font-size: 14px;
-        font-weight: 400;
-        line-height: 24px;
-        letter-spacing: 0.3px;
-    }
+  & a {
+    color: white;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 24px;
+    letter-spacing: 0.3px;
+  }
 `;
 
 export const HeaderLogoContainer = styled.a`
-    width: 100%;
-    max-width: 164px;
-    position: relative;
-    z-index: 2;
+  width: 100%;
+  max-width: 164px;
+  position: relative;
+  z-index: 2;
 
-    @media (max-width: 768px) {
-        max-width: 100px;
-    }
+  @media (max-width: 768px) {
+    max-width: 100px;
+  }
 `;
 
 export const HeaderMaxWidthContainer = styled.div`
-    max-width: 1440px;
-    margin: 0 auto;
+  max-width: 1440px;
+  margin: 0 auto;
 `;
 
 export const HeaderTopContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 38px 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 38px 60px;
 
-    @media (max-width: 768px) {
-        padding: 17px 20px;
-    }
-
+  @media (max-width: 768px) {
+    padding: 17px 20px;
+  }
 `;
 
 export const MobileNavClose = styled.span`
-    cursor: pointer;
-    padding-right: 26px;
+  cursor: pointer;
+  padding-right: 26px;
 `;
 
 export const MobileNavCloseContainer = styled.div`
-    width: 100%;
-    text-align: right;
-    padding-top: 24px;
+  width: 100%;
+  text-align: right;
+  padding-top: 24px;
 `;
 
 export const MobileNavLink = styled.a`
-    display: block;
-    text-decoration: none;
-    color: #000000 !important;
-    @include font-body-xs;
-    font-weight: 600;
-    margin: 22px 0;
-    transition: opacity 0.2s;
+  display: block;
+  text-decoration: none;
+  color: #000000 !important;
+  @include font-body-xs;
+  font-weight: 600;
+  margin: 22px 0;
+  transition: opacity 0.2s;
 `;
 
 export const MobileNavLinkContainer = styled.div`
-    padding-top: 30px;
-    padding-left: 40px;
+  padding-top: 30px;
+  padding-left: 40px;
 `;
 
 export const MobileNavSeparator = styled.div`
-    width: 60px;
-    height: 1px;
-    background-color: #cccccc;
+  width: 60px;
+  height: 1px;
+  background-color: #cccccc;
 `;
 
 export const MobileNavToggle = styled.div`
-    display: none;
-    position: relative;
-    z-index: 2;
+  display: none;
+  position: relative;
+  z-index: 2;
 
-    @media (max-width: 768px) {
-        display: inline-block;
-        cursor: pointer;
-    }
+  @media (max-width: 768px) {
+    display: inline-block;
+    cursor: pointer;
+  }
 `;
 
 export const MobileNavTray = styled.div`
-    height: 100vh;
-    width: 90%;
-    max-width: 260px;
-    position: fixed;
-    right: 0;
-    top: 0;
-    background-color: #ffffff;
-    z-index: 10;
-    transition: all 0.4s;
+  height: 100vh;
+  width: 90%;
+  max-width: 260px;
+  position: fixed;
+  right: 0;
+  top: 0;
+  background-color: #ffffff;
+  z-index: 10;
+  transition: all 0.4s;
 `;
 
 export const TextLink = styled.a`
-    margin: 0 10px;
+  margin: 0 10px;
 `;
