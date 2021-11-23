dbffd08fa9aba8a17ae84e867fda690a8b23af57
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2021-11-23 14:48:24 -0800

    Placeholder (maybe real?) Last Updated date

--- src/frontend/src/views/Terms/index.tsx
+++ src/frontend/src/views/Terms/index.tsx
@@ -19,7 +19,7 @@ export default function Terms(): JSX.Element {
     <>
       <Title>
         <H1>Chan Zuckerberg GEN EPI (formerly Aspen) Terms of Use</H1>
-        <H4>Last Updated: September 30, 2021. </H4>
+        <H4>Last Updated: December 6, 2021</H4>
       </Title>
       <P>
         Please read these Terms of Use (&quot;Terms&quot;) before using Chan
