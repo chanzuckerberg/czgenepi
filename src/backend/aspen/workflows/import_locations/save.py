986a56bdd8d5c2a8e552f1739cd6527d233c88be
lvreynoso <lreynoso@chanzuckerberg.com>
2021-11-30 12:18:15 -0800

    Use SQLAlchemy orm (async) instead of raw sql to import locations

--- src/backend/aspen/workflows/import_locations/save.py
+++ src/backend/aspen/workflows/import_locations/save.py
@@ -1,35 +1,66 @@
-import click
+import asyncio
 
+import click
+import sqlalchemy as sa
+import sqlalchemy.dialects.postgresql as postgresql
+
+from aspen.api.settings import Settings
 from aspen.config.config import Config
-from aspen.database.connection import (
-    get_db_uri,
-    init_db,
-    session_scope,
-    SqlAlchemyInterface,
-)
+from aspen.database.connection import init_async_db
+from aspen.database.models import GisaidMetadata, Location
+
+
+async def save():
+    settings = Settings()
+    db = init_async_db(settings.DB_DSN)
+
+    async with db.make_session() as session:
+        # Insert all locations from gisaid_metadata
+        gisaid_locations_select = sa.select(
+            GisaidMetadata.region,
+            GisaidMetadata.country,
+            GisaidMetadata.division,
+            GisaidMetadata.location,
+        ).distinct()
+        gisaid_locations_insert = (
+            postgresql.insert(Location.__table__)
+            .from_select(
+                ["region", "country", "division", "location"], gisaid_locations_select
+            )
+            .on_conflict_do_nothing(
+                index_elements=("region", "country", "division", "location")
+            )
+        )
+        await session.execute(gisaid_locations_insert)
+
+        # Make sure we have equivalent rows for region/country/division but with null locations
+        except_select = (
+            sa.select(Location.region, Location.country, Location.division)
+            .where(Location.location == None)
+            .distinct()
+        )
+        null_locations_select = (
+            sa.select(Location.region, Location.country, Location.division)
+            .distinct()
+            .except_(except_select)
+        )
+        null_locations_insert = Location.__table__.insert().from_select(
+            ["region", "country", "division"], null_locations_select
+        )
+        await session.execute(null_locations_insert)
+
+        await session.commit()
+
+    print("Successfully imported locations!")
 
 
 @click.command("save")
 @click.option("--test", type=bool, is_flag=True)
-def cli(
-    test: bool,
-):
+def cli(test: bool):
     if test:
         print("Success!")
         return
-
-    interface: SqlAlchemyInterface = init_db(get_db_uri(Config()))
-    with session_scope(interface) as session:
-        # Insert all locations from gisaid_metadata
-        insert_rows_from_gisaid = "INSERT INTO aspen.locations (region, country, division, location) SELECT DISTINCT region, country, division, location FROM aspen.gisaid_metadata ON CONFLICT (region, country, division, location) DO NOTHING"
-        # Make sure we have equivalent rows for region/country/division but with null locations
-        insert_rows_with_null_locations = "INSERT INTO aspen.locations (region, country, division) SELECT DISTINCT region, country, division FROM aspen.locations EXCEPT SELECT DISTINCT region, country, division FROM aspen.locations WHERE location IS NULL"
-        session.execute(insert_rows_from_gisaid)
-        session.execute(insert_rows_with_null_locations)
-
-        session.commit()
-
-        print("Successfully imported locations!")
+    asyncio.run(save())
 
 
 if __name__ == "__main__":
