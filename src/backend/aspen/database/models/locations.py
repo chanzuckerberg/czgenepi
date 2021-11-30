82dc7d0700ee96b980813f05f724eb8042431fac
lvreynoso <lreynoso@chanzuckerberg.com>
2021-11-30 15:31:51 -0800

    Handle samples with no value in location column

--- src/backend/aspen/database/models/locations.py
+++ src/backend/aspen/database/models/locations.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, UniqueConstraint
 
 from aspen.database.models.base import idbase
 from aspen.database.models.mixins import DictMixin
@@ -8,6 +8,11 @@ class Location(idbase, DictMixin):  # type: ignore
     """List of supported DPH/Sample locations, imported from Gisaid"""
 
     __tablename__ = "locations"
+    __table_args__ = (
+        UniqueConstraint(
+            "region", "country", "division", "location", name="uq_locations_cols"
+        ),
+    )
 
     region = Column(String, nullable=True)
     country = Column(String, nullable=True)
