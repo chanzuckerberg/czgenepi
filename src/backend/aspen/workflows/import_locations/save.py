b2cfd35ab6989af075d8901f37324abf8b39b03d
lvreynoso <lreynoso@chanzuckerberg.com>
2021-12-01 10:48:15 -0800

    Change async to sync db interactions

--- src/backend/aspen/workflows/import_locations/save.py
+++ src/backend/aspen/workflows/import_locations/save.py
@@ -1,19 +1,22 @@
-import asyncio
-
 import click
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
-from aspen.api.settings import Settings
-from aspen.database.connection import init_async_db
+from aspen.config.config import Config
+from aspen.database.connection import (
+    get_db_uri,
+    init_db,
+    session_scope,
+    SqlAlchemyInterface,
+)
 from aspen.database.models import GisaidMetadata, Location
 
 
-async def save():
-    settings = Settings()
-    db = init_async_db(settings.DB_DSN)
+def save():
+    config = Config()
+    interface: SqlAlchemyInterface = init_db(get_db_uri(config))
 
-    async with db.make_session() as session:
+    with session_scope(interface) as session:
         # Insert all locations from gisaid_metadata
         gisaid_locations_select = sa.select(
             GisaidMetadata.region,
@@ -30,7 +33,7 @@ async def save():
                 index_elements=("region", "country", "division", "location")
             )
         )
-        await session.execute(gisaid_locations_insert)
+        session.execute(gisaid_locations_insert)
 
         # Make sure we have equivalent rows for region/country/division but with null locations
         except_select = (
@@ -46,9 +49,9 @@ async def save():
         null_locations_insert = Location.__table__.insert().from_select(
             ["region", "country", "division"], null_locations_select
         )
-        await session.execute(null_locations_insert)
+        session.execute(null_locations_insert)
 
-        await session.commit()
+        session.commit()
 
     print("Successfully imported locations!")
 
@@ -59,7 +62,7 @@ def cli(test: bool):
     if test:
         print("Success!")
         return
-    asyncio.run(save())
+    save()
 
 
 if __name__ == "__main__":
