0411ae5074c3a705441fec329cfdd90926e92fda
lvreynoso <lreynoso@chanzuckerberg.com>
2021-12-06 10:04:14 -0800

    Remove unused styled components from LocationField

--- src/frontend/src/views/Upload/components/Metadata/components/Table/components/Row/components/LocationField/style.ts
+++ src/frontend/src/views/Upload/components/Metadata/components/Table/components/Row/components/LocationField/style.ts
@@ -1,18 +1,5 @@
 import styled from "@emotion/styled";
-import TextField from "@material-ui/core/TextField";
-import { fontBodyXxs, getColors, getSpaces } from "czifui";
-
-export const StyledTextField = styled(TextField)`
-  ${(props) => {
-    const spaces = getSpaces(props);
-
-    return `
-      min-width: ${(spaces?.l || 0) + 195}px;
-      padding-right: ${spaces?.l}px;
-      margin: 0;
-    `;
-  }}
-`;
+import { getSpaces } from "czifui";
 
 export const StyledDiv = styled.div`
   ${(props) => {
@@ -20,18 +7,6 @@ export const StyledDiv = styled.div`
 
     return `
       padding-right: ${spaces?.l}px;
-    `;
-  }}
-`;
-
-export const MenuSubtext = styled.div`
-  ${fontBodyXxs}
-
-  ${(props) => {
-    const colors = getColors(props);
-
-    return `
-      color: ${colors?.gray[500]};
     `;
   }}
 `;
