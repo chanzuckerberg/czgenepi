1d0b36dac449eccaa9ea83e8ff83985109f40ed1
lvreynoso <lreynoso@chanzuckerberg.com>
2021-12-08 14:22:54 -0800

    Clarify logic using set operations

--- src/backend/aspen/workflows/import_locations/save.py
+++ src/backend/aspen/workflows/import_locations/save.py
@@ -45,26 +45,24 @@ def save():
             .where(Location.location == None)
             .distinct()
         )
-        existing_null_locations = session.execute(existing_null_location_select).all()
+        existing_null_locations = set(
+            session.execute(existing_null_location_select).all()
+        )
 
         gisaid_null_locations_select = sa.select(
             GisaidMetadata.region,
             GisaidMetadata.country,
             GisaidMetadata.division,
         ).distinct()
-        gisaid_null_locations = session.execute(gisaid_null_locations_select).all()
+        gisaid_null_locations = set(session.execute(gisaid_null_locations_select).all())
 
-        new_null_locations = {
-            region_country_division_tuple
-            for region_country_division_tuple in gisaid_null_locations
-            if region_country_division_tuple not in existing_null_locations
-        }
+        new_null_locations = gisaid_null_locations - existing_null_locations
         new_null_location_values = list(
             map(
-                lambda location: {
-                    "region": location[0],
-                    "country": location[1],
-                    "division": location[2],
+                lambda region_country_division_tuple: {
+                    "region": region_country_division_tuple[0],
+                    "country": region_country_division_tuple[1],
+                    "division": region_country_division_tuple[2],
                     "location": None,
                 },
                 new_null_locations,
