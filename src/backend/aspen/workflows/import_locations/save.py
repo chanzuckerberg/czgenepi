3fc9e363db6f110dd94e2e35959277d2cbfc9279
Lucia Reynoso <lreynoso@chanzuckerberg.com>
2021-12-10 13:54:04 -0800

    Fix import_location script so it is idempotent (#852)
    * Fix import_location script so it is idempotent
    
    * Import all distinct region, country, division, location combinations from gisaid_metadata where location is not an empty string
    * Separately, insert rows with null location for every distinct region, country, division combination
    * Use Python set math to avoid massive join
    
    * Clarify logic using set operations
    
    * Tell flake8 we actually need double-equals
    
    * Take care of possible future edge case when importing gisaid metadata data

--- src/backend/aspen/workflows/import_locations/save.py
+++ src/backend/aspen/workflows/import_locations/save.py
@@ -1,6 +1,12 @@
+# flake8: noqa: E711
+# Doing a double-equals comparison to None is critical for the statements
+# that use it to compile to the intended SQL, which is why tell flake8 to
+# ignore rule E711 at the top of this file
+
 import click
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
+from sqlalchemy.sql.expression import and_
 
 from aspen.config.config import Config
 from aspen.database.connection import (
@@ -18,12 +24,16 @@ def save():
 
     with session_scope(interface) as session:
         # Insert all locations from gisaid_metadata
-        gisaid_locations_select = sa.select(
-            GisaidMetadata.region,
-            GisaidMetadata.country,
-            GisaidMetadata.division,
-            GisaidMetadata.location,
-        ).distinct()
+        gisaid_locations_select = (
+            sa.select(
+                GisaidMetadata.region,
+                GisaidMetadata.country,
+                GisaidMetadata.division,
+                GisaidMetadata.location,
+            )
+            .where(and_(GisaidMetadata.location != "", GisaidMetadata.location != None))
+            .distinct()
+        )
         gisaid_locations_insert = (
             postgresql.insert(Location.__table__)
             .from_select(
@@ -35,21 +45,40 @@ def save():
         )
         session.execute(gisaid_locations_insert)
 
-        # Make sure we have equivalent rows for region/country/division but with null locations
-        except_select = (
+        # Insert an entry with a null location for every distinct Region/Country/Division combination
+        existing_null_location_select = (
             sa.select(Location.region, Location.country, Location.division)
-            .where(Location.location is None)
+            .where(Location.location == None)
             .distinct()
         )
-        null_locations_select = (
-            sa.select(Location.region, Location.country, Location.division)
-            .distinct()
-            .except_(except_select)
+        existing_null_locations = set(
+            session.execute(existing_null_location_select).all()
         )
-        null_locations_insert = Location.__table__.insert().from_select(
-            ["region", "country", "division"], null_locations_select
+
+        gisaid_null_locations_select = sa.select(
+            GisaidMetadata.region,
+            GisaidMetadata.country,
+            GisaidMetadata.division,
+        ).distinct()
+        gisaid_null_locations = set(session.execute(gisaid_null_locations_select).all())
+
+        new_null_locations = gisaid_null_locations - existing_null_locations
+        new_null_location_values = list(
+            map(
+                lambda region_country_division_tuple: {
+                    "region": region_country_division_tuple[0],
+                    "country": region_country_division_tuple[1],
+                    "division": region_country_division_tuple[2],
+                    "location": None,
+                },
+                new_null_locations,
+            )
         )
-        session.execute(null_locations_insert)
+        if len(new_null_location_values) > 0:
+            new_null_locations_insert = Location.__table__.insert().values(
+                new_null_location_values
+            )
+            session.execute(new_null_locations_insert)
 
         session.commit()
 
