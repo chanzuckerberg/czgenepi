945fdb35ae0691ff8e539792107bcc2f0c7c79f1
Phoenix Logan <phoenix@phoenixlogan.net>
2021-12-15 16:50:59 -0800

    navbar useUserInfo cleanup (#873)

--- src/frontend/src/components/NavBar/components/RightNav/index.tsx
+++ src/frontend/src/components/NavBar/components/RightNav/index.tsx
@@ -1,56 +1,30 @@
-import cx from "classnames";
 import Link from "next/link";
 import React from "react";
-import ENV from "src/common/constants/ENV";
 import { useUserInfo } from "src/common/queries/auth";
-import { API } from "../../../../common/api";
 import { ROUTES } from "../../../../common/routes";
-import style from "../../index.module.scss";
 import UserMenu from "./components/UserMenu";
-import { ResourcesLink, UploadButton } from "./style";
+import { UploadButton } from "./style";
 
 export default function RightNav(): JSX.Element {
   const { data } = useUserInfo();
 
   const user = data?.user;
 
-  const signInLink = (
-    <a href={ENV.API_URL + API.LOG_IN} data-test-id="navbar-sign-in-link">
-      <div className={cx(style.item, style.link)}>Sign In</div>
-    </a>
-  );
-  const genEpiResourcesLink = (
-    <a href={ROUTES.RESOURCES}>
-      <ResourcesLink className={cx(style.item, style.link)}>
-        Gen Epi Resources
-      </ResourcesLink>
-    </a>
-  );
+  function LoggedInNav(): JSX.Element {
+    return (
+      <>
+        <Link href={ROUTES.UPLOAD_STEP1} passHref>
+          <a href="passHref">
+            <UploadButton isRounded variant="outlined">
+              Upload
+            </UploadButton>
+          </a>
+        </Link>
 
-  function IsLoggedIn(): JSX.Element {
-    if (user) {
-      return (
-        <>
-          <Link href={ROUTES.UPLOAD_STEP1} passHref>
-            <a href="passHref">
-              <UploadButton isRounded variant="outlined">
-                Upload
-              </UploadButton>
-            </a>
-          </Link>
-
-          <UserMenu user={user.name} />
-        </>
-      );
-    } else {
-      return (
-        <>
-          {genEpiResourcesLink}
-          {signInLink}
-        </>
-      );
-    }
+        <UserMenu user={user?.name} />
+      </>
+    );
   }
 
-  return <IsLoggedIn />;
+  return <LoggedInNav />;
 }
