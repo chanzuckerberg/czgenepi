d2871bde9a71bb39e41d4af02233a5801814e782
phoenixAja <phoenix@phoenixlogan.net>
2021-12-21 13:26:34 -0800

    tweak

--- src/backend/aspen/api/schemas/phylo_trees.py
+++ src/backend/aspen/api/schemas/phylo_trees.py
@@ -1,11 +1,11 @@
-from pydantic import constr, ConstrainedStr
+from pydantic import constr
 
 from aspen.api.schemas.base import BaseRequest, BaseResponse
 
 
 class PhyloTreeRequest(BaseRequest):
     id: int
-    name: ConstrainedStr = constr(min_length=1, max_length=128, strip_whitespace=True)  # type: ignore
+    name: constr(min_length=1, max_length=128, strip_whitespace=True)  # type: ignore
 
 
 class PhyloTreeResponse(BaseResponse):
