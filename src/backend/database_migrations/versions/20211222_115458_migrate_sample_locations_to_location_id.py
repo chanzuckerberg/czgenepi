5e4f35c192dc2834d2307c13d5669d513c663748
lvreynoso <lreynoso@chanzuckerberg.com>
2021-12-22 16:12:40 -0800

    Abandon check for existing columns in migration

--- src/backend/database_migrations/versions/20211222_115458_migrate_sample_locations_to_location_id.py
+++ src/backend/database_migrations/versions/20211222_115458_migrate_sample_locations_to_location_id.py
@@ -3,13 +3,9 @@
 Create Date: 2021-12-13 14:16:58.933547
 
 """
-from functools import reduce
-
 import enumtables  # noqa: F401
 import sqlalchemy as sa
 from alembic import op
-
-from aspen.database.models import meta, Sample
 
 # revision identifiers, used by Alembic.
 revision = "20211222_115458"
@@ -21,19 +17,6 @@ depends_on = None
 def upgrade():
     conn = op.get_bind()
 
-    samples_table = sa.schema.Table("samples", meta, autoload_with=conn)
-    sample_columns = [column.key for column in samples_table.columns]
-    deprecated_columns = ["region", "country", "division", "location"]
-    columns_present = reduce(
-        lambda count, column: count + 1 if column in sample_columns else count,
-        deprecated_columns,
-        0,
-    )
-    if not columns_present:
-        return
-    elif columns_present < len(deprecated_columns):
-        raise AssertionError("Unsupported database state.")
-
     set_full_locations_stmt = sa.sql.text(
         "UPDATE aspen.samples as s SET location_id = (SELECT id FROM aspen.locations as l WHERE l.region = s.region AND l.country = s.country AND l.division = s.division and l.location = s.location)"
     )
@@ -44,6 +27,7 @@ def upgrade():
     )
     conn.execute(set_broader_locations_stmt)
 
+    deprecated_columns = ["region", "country", "division", "location"]
     with op.batch_alter_table("samples", schema="aspen") as batch_op:
         for column in deprecated_columns:
             batch_op.drop_column(column)
