f19acc184f8048e81173031e997772b4b9a3cc1a
Jessica Gadling <jgadling@chanzuckerberg.com>
2022-01-04 16:43:26 -0800

    Fix samples update endpoint.

--- src/backend/aspen/api/views/samples.py
+++ src/backend/aspen/api/views/samples.py
@@ -17,8 +17,7 @@ from aspen.api.schemas.samples import (
     SampleDeleteResponse,
     SampleResponseSchema,
     SamplesResponseSchema,
-    UpdateSamplesRequest,
-    UpdateSamplesResponse
+    UpdateSamplesRequest
 )
 from aspen.api.settings import Settings
 from aspen.api.utils import authz_samples_cansee, determine_gisaid_status
@@ -225,7 +224,7 @@ async def update_samples(
     db: AsyncSession = Depends(get_db),
     settings: Settings = Depends(get_settings),
     user: User = Depends(get_auth_user),
-) -> UpdateSamplesResponse:
+) -> SamplesResponseSchema:
 
     # reorganize request data to make it easier to update
     reorganized_request_data = {
@@ -233,37 +232,28 @@ async def update_samples(
         for s in update_samples_request.samples
     }
     sample_ids_to_update = reorganized_request_data.keys()
+
     # Make sure these samples exist and are delete-able by the current user.
     sample_db_res = await get_owned_samples_by_ids(db, sample_ids_to_update, user)
     editable_samples = sample_db_res.all()
-
-    # # load the samples.
-    # all_samples_query = sa.select(Sample).options(  # type: ignore
-    #     selectinload(Sample.uploaded_pathogen_genome),
-    #     selectinload(Sample.submitting_group),
-    #     selectinload(Sample.uploaded_by),
-    #     selectinload(Sample.collection_location),
-    # )
-    # user_visible_samples_query = authz_samples_cansee(all_samples_query, None, user)
-    # user_visible_samples_result = await db.execute(user_visible_samples_query)
-    # editable_samples: List[Sample] = (
-    #     user_visible_samples_result.unique().scalars().all()
-    # )
 
     # are there any samples that can't be updated?
     uneditable_samples = [s for s in sample_ids_to_update if s not in [i.id for i in editable_samples]]
     if uneditable_samples:
         raise ex.NotFoundException("some samples cannot be updated")
 
-    # async with db as session:
+    res = SamplesResponseSchema(samples=[])
     for sample in editable_samples:
         update_data = reorganized_request_data[sample.id]
         for key, value in update_data:
-            if value:
+            if value != None:  # We need to be able to set private to False!
                 setattr(sample, key, value)
-
-        db.add(sample)
+        # Sequencing date is handled specially
+        if update_data.sequencing_date:
+            sample.uploaded_pathogen_genome.sequencing_date = update_data.sequencing_date
+        sample.show_private_identifier = True
+        res.samples.append(SampleResponseSchema.from_orm(sample))
 
     await db.commit()
 
-    return UpdateSamplesResponse(ids=[i for i in sample_ids_to_update])
+    return res
