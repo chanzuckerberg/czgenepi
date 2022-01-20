57cfe2c8ca76212381eae1f9b291eb43b732d818
lvreynoso <lreynoso@chanzuckerberg.com>
2022-01-20 09:50:07 -0800

    Fix downrevision identifier

--- src/backend/database_migrations/versions/20220119_153132_add_sample_accessions_table.py
+++ src/backend/database_migrations/versions/20220119_153132_add_sample_accessions_table.py
@@ -9,7 +9,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "20220119_153132"
-down_revision = "20220115_153132"
+down_revision = "20220115_011524"
 branch_labels = None
 depends_on = None
 
