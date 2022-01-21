57d5eb21b50b5e6c03448b7a9a2ae9ecb437c51a
lvreynoso <lreynoso@chanzuckerberg.com>
2022-01-21 13:01:35 -0800

    Update sample view tests

--- src/backend/aspen/api/views/tests/test_samples.py
+++ src/backend/aspen/api/views/tests/test_samples.py
@@ -11,6 +11,7 @@ from sqlalchemy.orm.exc import NoResultFound
 
 from aspen.api.schemas.base import convert_datetime_to_iso_8601
 from aspen.database.models import (
+    Accession,
     CanSee,
     DataType,
     Group,
@@ -168,11 +169,10 @@ async def test_samples_view_gisaid_no_info(
     location = location_factory(
         "North America", "USA", "California", "Santa Barbara County"
     )
-    sample = sample_factory(group, user, location)
+    sample = sample_factory(group, user, location, accessions={})
     # Test no GISAID accession logic
     uploaded_pathogen_genome = uploaded_pathogen_genome_factory(
         sample,
-        accessions=(),
     )
 
     async_session.add(group)
@@ -198,7 +198,7 @@ async def test_samples_view_gisaid_no_info(
                     "location": location.location,
                 },
                 "czb_failed_genome_recovery": False,
-                "gisaid": {"status": "Not Yet Submitted", "gisaid_id": None},
+                "gisaid": {"status": "Not Found", "gisaid_id": None},
                 "private_identifier": sample.private_identifier,
                 "public_identifier": sample.public_identifier,
                 "upload_date": convert_datetime_to_iso_8601(
@@ -283,76 +283,6 @@ async def test_samples_view_gisaid_not_eligible(
     assert response == expected
 
 
-async def test_samples_view_gisaid_submitted(
-    async_session: AsyncSession, http_client: AsyncClient
-):
-    group = group_factory()
-    user = user_factory(group)
-    location = location_factory(
-        "North America", "USA", "California", "Santa Barbara County"
-    )
-    sample = sample_factory(group, user, location)
-    # create a sample with a gisaid workflow but no accession yet
-    uploaded_pathogen_genome = uploaded_pathogen_genome_factory(
-        sample,
-        accessions=(
-            AccessionWorkflowDirective(
-                PublicRepositoryType.GISAID,
-                datetime.datetime.now(),
-                None,
-                None,
-            ),
-        ),
-    )
-    async_session.add(group)
-    await async_session.commit()
-
-    auth_headers = {"user_id": user.auth0_user_id}
-    res = await http_client.get(
-        "/v2/samples/",
-        headers=auth_headers,
-    )
-    response = res.json()
-    expected = {
-        "samples": [
-            {
-                "id": sample.id,
-                "collection_date": str(sample.collection_date),
-                "collection_location": {
-                    "id": location.id,
-                    "region": location.region,
-                    "country": location.country,
-                    "division": location.division,
-                    "location": location.location,
-                },
-                "czb_failed_genome_recovery": False,
-                "gisaid": {"status": "Submitted", "gisaid_id": None},
-                "private_identifier": sample.private_identifier,
-                "public_identifier": sample.public_identifier,
-                "upload_date": convert_datetime_to_iso_8601(
-                    uploaded_pathogen_genome.upload_date
-                ),
-                "sequencing_date": str(uploaded_pathogen_genome.sequencing_date),
-                "lineage": {
-                    "lineage": uploaded_pathogen_genome.pangolin_lineage,
-                    "probability": uploaded_pathogen_genome.pangolin_probability,
-                    "version": uploaded_pathogen_genome.pangolin_version,
-                    "last_updated": convert_datetime_to_iso_8601(
-                        uploaded_pathogen_genome.pangolin_last_updated
-                    ),
-                },
-                "private": False,
-                "submitting_group": {
-                    "id": group.id,
-                    "name": group.name,
-                },
-                "uploaded_by": {"id": user.id, "name": user.name},
-            }
-        ]
-    }
-    assert response == expected
-
-
 # Helper function for cansee tests
 async def _test_samples_view_cansee(
     async_session: AsyncSession,
@@ -379,14 +309,6 @@ async def _test_samples_view_cansee(
     )
     uploaded_pathogen_genome_factory(
         private_sample,
-        accessions=(
-            AccessionWorkflowDirective(
-                PublicRepositoryType.GISAID,
-                datetime.datetime.now(),
-                None,
-                None,
-            ),
-        ),
     )
 
     uploaded_pathogen_genome = uploaded_pathogen_genome_factory(sample)
@@ -532,7 +454,7 @@ async def test_samples_view_cansee_all(
             "czb_failed_genome_recovery": False,
             "gisaid": {
                 "status": "Accepted",
-                "gisaid_id": uploaded_pathogen_genome.accessions()[0].public_identifier,
+                "gisaid_id": sample.accessions[0].accession,
             },
             "private_identifier": sample.private_identifier,
             "public_identifier": sample.public_identifier,
@@ -559,171 +481,6 @@ async def test_samples_view_cansee_all(
             },
         }
     ]
-
-
-async def test_samples_failed_accession(
-    async_session: AsyncSession, http_client: AsyncClient
-):
-    """Add a sample with one successful and one failed accession attempt.  The samples
-    view should return the successful accession ID."""
-    group = group_factory()
-    user = user_factory(group)
-    location = location_factory(
-        "North America", "USA", "California", "Santa Barbara County"
-    )
-    sample = sample_factory(group, user, location)
-    uploaded_pathogen_genome = uploaded_pathogen_genome_factory(
-        sample,
-        accessions=(
-            # failed accession.
-            AccessionWorkflowDirective(
-                PublicRepositoryType.GISAID,
-                datetime.datetime.now() - datetime.timedelta(days=1, hours=2),
-                None,
-                None,
-            ),
-            AccessionWorkflowDirective(
-                PublicRepositoryType.GISAID,
-                datetime.datetime.now() - datetime.timedelta(days=1, hours=1),
-                datetime.datetime.now() - datetime.timedelta(days=1),
-                "public_identifier_succeeded",
-            ),
-        ),
-    )
-
-    for accession in uploaded_pathogen_genome.accessions():
-        print(type(accession))
-    async_session.add(group)
-    await async_session.commit()
-
-    auth_headers = {"user_id": user.auth0_user_id}
-    res = await http_client.get(
-        "/v2/samples/",
-        headers=auth_headers,
-    )
-    response = res.json()
-    expected = {
-        "samples": [
-            {
-                "id": sample.id,
-                "collection_date": str(sample.collection_date),
-                "collection_location": {
-                    "id": location.id,
-                    "region": location.region,
-                    "country": location.country,
-                    "division": location.division,
-                    "location": location.location,
-                },
-                "czb_failed_genome_recovery": False,
-                "gisaid": {
-                    "status": "Accepted",
-                    "gisaid_id": "public_identifier_succeeded",
-                },
-                "private_identifier": sample.private_identifier,
-                "public_identifier": sample.public_identifier,
-                "upload_date": convert_datetime_to_iso_8601(
-                    uploaded_pathogen_genome.upload_date
-                ),
-                "sequencing_date": str(uploaded_pathogen_genome.sequencing_date),
-                "lineage": {
-                    "lineage": uploaded_pathogen_genome.pangolin_lineage,
-                    "probability": uploaded_pathogen_genome.pangolin_probability,
-                    "version": uploaded_pathogen_genome.pangolin_version,
-                    "last_updated": convert_datetime_to_iso_8601(
-                        uploaded_pathogen_genome.pangolin_last_updated
-                    ),
-                },
-                "private": False,
-                "submitting_group": {
-                    "id": group.id,
-                    "name": group.name,
-                },
-                "uploaded_by": {"id": user.id, "name": user.name},
-            }
-        ]
-    }
-    assert response == expected
-
-
-async def test_samples_multiple_accession(
-    async_session: AsyncSession, http_client: AsyncClient
-):
-    """Add a sample with two successful accession attempts.  The samples view should
-    return the latest accession ID."""
-    group = group_factory()
-    user = user_factory(group)
-    location = location_factory(
-        "North America", "USA", "California", "Santa Barbara County"
-    )
-    sample = sample_factory(group, user, location)
-    uploaded_pathogen_genome = uploaded_pathogen_genome_factory(
-        sample,
-        accessions=(
-            # failed accession.
-            AccessionWorkflowDirective(
-                PublicRepositoryType.GISAID,
-                datetime.datetime.now() - datetime.timedelta(days=1, hours=2),
-                datetime.datetime.now() - datetime.timedelta(days=1, hours=1),
-                "public_identifier_earlier",
-            ),
-            AccessionWorkflowDirective(
-                PublicRepositoryType.GISAID,
-                datetime.datetime.now() - datetime.timedelta(days=1, hours=1),
-                datetime.datetime.now() - datetime.timedelta(days=1),
-                "public_identifier_later",
-            ),
-        ),
-    )
-    async_session.add(group)
-    await async_session.commit()
-
-    auth_headers = {"user_id": user.auth0_user_id}
-    res = await http_client.get(
-        "/v2/samples/",
-        headers=auth_headers,
-    )
-    response = res.json()
-    expected = {
-        "samples": [
-            {
-                "id": sample.id,
-                "collection_date": str(sample.collection_date),
-                "collection_location": {
-                    "id": location.id,
-                    "region": location.region,
-                    "country": location.country,
-                    "division": location.division,
-                    "location": location.location,
-                },
-                "czb_failed_genome_recovery": False,
-                "gisaid": {
-                    "status": "Accepted",
-                    "gisaid_id": "public_identifier_later",
-                },
-                "private_identifier": sample.private_identifier,
-                "public_identifier": sample.public_identifier,
-                "upload_date": convert_datetime_to_iso_8601(
-                    uploaded_pathogen_genome.upload_date
-                ),
-                "sequencing_date": str(uploaded_pathogen_genome.sequencing_date),
-                "lineage": {
-                    "lineage": uploaded_pathogen_genome.pangolin_lineage,
-                    "probability": uploaded_pathogen_genome.pangolin_probability,
-                    "version": uploaded_pathogen_genome.pangolin_version,
-                    "last_updated": convert_datetime_to_iso_8601(
-                        uploaded_pathogen_genome.pangolin_last_updated
-                    ),
-                },
-                "private": False,
-                "submitting_group": {
-                    "id": group.id,
-                    "name": group.name,
-                },
-                "uploaded_by": {"id": user.id, "name": user.name},
-            }
-        ]
-    }
-    assert response == expected
 
 
 async def test_samples_view_no_pangolin(
@@ -766,9 +523,7 @@ async def test_samples_view_no_pangolin(
                 "czb_failed_genome_recovery": False,
                 "gisaid": {
                     "status": "Accepted",
-                    "gisaid_id": uploaded_pathogen_genome.accessions()[
-                        0
-                    ].public_identifier,
+                    "gisaid_id": sample.accessions[0].accession,
                 },
                 "private_identifier": sample.private_identifier,
                 "public_identifier": sample.public_identifier,
@@ -860,6 +615,23 @@ async def test_delete_sample_success(
             rows += 1
             continue
         assert row.id == samples[2].id
+        rows += 1
+    # Make sure we actually processed the results above.
+    assert rows == 3
+
+    # Check that Accessions were deleted
+    rows = 0
+    for sample in samples:
+        res = await async_session.execute(
+            sa.select(Accession).filter(Accession.sample_id == sample.id)  # type: ignore
+        )
+        row = res.scalars().all()  # type: ignore
+        if not row:
+            assert sample.id in [samples[0].id, samples[1].id]
+            rows += 1
+            continue
+        for item in row:
+            assert item.sample_id == samples[2].id
         rows += 1
     # Make sure we actually processed the results above.
     assert rows == 3
