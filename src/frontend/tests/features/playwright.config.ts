3e4c610b75d0def4295e32ca16804d350719d0d6
Jessica Gadling <jgadling@chanzuckerberg.com>
2022-01-25 13:36:07 -0800

    Enable automatic screengrabs.

--- src/frontend/tests/features/playwright.config.ts
+++ src/frontend/tests/features/playwright.config.ts
@@ -2,6 +2,7 @@ import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
     timeout: 60000,
     use: {
+        screenshot: 'only-on-failure',
 	ignoreHTTPSErrors: true,
     },
 };
