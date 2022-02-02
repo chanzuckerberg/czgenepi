a0354e3f6ed5036225cb25afee33a6124e3ce0d2
Jessica Gadling <jgadling@chanzuckerberg.com>
2022-02-01 17:46:46 -0800

    bump

--- src/backend/aspen/api/utils/__init__.py
+++ src/backend/aspen/api/utils/__init__.py
@@ -3,7 +3,6 @@ from aspen.api.utils.find_samples_by_id import (  # noqa: F401
     get_matching_gisaid_ids,
     get_missing_and_found_sample_ids,
 )
-from aspen.api.utils.gisaid import get_matching_gisaid_ids  # noqa: F401
 from aspen.api.utils.sample import (  # noqa: F401
     determine_gisaid_status,
     format_sample_lineage,
