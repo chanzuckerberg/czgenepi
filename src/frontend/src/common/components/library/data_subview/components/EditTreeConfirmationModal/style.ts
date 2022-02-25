477e0c4610e9613f3fb0293eafd5ff08048151c8
phoenixAja <phoenix@phoenixlogan.net>
2022-02-25 14:15:13 -0800

    fixing tree edit modal stylings

--- src/frontend/src/common/components/library/data_subview/components/EditTreeConfirmationModal/style.ts
+++ src/frontend/src/common/components/library/data_subview/components/EditTreeConfirmationModal/style.ts
@@ -1,14 +1,25 @@
 import styled from "@emotion/styled";
+import IconButton from "@material-ui/core/IconButton";
 import { fontHeaderXl, getSpaces } from "czifui";
 
 export const StyledTitle = styled.div`
   ${fontHeaderXl}
+`;
 
+export const StyledDiv = styled.div`
   ${(props) => {
     const spaces = getSpaces(props);
-
     return `
-      margin-bottom: ${spaces?.xl}px;
+      padding-top: ${spaces?.l}px;
     `;
   }}
+`;
+
+export const StyledIconButton = styled(IconButton)`
+  display: flex;
+  align-self: flex-end;
+  padding: 0;
+  &:hover {
+    background-color: transparent;
+  }
 `;
