431ce5fdb50fc79e08d96092493ef5450dac9a53
Maya Lila <mlila@chanzuckerberg.com>
2022-03-03 13:59:22 -0800

    use a redirect instead of a copy

--- src/frontend/tests/staticFiles/security.test.ts
+++ src/frontend/tests/staticFiles/security.test.ts
@@ -12,19 +12,8 @@ const checkExpirationDate = (path: string) => {
   if (expirationDate < now) throw Error("security.txt files need updating");
 };
 
-test("ensure first security.txt file has not expired", () => {
-  expect(() => checkExpirationDate("public/security.txt")).not.toThrow();
-});
-
 test("ensure second security.txt file has not expired", () => {
   expect(() =>
     checkExpirationDate("public/.well-known/security.txt")
   ).not.toThrow();
-});
-
-test("ensure security files in sync", () => {
-  const file = readFileSync("public/security.txt").toString();
-  const file2 = readFileSync("public/.well-known/security.txt").toString();
-
-  expect(file == file2).toBeTruthy();
 });
