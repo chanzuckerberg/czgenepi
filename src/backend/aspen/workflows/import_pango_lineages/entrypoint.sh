8b5e3d2dbe99ce12103cfc0e8f50605436b62f5a
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2022-03-18 16:56:20 -0700

    Rename import.py ... not a good name for Python

--- src/backend/aspen/workflows/import_pango_lineages/entrypoint.sh
+++ src/backend/aspen/workflows/import_pango_lineages/entrypoint.sh
@@ -28,4 +28,4 @@ s3_destination="s3://${ASPEN_S3_DB_BUCKET}/${lineages_key}"
 aws s3 cp $filename $s3_destination
 
 # Parse lineages file and load into DB
-python3 ./import.py --lineages-file $filename
+python3 ./load_lineages.py --lineages-file $filename
--- src/backend/aspen/workflows/test-pangolin.sh
+++ src/backend/aspen/workflows/test-pangolin.sh
@@ -5,4 +5,4 @@ cd /usr/src/app/aspen/workflows/pangolin
 cd /usr/src/app/aspen/workflows/import_pango_lineages
 # Script has a required file arg, so make a dummy to be able to run smoke test
 touch test.txt
-/usr/local/bin/python3.9 import.py --test --lineages-file test.txt
+/usr/local/bin/python3.9 load_lineages.py --test --lineages-file test.txt
