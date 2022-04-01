177647410a0ae61bba133aaf18cad648a4ce8adf
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2022-04-01 11:41:02 -0700

    Fix my screwed up grammar

--- src/backend/aspen/api/views/pango_lineages.py
+++ src/backend/aspen/api/views/pango_lineages.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from aspen.api.deps import get_db
 from aspen.api.schemas.pango_lineages import PangoLineagesResponse
-from aspen.database.models import PangoLineages
+from aspen.database.models import PangoLineage
 
 router = APIRouter()
 
@@ -19,6 +19,6 @@ async def list_pango_lineages(db: AsyncSession = Depends(get_db)):
     lineage info. The only real info we have is the names of the lineages,
     so that's all we pull and return.
     """
-    all_lineages_query = sa.select(PangoLineages.lineage)  # type: ignore
+    all_lineages_query = sa.select(PangoLineage.lineage)  # type: ignore
     result = await db.execute(all_lineages_query)
     return {"all_pango_lineages": result.scalars().all()}
--- src/backend/aspen/database/models/pango_lineages.py
+++ src/backend/aspen/database/models/pango_lineages.py
@@ -3,15 +3,17 @@ from sqlalchemy import Column, String
 from aspen.database.models.base import idbase
 
 
-class PangoLineages(idbase):  # type: ignore
-    """List of Pango lineages.
+class PangoLineage(idbase):  # type: ignore
+    """A Pango lineage. Only real data is its official name (`lineage`).
 
-    Should generally duplicate those present at
+    Entire table taken together should be all the current Pango lineages.
+    This table gets regularly updated by a data workflow.
+    See workflow named `import_pango_lineages` for that process.
+
+    Intent of this table and the workflow is to duplicate info at
     https://raw.githubusercontent.com/cov-lineages/pango-designation/master/lineage_notes.txt
     According to Pangolin team, that is the best indication of current list
     ^^^ Source: https://github.com/cov-lineages/pango-designation/issues/456
-
-    This table gets regularly updated by a data job.
     """
 
     __tablename__ = "pango_lineages"
