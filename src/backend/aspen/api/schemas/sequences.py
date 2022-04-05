ac286ce9273542bfb8d2ed917971816dd8be656c
lvreynoso <lreynoso@chanzuckerberg.com>
2022-04-05 16:22:07 -0700

    Port getfastaurl to fastapi

--- src/backend/aspen/api/schemas/sequences.py
+++ src/backend/aspen/api/schemas/sequences.py
@@ -3,3 +3,10 @@ from aspen.api.schemas.base import BaseRequest
 
 class SequenceRequest(BaseRequest):
     sample_ids: list[str]
+
+class FastaURLRequest(BaseRequest):
+    samples: list[str]
+    downstream_consumer: str
+
+class FastaURLResponse(BaseResponse):
+    url: str
