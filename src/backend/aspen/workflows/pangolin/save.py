9bc13018f32a396e0f4ed66c559fa4cef694bde7
Lucia Reynoso <lreynoso@chanzuckerberg.com>
2022-04-06 15:16:11 -0700

    Change key for saving pango version (#1009)
    * Change key for saving pango version
    
    * Update get_probability() and fix tests
    
    * Remove accidental fasta commits
    
    * Lint

--- src/backend/aspen/workflows/pangolin/save.py
+++ src/backend/aspen/workflows/pangolin/save.py
@@ -24,7 +24,10 @@ def get_probability(row: dict) -> Optional[int]:
     """
     if row["ambiguity_score"]:
         return round(float(row["ambiguity_score"]) * 100)
-    elif "Assigned using designation hash" in row["note"]:
+    elif (
+        "Assigned using designation hash" in row["note"]
+        or "Assigned from designation hash" in row["note"]
+    ):
         return 100
     else:
         return None
@@ -44,7 +47,7 @@ def cli(pangolin_fh: io.TextIOBase, pangolin_last_updated: datetime):
             int(row["taxon"]): {
                 "lineage": row["lineage"],
                 "probability": get_probability(row),
-                "version": row["pangoLEARN_version"],
+                "version": row["version"],
             }
             for row in pango_csv
         }
