9d024d5d3ee6466e74089974dafddcfdf0819bcc
maya <mlila@chanzuckerberg.com>
2022-04-14 13:53:05 -0700

    Maya/warnings (#1023)
    * consolidate pluralize fxns
    
    * fix existing type warnings
    
    * add new warning types
    
    * remove unnecessary wrappers
    
    * refactor alertaccordion to use sds
    
    * lint

--- src/frontend/src/views/Upload/components/Metadata/components/ImportFile/components/Alerts/common/pluralize.ts
+++
@@ -1,9 +0,0 @@
-const WORDS_TO_PLURALIZE: Record<string, string> = {
-  Sample: "Samples",
-  sample: "samples",
-  was: "were",
-};
-
-export function maybePluralize(word: string, count: number): string {
-  return count > 1 ? WORDS_TO_PLURALIZE[word] : word;
-}
