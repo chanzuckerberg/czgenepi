e60951d6cdb28571b0a87a72d6fe6b218c3024a8
maya <mlila@chanzuckerberg.com>
2022-04-21 13:44:04 -0700

    Maya/184616 date range menu (#1040)
    * add collection date filter to ns tree modal
    
    * types
    
    * Update src/frontend/src/components/DateFilterMenu/constants.ts
    
    Co-authored-by: Phoenix Logan <phoenix@phoenixlogan.net>
    
    Co-authored-by: Phoenix Logan <phoenix@phoenixlogan.net>

---
+++ src/frontend/src/common/components/library/data_subview/components/CreateNSTreeModal/components/SampleFiltering/components/CollectionDateFilter/style.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+import { InputDropdown } from "czifui";
+import { DateFilterMenu } from "src/components/DateFilterMenu";
+
+export const StyledInputDropdown = styled(InputDropdown)`
+  width: 224px;
+`;
+
+export const StyledDateFilterMenu = styled(DateFilterMenu)`
+  max-height: 290px;
+  min-width: 270px;
+`;
--- src/frontend/src/common/types/dateFilter.d.ts
+++ src/frontend/src/common/types/dateFilter.d.ts
@@ -11,5 +11,5 @@ type DateMenuOption = {
   // For both `numDays...` it's relative to now() when option is chosen.
   // Assume start is guaranteed, but end cap is not.
   numDaysEndOffset?: number; // How far back end of date interval is
-  numDaysStartOffset: number; // How far back start of date interval is
+  numDaysStartOffset?: number; // How far back start of date interval is
 };
