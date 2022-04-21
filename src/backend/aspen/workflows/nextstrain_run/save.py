aa5daeea590e7ae1903cd9d5bb9b937dde1e4bce
Jessica Gadling <jgadling@chanzuckerberg.com>
2022-04-20 17:17:06 -0700

    Patch.

--- src/backend/aspen/workflows/nextstrain_run/save.py
+++ src/backend/aspen/workflows/nextstrain_run/save.py
@@ -58,9 +58,8 @@ def cli(
             session.query(PhyloRun)
             .filter(PhyloRun.workflow_id == phylo_run_id)
             .options(
-                joinedload(PhyloRun.inputs.of_type(UploadedPathogenGenome))
                 # load the sample that this uploaded pathogen genome was associated with
-                ,
+                joinedload(PhyloRun.inputs.of_type(UploadedPathogenGenome))
             )
             .one()
         )
