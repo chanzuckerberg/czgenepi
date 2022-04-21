05390e9bc919e898e8273dc898a857d969c60825
Maya Lila <mlila@chanzuckerberg.com>
2022-04-20 23:39:18 -0700

    move date constants into constants file

---
+++ src/frontend/src/components/DateFilterMenu/constants.ts
@@ -0,0 +1,41 @@
+import { DateMenuOption } from "src/components/DateFilterMenu";
+
+export const MENU_OPTIONS_UPLOAD_DATE: DateMenuOption[] = [
+  {
+    name: "Today",
+    numDaysStartOffset: 0,
+  },
+  {
+    name: "Yesterday",
+    numDaysEndOffset: 1,
+    numDaysStartOffset: 1,
+  },
+  {
+    name: "Last 7 Days",
+    numDaysStartOffset: 7,
+  },
+];
+
+export const MENU_OPTIONS_COLLECTION_DATE: DateMenuOption[] = [
+  {
+    name: "Last 7 Days",
+    numDaysStartOffset: 7,
+  },
+  {
+    name: "Last 30 Days",
+    numDaysStartOffset: 30,
+  },
+  // Average month has ~30.4 days, so that's why the non-round numbers
+  {
+    name: "Last 3 Months",
+    numDaysStartOffset: 91,
+  },
+  {
+    name: "Last 6 Months",
+    numDaysStartOffset: 182,
+  },
+  {
+    name: "Last Year",
+    numDaysStartOffset: 365,
+  },
+];
