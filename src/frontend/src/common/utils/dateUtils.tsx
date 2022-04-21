e283c9bfa845a04d2f7e82bf0f9932a96af4a796
Maya Lila <mlila@chanzuckerberg.com>
2022-04-21 10:43:04 -0700

    types

--- src/frontend/src/common/utils/dateUtils.tsx
+++ src/frontend/src/common/utils/dateUtils.tsx
@@ -8,12 +8,12 @@ export const getDateRangeLabel = ({
   currentLabel,
   startDate,
   endDate,
-}: Args): string | null => {
+}: Args): string => {
   // If a date menu option was selected, just use its specific name instead
   if (currentLabel) return currentLabel;
 
   // DateChip should only display if we have at least one of startDate/endDate
-  if (!startDate && !endDate) return null;
+  if (!startDate && !endDate) return "";
 
   // Get the date chip message. Structure varies if only one of the two dates.
   // Might be worth extracting date message to a common helper func elsewhere?
