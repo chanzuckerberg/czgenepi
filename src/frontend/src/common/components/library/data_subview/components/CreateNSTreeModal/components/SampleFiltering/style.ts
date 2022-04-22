2574d57cb1547bd5ac19a3ede74c8be140282ee2
vincent-czi <89553795+vincent-czi@users.noreply.github.com>
2022-04-22 14:10:03 -0700

    Lineage filter for Sample Filtering (#1027)
    * API call to pull lineages from BE
    
    * WIP: Plumbing for passing down lineages
    
    * WIP: Create dropdown options list to present
    
    * Majority of lineages selection logic
    
    * Ensure "All" always filtered; avoid slow renders
    
    * The styling is done, but at what cost?
    
    * Bugfix mild hack; CSS my enemy and shield
    
    * Grammar fixes
    
    * Remove no longer true comment

--- src/frontend/src/common/components/library/data_subview/components/CreateNSTreeModal/components/SampleFiltering/style.ts
+++ src/frontend/src/common/components/library/data_subview/components/CreateNSTreeModal/components/SampleFiltering/style.ts
@@ -1,5 +1,11 @@
 import styled from "@emotion/styled";
-import { fontHeaderXs, getColors, getSpaces, InputDropdown } from "czifui";
+import {
+  Dropdown,
+  DropdownPopper,
+  fontHeaderXs,
+  getColors,
+  getSpaces,
+} from "czifui";
 import { NewTabLink } from "src/common/components/library/NewTabLink";
 
 export const StyledContainer = styled.div`
@@ -62,6 +68,25 @@ export const StyledNewTabLink = styled(NewTabLink)`
   }}
 `;
 
-export const StyledInputDropdown = styled(InputDropdown)`
-  width: 200px;
+const DROPDOWN_WIDTH = "200px";
+
+// High specificity label styling is to beat out czifui coloring.
+export const StyledDropdown = styled(Dropdown)`
+  width: ${DROPDOWN_WIDTH};
+  .MuiButton-label > span {
+    color: black;
+  }
+` as typeof Dropdown; // assert b/c `styled` causes an interface hiccup;
+
+// The `min-width` property is necessary to cancel out a hardcoded czifui
+// value since we want to set a smaller `width` than that hardcoded number
+export const StyledDropdownPopper = styled(DropdownPopper)`
+  min-width: auto;
+  width: ${DROPDOWN_WIDTH};
+  ${(props) => {
+    const spaces = getSpaces(props);
+    return `
+      margin-top: ${spaces?.xs}px;
+    `;
+  }}
 `;
