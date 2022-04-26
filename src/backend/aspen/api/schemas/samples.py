c82e46f57aad2f8354721ba3dcaa1b3597882a1d
Jessica Gadling <jgadling@chanzuckerberg.com>
2022-04-26 10:01:08 -0700

    Support scorpio call/score. (#1042)
    * Support scorpio call/score.

--- src/backend/aspen/api/schemas/samples.py
+++ src/backend/aspen/api/schemas/samples.py
@@ -26,6 +26,9 @@ class SampleLineageResponse(BaseResponse):
     lineage: Optional[str]
     confidence: Optional[float]
     version: Optional[str]
+    scorpio_call: Optional[str]
+    scorpio_support: Optional[float]
+    qc_status: Optional[str]
 
 
 class SampleGroupResponse(BaseResponse):
