cef3f82cec8af3f36b9b7e2811db006b19cc0568
Jessica Gadling <jgadling@chanzuckerberg.com>
2022-04-27 17:23:43 -0700

    Move type definitions.

--- src/frontend/src/common/queries/auth.ts
+++ src/frontend/src/common/queries/auth.ts
@@ -19,6 +19,20 @@ export const USE_USER_INFO = {
   id: "userInfo",
 };
 
+interface RawGroupRequest {
+  id: number;
+  name: string;
+}
+
+interface RawUserRequest {
+  id: number;
+  name: string;
+  group: RawGroupRequest;
+  agreed_to_tos: boolean;
+  acknowledged_policy_version: string | null; // Date or null in DB. ISO 8601: "YYYY-MM-DD"
+  split_id: string;
+}
+
 const mapUserData = (obj: any): User => {
   return {
     acknowledgedPolicyVersion: obj.acknowledged_policy_version,
--- src/frontend/src/common/types/user.d.ts
+++ src/frontend/src/common/types/user.d.ts
@@ -11,17 +11,3 @@ interface User {
   acknowledgedPolicyVersion: string | null; // Date or null in DB. ISO 8601: "YYYY-MM-DD"
   splitId: string;
 }
-
-interface RawGroupRequest {
-  id: number;
-  name: string;
-}
-
-interface RawUserRequest {
-  id: number;
-  name: string;
-  group: RawGroupRequest;
-  agreed_to_tos: boolean;
-  acknowledged_policy_version: string | null; // Date or null in DB. ISO 8601: "YYYY-MM-DD"
-  split_id: string;
-}
