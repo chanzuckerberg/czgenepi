a4f40bcadd5b1254b454c562bc9c90d2becca59e
vincent-czi <89553795+vincent-czi@users.noreply.github.com>
2022-05-02 10:06:57 -0700

    Segment Analytics Initialization -- TEMPORARY (#1060)
    * Init process for Segment
    
    * wut? Errors from prettier on build??
    
    * appease cruel, fickle ESLint

--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -7,6 +7,7 @@ import React, { useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "semantic-ui-css/semantic.min.css";
 import style from "src/App.module.scss";
+import { SegmentInitializer } from "src/common/analytics/SegmentInitializer";
 import { ROUTES } from "src/common/routes";
 import { theme } from "src/common/styles/theme";
 import { setFeatureFlagsFromQueryParams } from "src/common/utils/featureFlags";
@@ -46,6 +47,7 @@ const App = ({ Component, pageProps }: AppProps): JSX.Element => {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
+      <SegmentInitializer />
       <QueryClientProvider client={queryClient}>
         <SplitInitializer>
           <StylesProvider injectFirst>
