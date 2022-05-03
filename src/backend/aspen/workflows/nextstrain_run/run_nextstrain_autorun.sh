a4ff334eaacc70e13d2aee471050fe0d64bd76ce
Lucia Reynoso <lreynoso@chanzuckerberg.com>
2022-05-03 14:08:06 -0700

    Automate scheduled group runs (#1066)

---
+++ src/backend/aspen/workflows/nextstrain_run/run_nextstrain_autorun.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# WDL inputs available through environmental vars:
+# AWS_REGION
+# GENEPI_CONFIG_SECRET_NAME
+# REMOTE_DEV_PREFIX (if set)
+# DEPLOYMENT_STAGE
+
+set -Eeuxo pipefail
+shopt -s inherit_errexit
+
+df 1>&2
+cat /proc/meminfo 1>&2
+
+start_time=$(date +%s)
+
+aws configure set region $AWS_REGION
+
+if [ -n "${BOTO_ENDPOINT_URL-}" ]; then
+  export aws="aws --endpoint-url ${BOTO_ENDPOINT_URL}"
+else
+  export aws="aws"
+fi
+
+# fetch aspen config
+set +x  # don't echo secrets
+echo "* set \$genepi_config"
+genepi_config="$($aws secretsmanager get-secret-value --secret-id $GENEPI_CONFIG_SECRET_NAME --query SecretString --output text)"
+echo "* set \$aspen_s3_db_bucket"
+aspen_s3_db_bucket="$(jq -r .S3_db_bucket <<< "$genepi_config")"
+set -x
+
+python3 /usr/src/app/aspen/workflows/nextstrain_run/autorun_scheduled.py
