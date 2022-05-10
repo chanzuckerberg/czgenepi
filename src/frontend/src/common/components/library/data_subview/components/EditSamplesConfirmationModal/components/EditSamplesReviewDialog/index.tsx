6d7a073d3b660cc293fc81932b72459c2ebfa057
maya <mlila@chanzuckerberg.com>
2022-05-10 13:57:46 -0700

    Maya/180631 sample review (#1077)
    * conditionally show different views in the modal
    
    * basic review modal
    
    * starter table
    
    * ew, yucky spacing
    
    * types

---
+++ src/frontend/src/common/components/library/data_subview/components/EditSamplesConfirmationModal/components/EditSamplesReviewDialog/index.tsx
@@ -0,0 +1,94 @@
+import { Checkbox } from "czifui";
+import React, { useState } from "react";
+import { NewTabLink } from "src/common/components/library/NewTabLink";
+import { ROUTES } from "src/common/routes";
+import { B } from "src/common/styles/basicStyle";
+import { pluralize } from "src/common/utils/strUtils";
+import AlertAccordion from "src/components/AlertAccordion";
+import { Table } from "./components/Table";
+import {
+  CalloutContainer,
+  CheckboxWrapper,
+  StyledButton,
+  StyledCallout,
+  StyledCollapseContent,
+  StyledNewTabLink,
+} from "./style";
+
+interface Props {
+  changedMetaData: any;
+  onClickBack(): void;
+}
+
+const EditSamplesReviewDialog = ({
+  changedMetaData,
+  onClickBack,
+}: Props): JSX.Element => {
+  const [isChecked, setChecked] = useState<boolean>(false);
+  const numChangedSamples = changedMetaData.length ?? 0;
+  const warningTitle = (
+    <>
+      <B>
+        {numChangedSamples} {pluralize("Sample", numChangedSamples)} have
+        updated privacy settings,
+      </B>{" "}
+      which will impact who can see your de-identified sample data. Read our{" "}
+      <StyledNewTabLink href={ROUTES.PRIVACY}>Privacy Policy</StyledNewTabLink>{" "}
+      for more information.
+    </>
+  );
+
+  const collapseContent = (
+    <>
+      <StyledCollapseContent>
+        <B>Changed Samples (Private ID):</B>
+      </StyledCollapseContent>
+    </>
+  );
+
+  return (
+    <>
+      <Table metadata={{}} />
+      <CheckboxWrapper>
+        <Checkbox
+          stage={isChecked ? "checked" : "unchecked"}
+          onClick={() => setChecked(!isChecked)}
+        />
+        <div>
+          I agree that the data I am uploading to CZ GEN EPI has been lawfully
+          collected and that I have all the necessary consents, permissions, and
+          authorizations needed to collect, share and export data as outlined in
+          the <NewTabLink href={ROUTES.TERMS}>Terms</NewTabLink> and{" "}
+          <NewTabLink href={ROUTES.PRIVACY}>Privacy Policy</NewTabLink>. I have
+          reviewed the data that I am uploading and can confirm that I am not
+          uploading any personally identifiable information.
+        </div>
+      </CheckboxWrapper>
+      <CalloutContainer>
+        <StyledCallout intent="info">
+          Once saved, the sample metadata above will overwrite all exisiting
+          data. <B>This action cannot be undone.</B>
+        </StyledCallout>
+        <AlertAccordion
+          intent="warning"
+          title={warningTitle}
+          collapseContent={collapseContent}
+        ></AlertAccordion>
+      </CalloutContainer>
+      <div>
+        <StyledButton sdsType="primary" sdsStyle="rounded">
+          Save
+        </StyledButton>
+        <StyledButton
+          sdsType="secondary"
+          sdsStyle="rounded"
+          onClick={onClickBack}
+        >
+          Back
+        </StyledButton>
+      </div>
+    </>
+  );
+};
+
+export { EditSamplesReviewDialog };
