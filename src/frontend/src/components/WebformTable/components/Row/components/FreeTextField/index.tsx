c0a9bd78fec1ab362a40da0ca5c4c438f27727c8
Phoenix Logan <phoenix@phoenixlogan.net>
2022-05-17 14:43:22 -0700

    edit cells highlighting (#1088)
    Co-authored-by: maya <mlila@chanzuckerberg.com>

--- src/frontend/src/components/WebformTable/components/Row/components/FreeTextField/index.tsx
+++ src/frontend/src/components/WebformTable/components/Row/components/FreeTextField/index.tsx
@@ -1,5 +1,5 @@
 import { FormikContextType } from "formik";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Metadata } from "src/components/WebformTable/common/types";
 import { StyledTextField } from "./style";
 
@@ -7,14 +7,31 @@ interface Props {
   fieldKey: keyof Metadata;
   formik: FormikContextType<Metadata>;
   isShown?: boolean;
+  shouldShowEditedInputAsMarked?: boolean; // used to mark edited cells as purple for crud
 }
 
 export default function FreeTextField({
   fieldKey,
   formik,
   isShown = true,
+  shouldShowEditedInputAsMarked = false,
 }: Props): JSX.Element | null {
-  const { handleChange, handleBlur, values, touched, errors } = formik;
+  const [isBackgroundColorShown, setIsBackgroundColorShown] =
+    useState<boolean>(false);
+
+  const { handleChange, handleBlur, values, touched, errors, initialValues } =
+    formik;
+
+  const initialValue = initialValues[fieldKey];
+  const changedValue = values[fieldKey];
+
+  useEffect(() => {
+    if (initialValue !== changedValue && shouldShowEditedInputAsMarked) {
+      setIsBackgroundColorShown(true);
+    } else {
+      setIsBackgroundColorShown(false);
+    }
+  }, [initialValue, changedValue, shouldShowEditedInputAsMarked]);
 
   const errorMessage = touched[fieldKey] && errors[fieldKey];
 
@@ -30,6 +47,7 @@ export default function FreeTextField({
       value={values[fieldKey] || ""}
       error={Boolean(errorMessage)}
       helperText={errorMessage}
+      isBackgroundColorShown={isBackgroundColorShown}
     />
   );
 }
