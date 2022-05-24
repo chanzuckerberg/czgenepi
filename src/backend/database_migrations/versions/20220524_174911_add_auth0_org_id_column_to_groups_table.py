35a55ea9656c90e897e4a39399a7ba193c3d6d03
lvreynoso <lreynoso@chanzuckerberg.com>
2022-05-24 10:55:54 -0700

    Add auth0_org_id column to groups table

---
+++ src/backend/database_migrations/versions/20220524_174911_add_auth0_org_id_column_to_groups_table.py
@@ -0,0 +1,29 @@
+"""Add auth0 org id column to groups table
+
+Create Date: 2022-05-24 17:49:12.876167
+
+"""
+import enumtables  # noqa: F401
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "20220524_174911"
+down_revision = "20220503_215541"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        "groups",
+        sa.Column("auth0_org_id", sa.String(), nullable=True),
+        schema="aspen",
+    )
+    op.create_unique_constraint(
+        op.f("uq_groups_auth0_org_id"), "groups", ["auth0_org_id"], schema="aspen"
+    )
+
+
+def downgrade():
+    pass
