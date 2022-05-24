29a688d3d74c1e47ec11a363ab57f41ac0a4f120
Maya Lila <mlila@chanzuckerberg.com>
2022-05-24 10:35:10 -0700

    genericize app padding

--- src/frontend/src/common/styles/mixins/global.ts
+++ src/frontend/src/common/styles/mixins/global.ts
@@ -1,6 +1,15 @@
 import styled from "@emotion/styled";
+import { CommonThemeProps, getSpaces } from "czifui";
 import { NAV_BAR_HEIGHT_PX } from "src/components/NavBar";
 
 export const PageContent = styled.div`
   height: calc(100% - ${NAV_BAR_HEIGHT_PX}px);
 `;
+
+export const ContentStyles = (props: CommonThemeProps) => {
+  const spaces = getSpaces(props);
+
+  return `
+    padding: ${spaces?.xl}px 125px ${spaces?.l}px 125px;
+  `;
+};
