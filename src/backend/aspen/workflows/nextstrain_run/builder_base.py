18a1867af74342fd23673069f6c8c248e79e0887
Dan Lu <dlu@chanzuckerberg.com>
2022-06-14 15:43:19 -0500

    lint

--- src/backend/aspen/workflows/nextstrain_run/builder_base.py
+++ src/backend/aspen/workflows/nextstrain_run/builder_base.py
@@ -60,17 +60,17 @@ class BaseNextstrainConfigBuilder:
         if (self.template_args.get("filter_start_date") is not None) and (self.template_args.get("filter_end_date") is not None):
             title_template = "{tree_type} tree for samples collected in {location} between {start_date} and {end_date}"
             build["title"] = title_template.format(
-                        tree_type=self.subsampling_scheme.title(),
-                        location=", ".join(location_values),
-                        start_date=dateparser.parse(self.template_args.get("filter_start_date")).strftime("%Y-%m-%d"),
-                        end_date=dateparser.parse(self.template_args.get("filter_end_date")).strftime("%Y-%m-%d")
-                    )
+                tree_type=self.subsampling_scheme.title(),
+                location=", ".join(location_values),
+                start_date=dateparser.parse(self.template_args.get("filter_start_date")).strftime("%Y-%m-%d"),
+                end_date=dateparser.parse(self.template_args.get("filter_end_date")).strftime("%Y-%m-%d")
+            )
         else:
             title_template = "{tree_type} tree for samples collected in {location}"
             build["title"] = title_template.format(
-                        tree_type=self.subsampling_scheme.title(),
-                        location=", ".join(location_values)
-                    )
+                tree_type=self.subsampling_scheme.title(),
+                location=", ".join(location_values)
+            )
 
         config["files"]["description"] = config["files"]["description"].format(
             tree_type=self.subsampling_scheme.lower()
