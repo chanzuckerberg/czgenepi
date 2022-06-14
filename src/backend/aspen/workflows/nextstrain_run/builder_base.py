e12f723ce9fae6015a749ae0928b56d023a3fc96
Dan Lu <dlu@chanzuckerberg.com>
2022-06-14 18:22:38 -0500

    lint hehehehe

--- src/backend/aspen/workflows/nextstrain_run/builder_base.py
+++ src/backend/aspen/workflows/nextstrain_run/builder_base.py
@@ -57,13 +57,19 @@ class BaseNextstrainConfigBuilder:
         build["subsampling_scheme"] = self.subsampling_scheme
 
         # Update the tree's title with build type, location and date range.
-        if (self.template_args.get("filter_start_date") is not None) and (self.template_args.get("filter_end_date") is not None):
+        if (self.template_args.get("filter_start_date") is not None) and (
+            self.template_args.get("filter_end_date") is not None
+        ):
             title_template = "{tree_type} tree for samples collected in {location} between {start_date} and {end_date}"
             build["title"] = title_template.format(
                 tree_type=self.subsampling_scheme.title(),
                 location=", ".join(location_values),
-                start_date=dateparser.parse(self.template_args.get("filter_start_date")).strftime("%Y-%m-%d"),
-                end_date=dateparser.parse(self.template_args.get("filter_end_date")).strftime("%Y-%m-%d")
+                start_date=dateparser.parse(
+                    self.template_args.get("filter_start_date")
+                ).strftime("%Y-%m-%d"),
+                end_date=dateparser.parse(
+                    self.template_args.get("filter_end_date")
+                ).strftime("%Y-%m-%d")
             )
         else:
             title_template = "{tree_type} tree for samples collected in {location}"
