c4b61e79aaa095eb5b2524378a54a9059c24fb06
Maya Lila <mlila@chanzuckerberg.com>
2022-06-15 13:46:35 -0700

    dont reload page hen viewing invitations from notification

--- src/frontend/src/views/GroupMembersPage/components/MembersTab/components/InviteModal/components/InvalidEmailError/components/SentNotification/index.tsx
+++ src/frontend/src/views/GroupMembersPage/components/MembersTab/components/InviteModal/components/InvalidEmailError/components/SentNotification/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from "czifui";
+import { useRouter } from "next/router";
 import React from "react";
 import { ROUTES } from "src/common/routes";
 import { pluralize } from "src/common/utils/strUtils";
@@ -11,6 +12,12 @@ interface Props {
 }
 
 const SentNotification = ({ numSent, onDismiss, open }: Props): JSX.Element => {
+  const router = useRouter();
+  const onClick = () => {
+    router.push(ROUTES.GROUP_INVITATIONS, undefined, { shallow: true });
+    onDismiss();
+  };
+
   return (
     <Notification
       buttonText="DISMISS"
@@ -22,7 +29,7 @@ const SentNotification = ({ numSent, onDismiss, open }: Props): JSX.Element => {
     >
       {numSent} {pluralize("Invitation", numSent)} {pluralize("has", numSent)}{" "}
       been sent.{" "}
-      <Link sdsStyle="dashed" href={ROUTES.GROUP_INVITATIONS}>
+      <Link sdsStyle="dashed" onClick={onClick}>
         View Invitations
       </Link>
       .
