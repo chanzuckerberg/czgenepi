f022ae790ba3b5c1efd3e1f8658afa08bf892c0d
maya <mlila@chanzuckerberg.com>
2022-06-16 09:45:52 -0700

    prevent table from flickering on scroll (#1161)

--- src/frontend/src/common/components/library/Table/components/Row/index.tsx
+++ src/frontend/src/common/components/library/Table/components/Row/index.tsx
@@ -1,22 +1,23 @@
-import React, { ReactNode } from "react";
+import React, { CSSProperties, ReactNode } from "react";
 import { Cell } from "../Cell";
 import { StyledHeader, StyledRow } from "./style";
 
 interface Props {
   cells: ReactNode[];
   isHeader?: boolean;
+  style?: CSSProperties;
 }
 
-const Row = ({ cells, isHeader }: Props): JSX.Element | null => {
+const Row = ({ cells, isHeader, style }: Props): JSX.Element | null => {
   if (cells.length === 0) return null;
 
   const mappedCells = cells.map((c, i) => <Cell key={i} content={c} />);
 
   if (isHeader) {
-    return <StyledHeader>{mappedCells}</StyledHeader>;
+    return <StyledHeader style={style}>{mappedCells}</StyledHeader>;
   }
 
-  return <StyledRow>{mappedCells}</StyledRow>;
+  return <StyledRow style={style}>{mappedCells}</StyledRow>;
 };
 
 export { Row };
