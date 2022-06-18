d0b3c8a5614dc873ba4e53355da4aafe43a677bc
Maya Lila <mlila@chanzuckerberg.com>
2022-06-18 02:30:24 -0400

    types

--- src/frontend/src/common/styles/mixins/global.ts
+++ src/frontend/src/common/styles/mixins/global.ts
@@ -1,6 +1,5 @@
 import styled from "@emotion/styled";
 import { CommonThemeProps, getSpaces } from "czifui";
-import { CSSProperties } from "react";
 import { NAV_BAR_HEIGHT_PX } from "src/components/NavBar";
 
 const BREAKPOINT = "768px";
@@ -27,7 +26,7 @@ export const PageContent = styled.div`
   height: calc(100% - ${NAV_BAR_HEIGHT_PX}px);
 `;
 
-export const ContentStyles = (props: CommonThemeProps): CSSProperties => {
+export const ContentStyles = (props: CommonThemeProps): string => {
   const spaces = getSpaces(props);
 
   return `
@@ -43,7 +42,7 @@ export const ContentStyles = (props: CommonThemeProps): CSSProperties => {
 
 export const MAX_CONTENT_WIDTH = 1308;
 
-export const ZebraStripes = (): CSSProperties => {
+export const ZebraStripes = (): string => {
   return `
     :nth-of-type(odd) {
       background-color: rgba(0, 0, 0, 0.04);
