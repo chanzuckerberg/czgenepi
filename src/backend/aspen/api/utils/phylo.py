6c5d7fab54da8914bd5ddd5fa2a552200a4aca7d
Dan Lu <dlu@chanzuckerberg.com>
2022-06-22 15:22:15 -0500

    final touch up

--- src/backend/aspen/api/utils/phylo.py
+++ src/backend/aspen/api/utils/phylo.py
@@ -7,10 +7,9 @@ from typing import Dict, Mapping, Optional, Set, Tuple
 import boto3
 import sqlalchemy as sa
 from sqlalchemy import asc
-from sqlalchemy.dialects import postgresql
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import aliased, selectinload
-from sqlalchemy.sql.expression import and_, not_, or_
+from sqlalchemy.sql.expression import and_, or_
 
 from aspen.api.error import http_exceptions as ex
 from aspen.api.utils import authz_phylo_tree_filters
@@ -197,7 +196,7 @@ async def _set_colors_for_location_category(
             )
         )
         .order_by(asc("distance"))
-        .limit(15)
+        .limit(16)
     )
 
     # raw_sql = sorting_query.compile(
@@ -206,13 +205,11 @@ async def _set_colors_for_location_category(
     # print(raw_sql)
 
     sorted_locations_result = await db.execute(sorting_query)
-    sorted_locations = [row[category] for row in sorted_locations_result]
+    location_strings = [row[category] for row in sorted_locations_result]
 
     # Add the locations we found location data for
     # If we still have fewer than 16, add whatever is left from the set we collected
     # in the tree, even if we don't have spatial data on them.
-    location_strings = [getattr(tree_location, category)]
-    location_strings.extend([location for location in sorted_locations])
     # print("Location strings from SQL result:", location_strings)
     # print("Number of location strings:", len(location_strings))
     if len(location_strings) < 16:
@@ -290,6 +287,7 @@ async def process_phylo_tree(
     json_data = json.loads(data)
 
     if id_style == "public":
+        json_data = await _set_colors(db, json_data, phylo_run)
         return json_data
 
     can_see_pi_group_ids: Set[int] = {user.group_id}
