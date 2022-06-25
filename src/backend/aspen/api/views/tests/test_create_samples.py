242759fd907793fb69034a9dc89a4b2b9535f4bd
Jessica Gadling <jgadling@chanzuckerberg.com>
2022-06-24 17:13:25 -0700

    Remove unused code.

--- src/backend/aspen/api/views/tests/test_create_samples.py
+++ src/backend/aspen/api/views/tests/test_create_samples.py
@@ -8,7 +8,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
 from aspen.database.models import PathogenGenome, Sample, UploadedPathogenGenome
-from aspen.test_infra.models.gisaid_metadata import gisaid_metadata_factory
 from aspen.test_infra.models.location import location_factory
 from aspen.test_infra.models.sample import sample_factory
 from aspen.test_infra.models.usergroup import group_factory, userrole_factory
@@ -483,19 +482,3 @@ async def test_samples_create_view_fail_missing_required_fields(
             },
         ]
     }
-
-
-async def setup_validation_data(session: AsyncSession, client: AsyncClient):
-    group = group_factory()
-    user = await userrole_factory(async_session, group)
-    location = location_factory(
-        "North America", "USA", "California", "Santa Barbara County"
-    )
-    sample = sample_factory(group, user, location)
-    gisaid_sample = gisaid_metadata_factory()
-    session.add(group)
-    session.add(sample)
-    session.add(gisaid_sample)
-    await session.commit()
-
-    return client, sample, gisaid_sample
