63407f5ad2b96146c066c58d01b2c6e72760e2f6
Maya Lila <mlila@chanzuckerberg.com>
2022-07-26 10:34:38 -0700

    sort members alphabetically, as a human reads

--- src/frontend/src/common/utils/strUtils.ts
+++ src/frontend/src/common/utils/strUtils.ts
@@ -16,3 +16,8 @@ export const pluralizeVerb = (str: string, count: number): string => {
   if (WORDS_TO_PLURALIZE[str]) return WORDS_TO_PLURALIZE[str];
   return str;
 };
+
+type SortReponse = -1 | 0 | 1;
+export const caseInsensitiveSort = (a: string, b: string): SortReponse => {
+  return a.toLowerCase() > b.toLowerCase() ? 1 : -1;
+};
