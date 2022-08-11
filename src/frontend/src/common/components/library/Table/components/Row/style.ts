07816b7da517012b076830ee33b1fac7b4c1c1c2
ehoops-cz <109251328+ehoops-cz@users.noreply.github.com>
2022-08-11 09:08:58 -0700

    SC-194364 get rid of semantic UI (#1267)
    * wip
    
    * Search input. Replace global styles.
    
    * remove unneeded styling
    
    * Lint rolling.
    
    * lint
    
    * Move font-family to global.css - the portal is outside StyledApp.
    
    * Fix dialog width.
    
    * Fix row height.
    
    * linting.
    
    Co-authored-by: Erin Hoops <erin.hoops@chanzuckerberg.com>

--- src/frontend/src/common/components/library/Table/components/Row/style.ts
+++ src/frontend/src/common/components/library/Table/components/Row/style.ts
@@ -1,19 +1,10 @@
 import styled from "@emotion/styled";
-import {
-  CommonThemeProps,
-  fontHeaderS,
-  fontHeaderXs,
-  getColors,
-  getSpaces,
-} from "czifui";
+import { fontHeaderS, fontHeaderXs, getColors } from "czifui";
 
-const sharedRowStyles = (props: CommonThemeProps) => {
-  const spaces = getSpaces(props);
-
+const sharedRowStyles = () => {
   return `
     display: flex;
     align-items: center;
-    padding: ${spaces?.l}px;
   `;
 };
 
@@ -21,12 +12,12 @@ export const StyledRow = styled.div`
   ${fontHeaderXs}
   ${sharedRowStyles}
 
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-
   ${(props) => {
     const colors = getColors(props);
 
     return `
+
+      border-bottom: 1px ${colors?.gray[200]} solid;
       &:hover {
         background-color: ${colors?.primary[100]};
       }
