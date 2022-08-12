f0d941f0408a0b67b8dd02e45235d3668b1db10a
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2022-08-11 23:47:01 -0700

    Bring in initial OneTrust stylesheet from Damian

--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -14,6 +14,7 @@ import { queryClient } from "src/common/queries/queryClient";
 import { store } from "src/common/redux";
 import { StyledApp } from "src/common/styles/appStyle";
 import "src/common/styles/global.css";
+import "src/common/styles/oneTrust.css";
 import { theme } from "src/common/styles/theme";
 import { setFeatureFlagsFromQueryParams } from "src/common/utils/featureFlags";
 import Nav from "src/components/NavBar";
