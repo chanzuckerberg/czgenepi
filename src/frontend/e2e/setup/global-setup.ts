4a9781171c370e128995c6acbaf774800031f048
Charles Moga <cmoga@contractor.chanzuckerberg.com>
2022-08-12 13:50:57 +0100

    storage stage for staging

--- src/frontend/e2e/setup/global-setup.ts
+++ src/frontend/e2e/setup/global-setup.ts
@@ -1,18 +1,21 @@
 import { chromium, FullConfig } from '@playwright/test';
-import { login } from '../utils/login';
+import { getByID, getByTestID } from "../utils/selectors";
 
 const username = process.env.USERNAME ?? '';
 const password = process.env.PASSWORD ?? '';
 
 async function globalSetup(config: FullConfig): Promise<void> {
     const { storageState } = config.projects[0].use;
+    const { baseURL } = config.projects[0].use;
     const browser = await chromium.launch();
 
     const page = await browser.newPage();
-    await login(page, username, password);
-    await page.context().storageState({
-        path: storageState,
-    });
+    await page.goto(baseURL as string);
+    await page.locator(getByTestID("navbar-sign-in-link")).click(); 
+    await page.locator(getByID("username")).first().fill(username); 
+    await page.locator(getByID("password")).first().fill(password);
+    await page.locator('button[type=submit] >> "Continue"').first().click(),
+    await page.context().storageState({ path: storageState as string }),
     await browser.close()
 }
 export default globalSetup;
--- src/frontend/e2e/tests/sample.spec.ts
+++ src/frontend/e2e/tests/sample.spec.ts
@@ -15,13 +15,24 @@ const tableHeaders = [
 ];
 
 test.describe("Samples page tests", () => {
-  test("Should verify sample listing", async ({ page }: { page: Page }) => {
+  test.beforeEach(async ({ page }, testInfo) => {
+    console.log(`Running ${testInfo.title}`);
     await page.goto("https://staging.czgenepi.org/data/samples");
+  });
+  test("Should verify sample listing", async ({ page }: { page: Page }) => {
     tableHeaders.forEach((header) => {
       expect(page.locator(getByText(header)).first()).not.toBeEmpty();
     });
     await expect(page.locator(getByTestID("data-menu-item"))).toHaveCount(
       TAB_COUNT
     );
+  });
+
+  test("Should verify sample data", async ({page,}: { page: Page }) => {
+    //wait until data is displayed
+    await page.waitForSelector('[data-test-id="table-row"]')
+    
+    // assert table is populated with at least one record
+    expect((await page.locator(getByTestID("table-row")).count())).toBeGreaterThan(0)
   });
 });
