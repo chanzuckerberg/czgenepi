4525e33c89ee0c0e8be07b6b37dc32f895f27123
ehoops-cz <erin.hoops+gh@chanzuckerberg.com>
2022-08-24 16:16:47 -0700

    Style button.

--- src/frontend/src/components/NavBar/components/AppNavBar/style.ts
+++ src/frontend/src/components/NavBar/components/AppNavBar/style.ts
@@ -1,5 +1,11 @@
 import styled from "@emotion/styled";
-import { CommonThemeProps, fontHeaderL, getColors, getSpaces } from "czifui";
+import {
+  CommonThemeProps,
+  fontHeaderL,
+  getColors,
+  getPalette,
+  getSpaces,
+} from "czifui";
 import LogoImage from "src/common/images/logo_complete_white.svg";
 import { iconFillWhite } from "src/common/styles/iconStyle";
 
@@ -73,6 +79,15 @@ export const NavOrg = styled.div`
 
 export const DropdownClickTarget = styled.button`
   display: flex;
+  align-items: center;
+  border: none;
+  ${(props: CommonThemeProps) => {
+    const palette = getPalette(props);
+
+    return `
+      background-color: ${palette?.common?.black};
+    `;
+  }}
 `;
 
 export const StyledNavIconWrapper = styled.div`
