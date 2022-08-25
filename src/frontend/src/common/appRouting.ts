a36a565fcfd4ffd4ced8ac67b4c50278665b72fa
Maya Lila <mlila@chanzuckerberg.com>
2022-08-25 08:03:04 -0700

    actually parse url params effectively and generate url

--- src/frontend/src/common/appRouting.ts
+++ src/frontend/src/common/appRouting.ts
@@ -9,25 +9,43 @@ import { ensureValidPathogen } from "./redux/utils/pathogenUtils";
 import { publicPaths } from "./routes";
 import { canUserViewGroup } from "./utils/userInfo";
 
+const GROUP_URL_INDICATOR = "group";
+const PATHOGEN_URL_INDICATOR = "pathogen";
+
 export const useAppRouting = (): void => {
   const router = useRouter();
   const path = router.basePath;
 
-  if (path in publicPaths) {
-    setPublicPagePath(router);
-  } else {
-    setCurrentWorkspace(router);
+  // public page paths shouldn't ever occur with the extra params
+  // so if a user navigates to a public page, just do nothing --
+  // they're already in the right place
+  if (!(path in publicPaths)) {
+    setCurrentWorkspacePath(router);
   }
 };
 
-// TODO (mlila): do i need to rename all the pages files to be ...params? not sure how
-// TODO          trailing params will affect fetching
-const setCurrentWorkspace = async (router) => {
+/**
+ * Ensures the groupId and pathogen in the url are valid and the user can
+ * access them. If not, redirects the user to a group/pathogen they _can_ access.
+ */
+const setCurrentWorkspacePath = async (router) => {
   const { params } = router.query;
 
-  // TODO (mlila): this is a fragile assumption about url params.
-  const potentialUrlPathogen = params?.pop();
-  const potentialUrlGroupId = params?.pop();
+  // nothing to parse here
+  if (!params) return;
+
+  let potentialUrlPathogen;
+  let potentialUrlGroupId;
+
+  for (let i = 0; i < params.length - 1; i++) {
+    if (params[i] === GROUP_URL_INDICATOR) {
+      potentialUrlGroupId = params[i + 1];
+    }
+
+    if (params[i] === PATHOGEN_URL_INDICATOR) {
+      potentialUrlPathogen = params[i + 1];
+    }
+  }
 
   const urlGroupId = await setWorkspaceGroupId(potentialUrlGroupId);
   const urlPathogen = await setWorkspacePathogen(potentialUrlPathogen);
@@ -36,6 +54,10 @@ const setCurrentWorkspace = async (router) => {
   // router.push(path);
 };
 
+/**
+ * Ensures the groupId in the url are valid and the user can
+ * access it. If not, returns a group the user can access.
+ */
 const setWorkspaceGroupId = async (potentialUrlGroupId: string): number => {
   const userInfo = await fetchUserInfo();
   const { dispatch, getState } = store;
@@ -57,6 +79,10 @@ const setWorkspaceGroupId = async (potentialUrlGroupId: string): number => {
   return selectCurrentGroup(state);
 };
 
+/**
+ * Ensures the pathogen in the url are valid and the user can
+ * access it. If not, returns a pathogen the user can access.
+ */
 const setWorkspacePathogen = async (potentialUrlPathogen: string): Pathogen => {
   const { dispatch, getState } = store;
   const state = getState();
