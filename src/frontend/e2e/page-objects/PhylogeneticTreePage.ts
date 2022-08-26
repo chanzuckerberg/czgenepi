19a8fc48906fe373f1494a18da9e460964d3147c
luisczi <lbrambila@contractor.chanzuckerberg.com>
2022-08-26 11:52:53 -0500

    odifyng selector

--- src/frontend/e2e/page-objects/PhylogeneticTreePage.ts
+++ src/frontend/e2e/page-objects/PhylogeneticTreePage.ts
@@ -15,6 +15,9 @@ export class PhylogeneticTreePage {
         readonly addForcedSamplesButton: Locator
         readonly createTreeButton: Locator
         readonly radiosTreeType: Locator
+        readonly errorToolTip: Locator
+        readonly exceedNumberOfCharsErrorMsg: Locator
+        readonly sampleWarningMessage: Locator
 
     constructor(page: Page){
         this.page = page
@@ -26,20 +29,25 @@ export class PhylogeneticTreePage {
         this.collectionDateDropDown = page.locator("p + button[label='All Time']");
         this.forcedIncludeTextArea = page.locator("//div[contains(@class,'MuiDialog-paperScrollPaper')]/descendant::textarea[not(@aria-hidden='true')]");
         this.addForcedSamplesButton = page.locator("//button[text()='Add']");
-        this.createTreeButton = page.locator("//span[text()='Create Tree']");
+        this.createTreeButton = page.locator("//button[text()='Create Tree']");
         this.radiosTreeType = page.locator("span[class*='Radio'] > input");
+        this.errorToolTip = page.locator("//div[@role='tooltip']/div");
+        this.exceedNumberOfCharsErrorMsg = page.locator("//div[text()='Name exceeds the 128 character limit.']");
+        this.sampleWarningMessage = page.locator("//div[contains(@class,'MuiDialog-paperScrollPaper')]/descendant::div[contains(@class,'MuiAlert-messag')]/descendant::span[1]");
     }
 
-    async createTree(treeInfo: TreeInfo){
+    async fillTreeInfo(treeInfo: TreeInfo){
         await this.treeNameInput.type(treeInfo.treeName);
         const types = await this.radiosTreeType.evaluateAll(radios => radios.map( element =>  element.getAttribute('value')))
-        for(let i = 0; i < types.length; i++){
-            if(treeInfo.treeType.toUpperCase() === types[i]){
-               await this.page.locator('//span[text()="${VAR} "]'.replace('${VAR}',treeInfo.treeType)).click();
+        if (treeInfo.treeType.length > 0){
+            for(let i = 0; i < types.length; i++){
+                if(treeInfo.treeType.toUpperCase() === types[i]){
+               //    await this.page.locator('//input[contains(@value,"${VAR}")]'.replace('${VAR}',treeInfo.treeType)).click();
+                    await this.page.locator(`.MuiFormGroup-root > label:nth-of-type(${i+1}) > span > input`).click();
+                }
             }
         }
-        //console.log("ESTOS SON LOS RATIOS "+types);
-         if(typeof Array.isArray(await treeInfo.lineage)){
+         if(Array.isArray(treeInfo.lineage)){
           await this.lineageDropDown.click();
             for(let j = 0; j < treeInfo.lineage.length; j++){
                 await this.page.locator("//div[@role='tooltip']/descendant::input").type(treeInfo.lineage[j]);
@@ -57,7 +65,7 @@ export class PhylogeneticTreePage {
             await this.collectionDateDropDown.click();
             await this.page.locator("//div[not(@aria-hidden='true') and contains(@class,'MuiPopover-root')]/descendant::span[text()='${VAR}']".replace('${VAR}',treeInfo.collectionDate)).click();
         }
-        else if(typeof Array.isArray(await treeInfo.collectionDate)){
+        else if(Array.isArray(await treeInfo.collectionDate)){
             await this.collectionDateDropDown.click();
             await this.page.locator("//div[not(@aria-hidden='true') and contains(@class,'MuiPopover-root')]/descendant::input[1]").type(treeInfo.collectionDate[0]);
             await this.page.locator("//div[not(@aria-hidden='true') and contains(@class,'MuiPopover-root')]/descendant::input[2]").type(treeInfo.collectionDate[1]);
@@ -67,7 +75,28 @@ export class PhylogeneticTreePage {
             await this.forcedIncludeTextArea.type(treeInfo.forceIncludedSamples);
             await this.addForcedSamplesButton.click();
         } 
-       // await this.createTreeButton.click();
+    }
+
+    async submitTreeinfo(){
+        await this.createTreeButton.click();
+    }
+
+    async createTree(treeInfo: TreeInfo){
+        await this.fillTreeInfo(treeInfo);
+        await this.submitTreeinfo();
+    }
+
+    async getToolTipErrorMsg(): Promise<string>{
+        await this.createTreeButton.hover({force: true});
+        return await this.errorToolTip.textContent() as string;
+    }
+
+    async getExceedNumberCharactersErrorMsg(): Promise<string>{
+        return await this.exceedNumberOfCharsErrorMsg.textContent() as string;
+    }
+
+    async getSampleWarningMsg(): Promise<string>{
+        return await this.sampleWarningMessage.textContent() as string;
     }
 
 }
