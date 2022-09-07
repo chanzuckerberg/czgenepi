bc4403f4674227d70ecd3244b01c15a6eb4bc5a4
Jessica Gadling <jgadling@chanzuckerberg.com>
2022-09-07 16:03:35 -0700

    Make sure all trees/runs have a pathogen.

---
+++ src/backend/database_migrations/versions/20220907_225235_add_pathogens_to_all_trees.py
@@ -0,0 +1,54 @@
+"""add pathogens to all trees
+
+Create Date: 2022-09-07 22:52:41.089769
+
+"""
+import enumtables  # noqa: F401
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "20220907_225235"
+down_revision = "20220902_001655"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # backpopulate pathogen fields to sars-cov-2
+    op.execute(
+        """
+        UPDATE aspen.phylo_runs
+        SET pathogen_id=(
+            SELECT id from aspen.pathogens WHERE slug='SC2'
+        )
+        WHERE pathogen_id IS NULL
+        """
+    )
+    op.execute(
+        """
+        UPDATE aspen.phylo_trees
+        SET pathogen_id=(
+            SELECT id from aspen.pathogens WHERE slug='SC2'
+        )
+        WHERE pathogen_id IS NULL
+        """
+    )
+    op.alter_column(
+        "phylo_runs",
+        "pathogen_id",
+        existing_type=sa.INTEGER(),
+        nullable=False,
+        schema="aspen",
+    )
+    op.alter_column(
+        "phylo_trees",
+        "pathogen_id",
+        existing_type=sa.INTEGER(),
+        nullable=False,
+        schema="aspen",
+    )
+
+
+def downgrade():
+    raise NotImplementedError("don't downgrade")
