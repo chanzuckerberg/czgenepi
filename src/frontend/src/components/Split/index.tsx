245cf55360184eae6f31a59d6e81241e81dfb93b
Maya Lila <mlila@chanzuckerberg.com>
2022-09-12 15:42:27 -0700

    remove old ff

--- src/frontend/src/components/Split/index.tsx
+++ src/frontend/src/components/Split/index.tsx
@@ -46,7 +46,6 @@ import { useUserInfo } from "src/common/queries/auth";
 export enum FEATURE_FLAGS {
   // my_flag_name = "my_flag_name", (<-- format example)
   prep_files = "prep_files",
-  sample_filtering_tree_creation = "sample_filtering_tree_creation",
 }
 
 // Keyword to tell Split client it's running in local-only mode.
