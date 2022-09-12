ab64e9a5ef446e9284c68d0e8e8b974fd669e8d7
Jessica Gadling <jgadling@chanzuckerberg.com>
2022-09-12 16:00:02 -0700

    minus 2000 for buffer.

--- .happy/terraform/modules/sfn_config/main.tf
+++ .happy/terraform/modules/sfn_config/main.tf
@@ -8,8 +8,8 @@ locals {
       )
     }
     OutputPrefix = "s3://${var.swipe_comms_bucket}/swipe${var.remote_dev_prefix}/${var.app_name}/results",
-    RunSPOTMemory = var.memory,
-    RunEC2Memory = var.memory,
+    RunSPOTMemory = var.memory - 2000,
+    RunEC2Memory = var.memory - 2000,
     RunSPOTVcpu = var.vcpus,
     RunEC2Vcpu = var.vcpus,
     RUN_WDL_URI = "s3://${aws_s3_bucket_object.wdl.bucket}${aws_s3_bucket_object.wdl.key}",
