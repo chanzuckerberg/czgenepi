9c361556eb89a05ed1f134b94466b83c8b01a29e
ehoops-cz <erin.hoops+gh@chanzuckerberg.com>
2022-09-12 12:25:54 -0700

    oops, removing testing style.

--- src/frontend/src/common/styles/accessibility.ts
+++ src/frontend/src/common/styles/accessibility.ts
@@ -20,7 +20,6 @@ export const srOnly = (): string => {
 export const accessibleFocusBorder = (): string => {
   return `
     &:focus, &:focus-within {
-      border: 1px solid blue
       outline: 5px auto Highlight;
       outline: 5px auto -webkit-focus-ring-color;
     }
