96b4b4adfc77b5d37fc1eb2d24495dbf57dc9f01
ehoops-cz <erin.hoops+gh@chanzuckerberg.com>
2022-09-14 09:27:25 -0700

    Add Galago integration analytics.

--- src/frontend/src/views/Data/components/TreeActionMenu/components/OpenInGalagoButton/index.tsx
+++ src/frontend/src/views/Data/components/TreeActionMenu/components/OpenInGalagoButton/index.tsx
@@ -1,5 +1,10 @@
 import { ButtonIcon, Icon, Tooltip } from "czifui";
 import { useState } from "react";
+import {
+  AnalyticsTreeActionsClickGalago,
+  EVENT_TYPES,
+} from "src/common/analytics/eventTypes";
+import { analyticsTrackEvent } from "src/common/analytics/methods";
 import { TREE_STATUS } from "src/common/constants/types";
 import { GalagoConfirmationModal } from "src/views/Data/components/GalagoConfirmationModal";
 
@@ -14,7 +19,15 @@ const OpenInGalagoButton = ({ item }: Props): JSX.Element => {
   const isDisabled = status !== TREE_STATUS.Completed || !treeId;
 
   const handleClickOpen = () => {
-    if (!isDisabled) setOpen(true);
+    if (!isDisabled) {
+      analyticsTrackEvent<AnalyticsTreeActionsClickGalago>(
+        EVENT_TYPES.TREE_ACTIONS_CLICK_GALAGO,
+        {
+          tree_id: treeId,
+        }
+      );
+      setOpen(true);
+    }
   };
 
   const handleClose = () => {
@@ -43,12 +56,15 @@ const OpenInGalagoButton = ({ item }: Props): JSX.Element => {
             sdsType="primary"
             size="large"
           >
-            <Icon sdsIcon="lightBulb" sdsSize="s" sdsType="iconButton" />
+            <Icon
+              sdsIcon="barChartVertical3"
+              sdsSize="s"
+              sdsType="iconButton"
+            />
           </ButtonIcon>
         </span>
       </Tooltip>
       {treeId && (
-        // TODO: (ehoops): replace this with the Galago modal as part of SC-214165
         <GalagoConfirmationModal
           open={open}
           onClose={handleClose}
