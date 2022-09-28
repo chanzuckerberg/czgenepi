84e53d1d06f920cb893b0a765fdb01235a846221
ehoops-cz <erin.hoops+gh@chanzuckerberg.com>
2022-09-28 12:00:45 -0700

    Split update.

---
+++ src/frontend/src/components/Split/types.ts
@@ -0,0 +1,34 @@
+/**
+ * Team convention for values of an enabled flag that is just a simple on/off.
+ */
+export enum SPLIT_SIMPLE_FLAG {
+  ON = "on",
+  OFF = "off",
+}
+
+/**
+ * Canonical listing of all User-based Split feature flags FE needs to know about.
+ * One enum per traffic type (currently, we have `user` and `pathogen` types)
+ *
+ * If you modify the feature flags while doing dev work, you will need to
+ * /reload/ your browser. Do not just depend on the soft [Fast Refresh]
+ * functionality of our dev server because the feature flags are injected
+ * into the Split config, and that is only rebuilt with a real refresh.
+ */
+export enum USER_FEATURE_FLAGS {
+  // my_flag_name = "my_flag_name", (<-- format example)
+  galago_integration = "galago_integration",
+  prep_files = "prep_files",
+  tree_location_filter = "tree_location_filter",
+}
+
+/**
+ * Canonical listing of all Pathogen-based Split feature flags FE needs to know about.
+ * One enum per traffic type (currently, we have `user` and `pathogen` types)
+ */
+export enum PATHOGEN_FEATURE_FLAGS {
+  galago_linkout = "galago_linkout",
+  lineage_filter_enabled = "lineage_filter_enabled",
+  public_repository = "public_repository",
+  usher_linkout = "usher_linkout",
+}
