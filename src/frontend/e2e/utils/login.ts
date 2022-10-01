eaba6a1a093c6f7373f38746b5e529fd9b3553a7
Charles Moga <cmoga@contractor.chanzuckerberg.com>
2022-10-01 16:40:20 +0100

    test direct dial to login page for local

--- src/frontend/e2e/utils/login.ts
+++ src/frontend/e2e/utils/login.ts
@@ -30,7 +30,7 @@ export async function login(
     password = `${process.env.PASSWORD}`;
 
     const authUrl =
-      "https://oidc.genepinet.localdev:8443/Account/Login?ReturnUrl=%2Fconnect%2Fauthorize%2Fcallback%3Fresponse_type%3Dcode%26client_id%3Dlocal-client-id%26redirect_uri%3Dhttp%253A%252F%252Fbackend.genepinet.localdev%253A3000%252Fv2%252Fauth%252Fcallback%26scope%3Dopenid%2520profile%2520email%2520offline_access%26state%3DspMNx91QxfKVyXevXEUsuZasZCUKrD%26nonce%3D3Q04286TSbfqCWtOLE4x";
+      "oidc.genepinet.localdev:8443/Account/Login?ReturnUrl=%2Fconnect%2Fauthorize%2Fcallback%3Fresponse_type%3Dcode%26client_id%3Dlocal-client-id%26redirect_uri%3Dhttp%253A%252F%252Fbackend.genepinet.localdev%253A3000%252Fv2%252Fauth%252Fcallback%26scope%3Dopenid%2520profile%2520email%2520offline_access%26state%3DspMNx91QxfKVyXevXEUsuZasZCUKrD%26nonce%3D3Q04286TSbfqCWtOLE4x";
     await Promise.all([page.waitForNavigation(), await page.goto(authUrl)]);
   } else {
     await Promise.all([
