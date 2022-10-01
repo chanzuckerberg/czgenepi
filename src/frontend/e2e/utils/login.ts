9f4ffe490c965bb87fd970adc4c396e3fe5c11a7
Charles Moga <cmoga@contractor.chanzuckerberg.com>
2022-10-01 16:32:19 +0100

    test direct dial to login page for local

--- src/frontend/e2e/utils/login.ts
+++ src/frontend/e2e/utils/login.ts
@@ -28,13 +28,23 @@ export async function login(
     buttonText = "Login";
     username = `${process.env.USERNAME}`;
     password = `${process.env.PASSWORD}`;
+
+    const authUrl =
+      "https://oidc.genepinet.localdev:8443/Account/Login?ReturnUrl=%2Fconnect%2Fauthorize%2Fcallback%3Fresponse_type%3Dcode%26client_id%3Dlocal-client-id%26redirect_uri%3Dhttp%253A%252F%252Fbackend.genepinet.localdev%253A3000%252Fv2%252Fauth%252Fcallback%26scope%3Dopenid%2520profile%2520email%2520offline_access%26state%3DspMNx91QxfKVyXevXEUsuZasZCUKrD%26nonce%3D3Q04286TSbfqCWtOLE4x";
+    await Promise.all([page.waitForNavigation(), await page.goto(authUrl)]);
+  } else {
+    await Promise.all([
+      page.waitForNavigation(),
+      await page.goto(`${process.env.BASEURL}`),
+      await page.locator(getByTestID("navbar-sign-in-link")).click(),
+    ]);
   }
 
   await Promise.all([
     page.waitForNavigation(),
-    await page.goto(`${process.env.BASEURL}`),
-    await page.locator(getByTestID("navbar-sign-in-link")).click(),
-    console.log(await page.url()),
+    // await page.goto(`${process.env.BASEURL}`),
+    // await page.locator(getByTestID("navbar-sign-in-link")).click(),
+
     await page.locator(getByID(usernameInputSelector)).fill(username),
     await page.locator(getByID(passwordInputSelector)).fill(password),
     page.locator(`button[type=submit] >> "${buttonText}"`).click(),
