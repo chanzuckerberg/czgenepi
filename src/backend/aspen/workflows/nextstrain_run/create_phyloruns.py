467e89dbf0e4560a6e6dd5daab5bad0ea4ee89bf
Jessica Gadling <jgadling@chanzuckerberg.com>
2022-10-10 11:27:20 -0700

    Formatting.

--- src/backend/aspen/workflows/nextstrain_run/create_phyloruns.py
+++ src/backend/aspen/workflows/nextstrain_run/create_phyloruns.py
@@ -5,7 +5,6 @@ from typing import Any, Dict, MutableSequence
 
 import click
 import sqlalchemy as sa
-from sqlalchemy.sql.expression import and_, or_
 
 from aspen.api.settings import CLISettings
 from aspen.config.config import Config
@@ -30,6 +29,7 @@ from aspen.util.swipe import NextstrainScheduledJob
 SCHEDULED_TREE_TYPE = "OVERVIEW"
 
 TEMPLATE_ARGS = {"filter_start_date": "12 weeks ago", "filter_end_date": "now"}
+
 
 def create_phylo_run(
     session,
@@ -67,17 +67,20 @@ def create_phylo_run(
 @click.group()
 def cli():
     pass
-    
+
+
 @cli.command("launch")
 @click.argument("group", required=True)
 @click.option("--template-args", type=str, default="{}")
 @click.option("--tree-type", type=str, default="OVERVIEW")
 @click.option("--pathogen", type=str, default="SC2")
 @click.option("--launch-sfn", is_flag=True, default=False)
-def launch_one(group: Group, template_args: str, tree_type: str, pathogen: str, launch_sfn: bool):
+def launch_one(
+    group: Group, template_args: str, tree_type: str, pathogen: str, launch_sfn: bool
+):
     # NOTE/TODO - this currently doesn't do any smart input validation, it will just
     # let python raise an exception and explode if anything doesn't make sense.
-    tree_type_obj=TreeType(tree_type)
+    tree_type_obj = TreeType(tree_type)
     interface: SqlAlchemyInterface = init_db(get_db_uri(Config()))
     settings = CLISettings()
     template_args_obj: Dict[str, Any] = json.loads(template_args)
@@ -87,10 +90,10 @@ def launch_one(group: Group, template_args: str, tree_type: str, pathogen: str, 
         else:
             where_clause = Group.name == group
         groups_query = sa.select(Group).where(where_clause)
-        group: Group = (
-            db.execute(groups_query).scalars().one()
+        group: Group = db.execute(groups_query).scalars().one()
+        workflow = create_phylo_run(
+            db, group, template_args_obj, tree_type_obj, pathogen
         )
-        workflow = create_phylo_run(db, group, template_args_obj, tree_type_obj, pathogen)
 
         job = NextstrainScheduledJob(settings)
         db.commit()
@@ -106,7 +109,7 @@ def launch_all(pathogen):
     settings = CLISettings()
 
     interface: SqlAlchemyInterface = init_db(get_db_uri(Config()))
-    tree_type=TreeType("OVERVIEW")
+    tree_type = TreeType("OVERVIEW")
     with session_scope(interface) as db:
         all_groups_query = sa.select(Group)
         all_groups: MutableSequence[Group] = (
@@ -118,7 +121,9 @@ def launch_all(pathogen):
                 schedule_expression is None
                 or datetime.date.today().weekday() in schedule_expression
             ):
-                workflow = create_phylo_run(db, group, TEMPLATE_ARGS, tree_type, pathogen)
+                workflow = create_phylo_run(
+                    db, group, TEMPLATE_ARGS, tree_type, pathogen
+                )
 
                 job = NextstrainScheduledJob(settings)
                 db.commit()
