e015a2dbe479a59a3506c0c005a17f0c893ca335
charles-testco <109340488+charles-testco@users.noreply.github.com>
2022-10-18 06:53:57 +0100

    [SC-215051] Replace css Xpath selectors (#1375)
    * updated filter.ts
    
    * More refactoring
    
    * updated viewport
    
    * fixed uploading
    
    * refactor upload tests
    
    * fixed lint errors
    
    * removed test.only
    
    * skipped filtering/sample tests which are beining enabled in a separate branch
    
    * moved files
    
    * added fasta, fast file types
    
    * added security tests
    
    * fixed file extension
    
    * merged trunk and fixed tests
    
    * review comments
    
    * fixed tests
    
    * addressed review comments
    
    * fixed test
    
    * fixed test
    
    * cleanup

--- src/frontend/e2e/utils/sample.ts
+++ src/frontend/e2e/utils/sample.ts
@@ -1,21 +1,22 @@
 import { sample } from "lodash";
 import {
+  getADateInThePast,
+  getValueOrDefault,
   generatePrivateSampleId,
   generatePublicSampleId,
-  getADateInThePast,
   getRandomNumber,
-  getValueOrDefault,
+  getLocations,
 } from "./common";
 import * as dotenv from "dotenv";
+import { SampleResponseDefaults } from "./schemas/sampleResponseDefaults";
+import { SampleResponseData } from "./schemas/sampleResponseData";
+import { SampleUploadData } from "./schemas/sampleUploadData";
 
 dotenv.config();
 
-const defaultSequence =
-  "ATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTCATTAAAGCCCCCAAGTC";
-const locationId = 166768; //todo: will be good to get this from API and then choose randomly
 const lineages = ["A", "BA.1.1", "BA.1.15"]; //todo: will be good to get this from API and then choose randomly
-const trueOrFalse = [true, false];
 
+const locations = getLocations();
 /**
  * This method generates sample data that can be used for uploading
  * @param defaults - user supplied sample data to be included
@@ -23,70 +24,52 @@ const trueOrFalse = [true, false];
  * e.g. 5 means sample collection date will be no earlier than 5 days
  * @returns SampleUploadData
  */
-export function getSampleUploadData(
+export function createSampleUploadData(
   defaults?: SampleUploadData,
-  minCollectionDays = 0,
-  maxCollectionDays = 10
+  maxCollectionDateAge = 10
 ): SampleUploadData {
   const collectionDate = getValueOrDefault(
-    defaults?.sample?.collection_date,
-    getADateInThePast(minCollectionDays)
+    defaults?.collection_date,
+    getADateInThePast(maxCollectionDateAge)
+  );
+  const sequencingDate = getValueOrDefault(
+    defaults?.sequencing_date,
+    getADateInThePast(maxCollectionDateAge)
   );
   return {
-    pathogen_genome: {
-      sequence: defaultSequence,
-      sequencing_date: getValueOrDefault(
-        defaults?.pathogen_genome?.sequencing_date,
-        getADateInThePast(minCollectionDays, maxCollectionDays, collectionDate)
-      ) as string,
-    },
-    sample: {
-      collection_date: collectionDate as string,
-      location_id: getValueOrDefault(
-        defaults?.sample?.location_id,
-        locationId
-      ) as number,
-      private: getValueOrDefault(
-        defaults?.sample?.private,
-        sample(trueOrFalse)
-      ) as boolean,
-      private_identifier: getValueOrDefault(
-        defaults?.sample?.private_identifier,
-        generatePrivateSampleId()
-      ) as string,
-      public_identifier: getValueOrDefault(
-        defaults?.sample?.public_identifier,
-        generatePublicSampleId()
-      ) as string,
-    },
+    collection_date: collectionDate,
+    location: getValueOrDefault(defaults?.location, sample(locations)),
+    private: getValueOrDefault(defaults?.private, false),
+    private_id: getValueOrDefault(
+      defaults?.private_id,
+      generatePrivateSampleId()
+    ),
+    public_id: getValueOrDefault(defaults?.public_id, generatePublicSampleId()),
+    sequencing_date: sequencingDate,
   };
 }
 
 /**
  * method creates synthetic data for stubbing get Sample api response
  * @param defaults - user supplied sample data to be included
- * @param maxDays  - specifies earliest day of the sample, defaults 10
- * @param minDays  - specifies lates day of the sample default 0 (today).
+ * @param maxCollectionDateAge  - specifies earliest day of the sample.
+ * e.g. 5 means sample collection date will be no earlier than 5 days
  * @returns GetSampleResponseData
  */
 export function getSampleResponseData(
-  defaults?: Partial<SampleResponseDefaults>,
-  minDays = 0,
-  maxDays = 10
+  defaults?: SampleResponseDefaults,
+  maxCollectionDateAge = 10
 ): SampleResponseData {
   return {
-    collection_date: getADateInThePast(minDays, maxDays),
+    collection_date: getADateInThePast(maxCollectionDateAge),
     collection_location: {
       country: "USA",
       division: "California",
-      id: getValueOrDefault(defaults?.collection_location, locationId),
+      id: getRandomNumber(),
       location: "Corodano",
       region: "California",
     },
-    czb_failed_genome_recovery: getValueOrDefault(
-      defaults?.czb_failed_genome_recovery,
-      sample(trueOrFalse)
-    ),
+    czb_failed_genome_recovery: true,
     gisaid: {
       gisaid_id: "",
       status: "Not Found",
@@ -95,7 +78,7 @@ export function getSampleResponseData(
     lineage: {
       confidence: "",
       last_updated: getADateInThePast(),
-      lineage: getValueOrDefault(defaults?.lineage, sample(lineages)),
+      lineage: sample(lineages),
       qc_status: "pass",
       scorpio_call: "Omicron (BA.1-like)",
       scorpio_support: 0.93,
@@ -115,69 +98,4 @@ export function getSampleResponseData(
       name: "Playwright",
     },
   };
-}
-
-export interface SampleUploadData {
-  pathogen_genome: {
-    sequence: string;
-    sequencing_date: string;
-  };
-  sample: {
-    collection_date: string;
-    location_id: number;
-    private: boolean;
-    private_identifier: string;
-    public_identifier: string;
-  };
-}
-
-export interface SampleResponseData {
-  id: number | undefined;
-  collection_date: string | undefined;
-  collection_location: {
-    id?: number;
-    region: string;
-    country: string;
-    division: string;
-    location: string;
-  };
-  czb_failed_genome_recovery?: boolean;
-  gisaid: {
-    gisaid_id: any;
-    status: string;
-  };
-  lineage: {
-    last_updated: string;
-    lineage?: string;
-    confidence: string;
-    version: string;
-    scorpio_call: string;
-    scorpio_support: number;
-    qc_status: string;
-  };
-  private?: boolean;
-  private_identifier: string;
-  public_identifier: string;
-  sequencing_date: string;
-  submitting_group: {
-    id: number;
-    name: string;
-  };
-  upload_date?: string;
-  uploaded_by: {
-    id: number;
-    name: string;
-  };
-}
-
-export interface SampleResponseDefaults {
-  collection_date?: string;
-  collection_location?: number;
-  czb_failed_genome_recovery?: boolean;
-  gisaid_id?: string | null;
-  gisaid_status?: string;
-  id?: number;
-  lineage?: string;
-  private?: boolean;
-  upload_date?: string;
 }
