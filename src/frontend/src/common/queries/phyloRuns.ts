93dae15f9e2aafc3f6f61057d9d05afd45cfdb51
Maya Lila <mlila@chanzuckerberg.com>
2022-10-20 08:00:13 -0700

    transform tree data in react query selector, like with other kinds of data

--- src/frontend/src/common/queries/phyloRuns.ts
+++ src/frontend/src/common/queries/phyloRuns.ts
@@ -5,6 +5,7 @@ import {
   useQueryClient,
   UseQueryResult,
 } from "@tanstack/react-query";
+import { map } from "lodash";
 import {
   DEFAULT_DELETE_OPTIONS,
   fetchPhyloRuns,
@@ -13,11 +14,29 @@ import {
   PhyloRunResponse,
 } from "../api";
 import { API_URL } from "../constants/ENV";
+import {
+  getDownloadLinks,
+  getTreeType,
+  reduceObjectArrayToLookupDict,
+} from "../utils/dataTransforms";
 import { ENTITIES } from "./entities";
 import { MutationCallbacks } from "./types";
 
 /* custom hook to automatically expire tree info when needed */
 /* such as when trees are deleted */
+// TODO-TR (mlila): types
+const mapPhyloRuns = (data) => {
+  const phyloRuns: any[] = data.phylo_runs;
+
+  const transformedRuns = phyloRuns.map((p) => ({
+    ...p,
+    ...getDownloadLinks(p),
+    treeType: getTreeType(p),
+  }));
+
+  return reduceObjectArrayToLookupDict(transformedRuns, "id");
+};
+
 export const USE_PHYLO_RUN_INFO = {
   entities: [ENTITIES.PHYLO_RUN_INFO],
   id: "phyloRunInfo",
@@ -26,6 +45,7 @@ export const USE_PHYLO_RUN_INFO = {
 export function usePhyloRunInfo(): UseQueryResult<PhyloRunResponse, unknown> {
   return useQuery([USE_PHYLO_RUN_INFO], fetchPhyloRuns, {
     retry: false,
+    select: mapPhyloRuns,
   });
 }
 
