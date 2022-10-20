fd840cd53d0127a0f15095b22cd9bfa36fc0d9dc
Maya Lila <mlila@chanzuckerberg.com>
2022-10-20 08:01:25 -0700

    set up tree view

--- src/frontend/src/views/Data/components/TreesView/index.tsx
+++ src/frontend/src/views/Data/components/TreesView/index.tsx
@@ -1,5 +1,35 @@
+import { map } from "lodash";
+import { useEffect, useState } from "react";
+import { HeadAppTitle } from "src/common/components";
+import { usePhyloRunInfo } from "src/common/queries/phyloRuns";
+
 const TreesView = (): JSX.Element => {
-  return <div>test</div>;
+  // initialize state
+  const [isDataLoading, setIsDataLoading] = useState(false);
+
+  // load tree data from server
+  const phyloRunResponse = usePhyloRunInfo();
+  const { data: phyloRuns, isLoading, isFetching } = phyloRunResponse;
+
+  // determine whether we should show loading ui or interactive ui
+  useEffect(() => {
+    setIsDataLoading(true);
+
+    if (isLoading || isFetching) return;
+
+    setIsDataLoading(false);
+  }, [isLoading, isFetching]);
+
+  console.log(phyloRuns);
+
+  return (
+    <>
+      <HeadAppTitle subTitle="Trees" />
+      {map(phyloRuns, (r) => (
+        <div>{r.id}</div>
+      ))}
+    </>
+  );
 };
 
 export { TreesView };
