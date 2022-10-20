3eaa45f0e3bdccb1e80ae54c41657528c479877a
Maya Lila <mlila@chanzuckerberg.com>
2022-10-20 08:01:03 -0700

    comment

--- src/frontend/src/common/titles.ts
+++ src/frontend/src/common/titles.ts
@@ -3,6 +3,5 @@ import { ROUTES } from "./routes";
 // For use in the `<head>` title of page
 export const PAGE_TITLES: Record<string, string> = {
   [ROUTES.HOMEPAGE]: "Public Health Open-Source Tool",
-  [ROUTES.DATA_SAMPLES]: "Samples",
   [ROUTES.PHYLO_TREES]: "Phylogenetic Trees",
 };
