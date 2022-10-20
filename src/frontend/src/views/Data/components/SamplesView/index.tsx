3053a10b758ab4703da7395a8cbda66bd90ec87a
Maya Lila <mlila@chanzuckerberg.com>
2022-10-20 08:01:15 -0700

    set up sample view

--- src/frontend/src/views/Data/components/SamplesView/index.tsx
+++ src/frontend/src/views/Data/components/SamplesView/index.tsx
@@ -1,5 +1,33 @@
+import { map } from "lodash";
+import { useEffect, useState } from "react";
+import { HeadAppTitle } from "src/common/components";
+import { useSampleInfo } from "src/common/queries/samples";
+
 const SamplesView = (): JSX.Element => {
-  return <div>test</div>;
+  // initialize state
+  const [isDataLoading, setIsDataLoading] = useState(false);
+
+  // load sample data from server
+  const sampleResponse = useSampleInfo();
+  const { data: samples, isLoading, isFetching } = sampleResponse;
+
+  // determine whether we should show loading ui or interactive ui
+  useEffect(() => {
+    setIsDataLoading(true);
+
+    if (isLoading || isFetching) return;
+
+    setIsDataLoading(false);
+  }, [isLoading, isFetching]);
+
+  return (
+    <>
+      <HeadAppTitle subTitle="Samples" />
+      {map(samples, (s) => (
+        <div>{s.publicId}</div>
+      ))}
+    </>
+  );
 };
 
 export { SamplesView };
