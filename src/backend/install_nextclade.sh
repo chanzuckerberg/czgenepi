ef3ede2b7cb99622fff02b069408ef28eadab70b
Phoenix Logan <phoenix@phoenixlogan.net>
2022-10-31 10:46:55 -0700

    remove nextclade install script

--- src/backend/install_nextclade.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-# install latest version of nextclade
-curl -fsSL "https://github.com/nextstrain/nextclade/releases/latest/download/nextclade-x86_64-unknown-linux-gnu" -o "nextclade" && chmod +x nextclade
-
-# test nextclade installed
-nextclade --version
