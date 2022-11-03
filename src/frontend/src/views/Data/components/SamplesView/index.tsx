963ec45f03b4f177fd554b61b99d05a36d2f9fd2
maya <mlila@chanzuckerberg.com>
2022-11-03 11:58:31 -0700

    add search and filter to samples table (#1432)
    * add search to samples tables
    
    * add filtering to samples table
    
    * types
    
    * also empty table when no results
    
    * better col defs and checkboxes in samples table
    
    * types

--- src/frontend/src/views/Data/components/SamplesView/index.tsx
+++ src/frontend/src/views/Data/components/SamplesView/index.tsx
@@ -1,9 +1,11 @@
 import { DefaultMenuSelectOption } from "czifui";
-import { compact, map, noop, uniq } from "lodash";
+import { compact, map, uniq } from "lodash";
 import { useEffect, useState } from "react";
 import { HeadAppTitle } from "src/common/components";
 import { useNewSampleInfo as useSampleInfo } from "src/common/queries/samples";
+import { IdMap } from "src/common/utils/dataTransforms";
 import { FilterPanel } from "src/components/FilterPanel";
+import { SearchBar } from "src/components/Table/components/SearchBar";
 import { StyledView } from "../../style";
 import { DataNavigation } from "../DataNavigation";
 import { SamplesTable } from "./components/SamplesTable";
@@ -13,20 +15,41 @@ const SamplesView = (): JSX.Element => {
   // initialize state
   // TODO-TR (mlilia): types
   const [isFilterPanelOpen, setIsFilterPanelOpen] = useState<boolean>(true);
-  // @ts-expect-error: temp
-  const [dataFilterFunc, setDataFilterFunc] = useState<any>(); // eslint-disable-line
+  const [activeFilterCount, setActiveFilterCount] = useState<number>(0);
+  const [dataFilterFunc, setDataFilterFunc] = useState<any>();
   const [lineages, setLineages] = useState<DefaultMenuSelectOption[]>([]);
+  // filters rows for current search query
+  const [searchResults, setSearchResults] = useState<IdMap<Sample>>({});
+  // rows that are actually shown in the table
+  const [displayedRows, setDisplayedRows] = useState<IdMap<Sample>>({});
 
   // load sample data from server
   const { data: samples, isFetching, isLoading } = useSampleInfo();
 
+  // only display rows that match the current search and the current filters
+  useEffect(() => {
+    const hasSearchFilteredRows = Object.keys(searchResults).length > 0;
+    if (!hasSearchFilteredRows) {
+      setDisplayedRows({});
+      return;
+    }
+
+    if (!dataFilterFunc) {
+      setDisplayedRows(searchResults);
+      return;
+    }
+
+    const filteredRows = dataFilterFunc(searchResults);
+    setDisplayedRows(filteredRows);
+  }, [searchResults, dataFilterFunc]);
+
   // update list of lineages to use in the filter panel on the left side of the screen
   useEffect(() => {
-    const lineages = uniq(compact(map(samples, (d) => d.lineage?.lineage)))
+    const newLineages = uniq(compact(map(samples, (d) => d.lineage?.lineage)))
       .sort()
       .map((name) => ({ name }));
 
-    setLineages(lineages);
+    setLineages(newLineages);
   }, [samples]);
 
   const toggleFilterPanel = () => {
@@ -38,18 +61,24 @@ const SamplesView = (): JSX.Element => {
       <HeadAppTitle subTitle="Samples" />
       <DataNavigation
         shouldShowSampleFilterToggle
-        activeFilterCount={0} // TODO-TR (mlila): use actual count
+        activeFilterCount={activeFilterCount}
         toggleFilterPanel={toggleFilterPanel}
       />
       <Flex>
         <FilterPanel
           lineages={lineages}
           isOpen={isFilterPanelOpen}
-          setActiveFilterCount={noop}
+          setActiveFilterCount={setActiveFilterCount}
           setDataFilterFunc={setDataFilterFunc}
           data-test-id="menu-item-sample-count"
         />
-        <SamplesTable isLoading={isLoading || isFetching} data={samples} />
+        <div>
+          <SearchBar tableData={samples} onSearchComplete={setSearchResults} />
+          <SamplesTable
+            isLoading={isLoading || isFetching}
+            data={displayedRows}
+          />
+        </div>
       </Flex>
     </StyledView>
   );
