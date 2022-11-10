d64d97f25186476e681fad4c0c82e7a3e613f576
Jessica Gadling <jgadling@chanzuckerberg.com>
2022-11-09 18:02:18 -0800

    remove unneeded tests.

--- src/backend/pipeline_tests/test_gisaid_pipeline.py
+++ src/backend/pipeline_tests/test_gisaid_pipeline.py
@@ -13,25 +13,6 @@ from aspen.database.models import (
 )
 
 
-def test_gisiad_db_objects_were_created(session):
-    sample = (
-        session.execute(
-            sa.select(GisaidMetadata).where(
-                GisaidMetadata.strain == "Wuhan/TEST_SAMPLE/2019"
-            )
-        )
-        .scalars()
-        .one()
-    )
-    assert sample.strain == "Wuhan/TEST_SAMPLE/2019"
-    assert sample.pango_lineage == "B"
-    assert sample.region == "Asia"
-    assert sample.country == "China"
-    assert sample.division == "Hubei"
-    assert sample.location == "Test Location"
-    assert sample.gisaid_epi_isl == "TEST_EPI_ISL_123"
-
-
 def test_public_repo_db_objects_were_created(session):
     sample = (
         session.execute(
@@ -46,12 +27,12 @@ def test_public_repo_db_objects_were_created(session):
         .one()
     )
     assert sample.strain == "Wuhan/TEST_SAMPLE/2019"
-    assert sample.pango_lineage == "B"
+    assert sample.lineage == "B"
     assert sample.region == "Asia"
     assert sample.country == "China"
     assert sample.division == "Hubei"
     assert sample.location == "Test Location"
-    assert sample.gisaid_epi_isl == "TEST_EPI_ISL_123"
+    assert sample.isl == "TEST_EPI_ISL_123"
 
 
 def test_new_workflow_artifacts_exist(session):
@@ -69,20 +50,3 @@ def test_new_workflow_artifacts_exist(session):
     today = datetime.datetime.now().strftime("%Y%m%d")
     assert today in aligned_dump.sequences_s3_key
     assert today in aligned_dump.metadata_s3_key
-
-
-def test_locations_import(session):
-    location = (
-        session.execute(  # type: ignore
-            sa.select(Location)  # type: ignore
-            .where(Location.location == "Test Location")  # type: ignore
-            .limit(1)  # type: ignore
-        )
-        .scalars()
-        .one()
-    )
-
-    assert location.region == "Asia"
-    assert location.country == "China"
-    assert location.division == "Hubei"
-    assert location.location == "Test Location"
