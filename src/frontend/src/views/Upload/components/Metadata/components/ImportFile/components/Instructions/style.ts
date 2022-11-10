a214bc467db354a1b209a68cb186c684b071863a
maya <mlila@chanzuckerberg.com>
2022-11-10 11:15:30 -0800

    Update src/frontend/src/views/Upload/components/Metadata/components/ImportFile/components/Instructions/style.ts

--- src/frontend/src/views/Upload/components/Metadata/components/ImportFile/components/Instructions/style.ts
+++ src/frontend/src/views/Upload/components/Metadata/components/ImportFile/components/Instructions/style.ts
@@ -13,7 +13,7 @@ export const SemiBold = styled.span`
     const fontWeights = getFontWeights(props);
 
     return `
-      font-weight: ${fontWeights?.semibold}
+      font-weight: ${fontWeights?.semibold};
     `;
   }}
 `;
