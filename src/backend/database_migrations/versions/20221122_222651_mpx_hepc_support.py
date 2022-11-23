da7f01d8c64bfc3232e7598476a69d91d799771e
Phoenix Logan <phoenix@phoenixlogan.net>
2022-11-23 10:04:05 -0800

    update nextval for pathogen_repo_configs_id_seq

--- src/backend/database_migrations/versions/20221122_222651_mpx_hepc_support.py
+++ src/backend/database_migrations/versions/20221122_222651_mpx_hepc_support.py
@@ -17,16 +17,17 @@ depends_on = None
 def upgrade():
     op.execute(
         """
-        INSERT INTO aspen.pathogens (id, slug, name) VALUES ((SELECT MAX(id)+1 FROM aspen.pathogens), 'HCV', 'Hepatitis C'), ((SELECT MAX(id)+2 FROM aspen.pathogens), 'MPX', 'Monkeypox')
+        INSERT INTO aspen.pathogens (slug, name) VALUES ('HCV', 'Hepatitis C'), ('MPX', 'Monkeypox')
         ON CONFLICT (slug) DO NOTHING
         """
     )  # on conflict do nothing is because in local dev we already have MPX in the database
 
+    op.execute("select nextval('aspen.pathogen_repo_configs_id_seq')")
     op.execute(
         """
-        INSERT INTO aspen.pathogen_repo_configs (id, prefix, public_repository_id, pathogen_id) VALUES
-             ( (SELECT MAX(id)+1 FROM aspen.pathogen_repo_configs), 'hcv', (SELECT id from aspen.public_repositories WHERE name='GenBank'), (SELECT id from aspen.pathogens WHERE slug='HCV') ),
-             ( (SELECT MAX(id)+2 FROM aspen.pathogen_repo_configs), 'hMpxV', (SELECT id from aspen.public_repositories WHERE name='GenBank'), (SELECT id from aspen.pathogens WHERE slug='MPX') )
+        INSERT INTO aspen.pathogen_repo_configs (prefix, public_repository_id, pathogen_id) VALUES
+             ( 'hcv', (SELECT id from aspen.public_repositories WHERE name='GenBank'), (SELECT id from aspen.pathogens WHERE slug='HCV') ),
+             ( 'hMpxV', (SELECT id from aspen.public_repositories WHERE name='GenBank'), (SELECT id from aspen.pathogens WHERE slug='MPX') )
         """
     )
 
