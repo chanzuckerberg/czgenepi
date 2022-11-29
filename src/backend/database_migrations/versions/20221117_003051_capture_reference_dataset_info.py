8c46fc55c55dedbfb119c69fc72b5f40e72f7b9e
vincent-czi <89553795+vincent-czi@users.noreply.github.com>
2022-11-28 16:33:54 -0800

    Migrations: Lineage QC (#1477)
    * Migration to capture ref data info
    
    * Migration: Add enum tables for QC & Mutations
    
    This migration depends on the target DB having no data for any of
    tables sample_qc_metrics and sample_mutations because of the NOT NULL
    constraint on the foreign key to the new enum tables. If our DBs
    do have that, need to go back and modify this migration
    
    * Migration: Add MPX pathogen and nextclade datasets
    
    * Migration: qc_score nullable
    
    * Quiet merge conflict around DB migrations
    
    * Migration: linting

---
+++ src/backend/database_migrations/versions/20221117_003051_capture_reference_dataset_info.py
@@ -0,0 +1,62 @@
+"""Capture reference dataset info
+
+Create Date: 2022-11-17 00:30:52.641984
+
+"""
+import enumtables  # noqa: F401
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "20221117_003051"
+down_revision = "20221101_201105"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        "sample_lineages",
+        sa.Column("reference_dataset_name", sa.String(), nullable=True),
+        schema="aspen",
+    )
+    op.add_column(
+        "sample_lineages",
+        sa.Column("reference_sequence_accession", sa.String(), nullable=True),
+        schema="aspen",
+    )
+    op.add_column(
+        "sample_lineages",
+        sa.Column("reference_dataset_tag", sa.String(), nullable=True),
+        schema="aspen",
+    )
+    op.add_column(
+        "sample_mutations",
+        sa.Column("reference_sequence_accession", sa.String(), nullable=True),
+        schema="aspen",
+    )
+    op.add_column(
+        "sample_qc_metrics",
+        sa.Column("reference_dataset_name", sa.String(), nullable=True),
+        schema="aspen",
+    )
+    op.add_column(
+        "sample_qc_metrics",
+        sa.Column("reference_sequence_accession", sa.String(), nullable=True),
+        schema="aspen",
+    )
+    op.add_column(
+        "sample_qc_metrics",
+        sa.Column("reference_dataset_tag", sa.String(), nullable=True),
+        schema="aspen",
+    )
+
+
+def downgrade():
+    op.drop_column("sample_qc_metrics", "reference_dataset_tag", schema="aspen")
+    op.drop_column("sample_qc_metrics", "reference_sequence_accession", schema="aspen")
+    op.drop_column("sample_qc_metrics", "reference_dataset_name", schema="aspen")
+    op.drop_column("sample_mutations", "reference_sequence_accession", schema="aspen")
+    op.drop_column("sample_lineages", "reference_dataset_tag", schema="aspen")
+    op.drop_column("sample_lineages", "reference_sequence_accession", schema="aspen")
+    op.drop_column("sample_lineages", "reference_dataset_name", schema="aspen")
