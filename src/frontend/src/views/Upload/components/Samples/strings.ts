bb468f81ff6b49207790ac2cf11654e26fa69d39
ehoops-cz <109251328+ehoops-cz@users.noreply.github.com>
2022-11-30 15:09:28 -0800

    [SC-225224] copy changes to upload flow (#1483)
    * ImportFile strings.
    
    * Copy updates.
    
    * copy
    
    * More copy updates.
    
    * Rename strings.ts to pathogenConfig.ts
    
    * linting.
    
    * Add Nextstrain defaults link

--- src/frontend/src/views/Upload/components/Samples/strings.ts
+++ src/frontend/src/views/Upload/components/Samples/strings.ts
@@ -1,5 +1,5 @@
 import { Pathogen } from "src/common/redux/types";
-import { PathogenConfigType } from "src/common/types/strings";
+import { PathogenConfigType } from "src/common/types/pathogenConfig";
 
 interface UploadSamplesStrings {
   header: string;
@@ -12,10 +12,9 @@ export const pathogenStrings: PathogenConfigType<UploadSamplesStrings> = {
     acceptedFormats:
       "Accepted file formats: fasta (.fa or .fasta), fasta.gz (.fa.gz), fasta.zip, plain text (.txt)",
   },
-  // TODO: (ehoops) - these are just copy pasta to appease typescript. Copy changes have their own ticket.
   [Pathogen.MONKEY_POX]: {
-    header: "Select SARS-CoV-2 Consensus Genome Files",
+    header: "Select Mpox Consensus Genome Files",
     acceptedFormats:
-      "Accepted file formats: fasta (.fa or .fasta), fasta.gz (.fa.gz), fasta.zip, plain text (.txt)",
+      "Accepted file formats: fasta (.fa or .fasta), fasta.gz (.fa.gz), fasta.zip",
   },
 };
