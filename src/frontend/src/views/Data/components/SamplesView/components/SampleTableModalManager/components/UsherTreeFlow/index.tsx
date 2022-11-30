74245c2452e7c731f9632378d84357b257b63754
lvreynoso <lreynoso@chanzuckerberg.com>
2022-11-30 14:14:39 -0800

    Select db query param argument based on current pathogen

--- src/frontend/src/views/Data/components/SamplesView/components/SampleTableModalManager/components/UsherTreeFlow/index.tsx
+++ src/frontend/src/views/Data/components/SamplesView/components/SampleTableModalManager/components/UsherTreeFlow/index.tsx
@@ -3,6 +3,7 @@ import { EVENT_TYPES } from "src/common/analytics/eventTypes";
 import { analyticsTrackEvent } from "src/common/analytics/methods";
 import { addNotification } from "src/common/redux/actions";
 import { useDispatch } from "src/common/redux/hooks";
+import { Pathogen } from "src/common/redux/types";
 import { ROUTES } from "src/common/routes";
 import { NotificationComponents } from "src/components/NotificationManager/components/Notification";
 import { UsherConfirmationModal } from "./components/UsherConfirmationModal";
@@ -14,14 +15,20 @@ interface Props {
   shouldStartUsherFlow: boolean;
 }
 
+const USHER_DBS: Record<Pathogen, string> = {
+  SC2: "wuhCor1",
+  MPX: "hub_3471181_GCF_014621545.1",
+};
+
 const generateUsherLink = (
   remoteFile: string,
   treeType: string,
-  sampleCount: number
+  sampleCount: number,
+  pathogen: Pathogen
 ) => {
   const encodedFileLink = encodeURIComponent(remoteFile);
 
-  const DB_PARAM = `db=wuhCor1`;
+  const DB_PARAM = USHER_DBS[pathogen];
   const FILE_PARAM = `remoteFile=${encodedFileLink}`;
   const TREE_TYPE_PARAM = `phyloPlaceTree=${treeType}`;
   const SAMPLE_COUNT_PARAM = `subtreeSize=${sampleCount}`;
@@ -65,9 +72,10 @@ const UsherTreeFlow = ({
   const onLinkCreateSuccess = (
     url: string,
     treeType: string,
-    sampleCount: number
+    sampleCount: number,
+    pathogen: Pathogen
   ) => {
-    const usherLink = generateUsherLink(url, treeType, sampleCount);
+    const usherLink = generateUsherLink(url, treeType, sampleCount, pathogen);
     setUsherLink(usherLink);
     setIsConfirmOpen(true);
   };
