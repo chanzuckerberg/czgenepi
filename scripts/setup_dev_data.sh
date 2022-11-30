13bdbcaa1879179282c689c4773120794ee2d271
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2022-11-30 12:29:57 -0800

    Remove duplicated local SSM param in setup

--- scripts/setup_dev_data.sh
+++ scripts/setup_dev_data.sh
@@ -154,21 +154,6 @@ ${local_aws} ssm put-parameter --name /genepi/local/localstack/nextstrain-sfn --
   "RunSPOTVcpu":10,
   "StateMachineArn":'${LOCAL_SFN_ARN}'
 }'
-${local_aws} ssm delete-parameter --name /genepi/local/localstack/pangolin-ondemand-sfn
-${local_aws} ssm put-parameter --name /genepi/local/localstack/pangolin-ondemand-sfn --value '{
-  "Input":{
-    "Run":{
-      "aws_region":"us-west-2",
-      "docker_image_id":"genepi-pangolin"}
-    },
-  "OutputPrefix":"s3://aspen-batch/pangolin-ondemand-sfn/results",
-  "RUN_WDL_URI":"s3://aspen-batch/pangolin-ondemand.wdl-v0.0.1.wdl",
-  "RunEC2Memory":120000,
-  "RunEC2Vcpu":1,
-  "RunSPOTMemory":120000,
-  "RunSPOTVcpu":1,
-  "StateMachineArn":'${LOCAL_SFN_ARN}'
-}'
 ${local_aws} ssm delete-parameter --name /genepi/local/localstack/lineage-qc-ondemand-sfn
 ${local_aws} ssm put-parameter --name /genepi/local/localstack/lineage-qc-ondemand-sfn --value '{
   "Input":{
