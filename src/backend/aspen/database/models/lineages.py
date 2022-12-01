98a91e7b7d527580485ef0739cf99bea343282eb
Phoenix Logan <phoenix@phoenixlogan.net>
2022-12-01 09:54:50 -0800

    migration to populate last_updated

--- src/backend/aspen/database/models/lineages.py
+++ src/backend/aspen/database/models/lineages.py
@@ -9,6 +9,7 @@ from sqlalchemy import (
     PrimaryKeyConstraint,
     String,
     UniqueConstraint,
+    DateTime,
 )
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import relationship
@@ -107,6 +108,7 @@ class SampleLineage(idbase):  # type: ignore
     lineage = Column(String, nullable=False)
     lineage_probability = Column(Float, nullable=True)
     raw_lineage_output = Column(JSONB, nullable=True)
+    last_updated= Column(DateTime, nullable=True)
 
     # For Nextclade, we need to track the underlying reference data bundle
     # that was involved to know how the lineage call was made.
