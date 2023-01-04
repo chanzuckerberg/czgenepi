f04fec8e465158524a616dbed2dc0896ce3bd28d
ehoops-cz <erin.hoops+gh@chanzuckerberg.com>
2023-01-04 15:25:15 -0800

    Copy changes for covid too.

--- src/frontend/src/views/Data/components/SamplesView/components/SampleTableModalManager/components/CreateNSTreeModal/components/RadioLabel/strings.ts
+++ src/frontend/src/views/Data/components/SamplesView/components/SampleTableModalManager/components/CreateNSTreeModal/components/RadioLabel/strings.ts
@@ -20,21 +20,21 @@ export const pathogenStrings: PathogenConfigType<RadioLabelStrings> = {
     nonContextualizedBestFor:
       "Best for uncovering sampling bias in your own sampling effort.",
     nonContextualizedDescription:
-      "Includes samples from only your jurisdiction from both CZ GEN EPI and GISAID.",
+      "Builds tree with only samples of interest (defined by location, date, and lineage).",
     nonContextualizedGoodFor:
-      "Good for seeing viral diversity in your jurisdiction that may not be captured by your own sampling effort.",
+      "Good for seeing viral diversity in the public repository that may not be captured by your own sampling effort.",
     nonContextualizedNotRecommended:
-      "Not recommended for epidemiological interpretation due to lack of visibility into viral diversity outside of your jurisdiction and omission of closely-related samples.",
+      "Not recommended for epidemiological interpretation due to lack of visibility into viral diversity outside of the selections below and omission of closely-related samples.",
     overviewBestFor:
-      "Best for generating a summary tree of samples from your jurisdiction, in the context of genetically similar GISAID samples from outside of your jurisdiction.",
+      "Best for generating a summary tree of samples of interest, in the context of genetically similar GISAID samples.",
     overviewDescription:
-      "Includes samples from both within and outside of your jurisdiction, at a ratio of roughly 2:1.",
+      "Builds tree focused around samples of interest (defined by location, date, and lineage) and closely-related samples, at a ratio of roughly 2:1.",
     overviewGoodFor1: "Good for identifying possible local outbreaks.",
     overviewGoodFor2:
-      "Good for specifying lineage or collection date range to customize samples from your jurisdiction.",
+      "Good for specifying sample location, collection date, or lineages instead of the defaults used for your CZ GEN EPI weekly build.",
     targetedBestFor: "Best for investigating an identified outbreak.",
     targetedDescription:
-      "Includes selected samples and samples that are closely related to the selected samples, at a ratio of roughly 1:2.",
+      "Builds tree with selected samples (from the sample table or the ID box below) and closely-related samples, at a ratio of roughly 1:2.",
     targetedGoodFor:
       "Good for identifying samples most closely related to the selected samples among all samples in GISAID and your CZ GEN EPI samples.",
   },
