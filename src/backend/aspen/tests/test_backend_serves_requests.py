5e41b9608dfe595ca5c1f9b0a524042258f07599
Phoenix Logan <phoenix@phoenixlogan.net>
2023-01-11 16:10:54 -0800

    lint

--- src/backend/aspen/tests/test_backend_serves_requests.py
+++ src/backend/aspen/tests/test_backend_serves_requests.py
@@ -2,6 +2,8 @@ import requests
 
 
 def test_backend_serves_requests():
-    
-    backend_request = requests.get("http://backend.genepinet.localdev:3000/v2/openapi.json")
+
+    backend_request = requests.get(
+        "http://backend.genepinet.localdev:3000/v2/openapi.json"
+    )
     assert backend_request.status_code == 200
