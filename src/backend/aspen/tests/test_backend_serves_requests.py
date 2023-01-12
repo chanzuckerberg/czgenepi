3a3c96a7f27c1f1fcc9a739ae4bbb6379c4600b1
Phoenix Logan <phoenix@phoenixlogan.net>
2023-01-12 10:40:37 -0800

    including staging and prod checks

--- src/backend/aspen/tests/test_backend_serves_requests.py
+++ src/backend/aspen/tests/test_backend_serves_requests.py
@@ -1,9 +1,11 @@
-import requests
+import urllib.request
 
 
 def test_backend_serves_requests():
-
-    backend_request = requests.get(
-        "http://backend.genepinet.localdev:3000/v2/health"
-    )
-    assert backend_request.status_code == 200
+    for env in [
+        "http://backend.genepinet.localdev:3000",
+        "https://api.staging.czgenepi.org",
+        "https://api.czgenepi.org",
+    ]:
+        request = urllib.request.urlopen(f"{env}/v2/health")
+        assert request.status == 200
