b629bf490818532775521385d2ac9ed183437095
Jessica Gadling <jgadling@chanzuckerberg.com>
2023-01-12 16:10:13 -0800

    Revert "including staging and prod checks"
    This reverts commit 3a3c96a7f27c1f1fcc9a739ae4bbb6379c4600b1.

--- src/backend/aspen/tests/test_backend_serves_requests.py
+++ src/backend/aspen/tests/test_backend_serves_requests.py
@@ -1,11 +1,9 @@
-import urllib.request
+import requests
 
 
 def test_backend_serves_requests():
-    for env in [
-        "http://backend.genepinet.localdev:3000",
-        "https://api.staging.czgenepi.org",
-        "https://api.czgenepi.org",
-    ]:
-        request = urllib.request.urlopen(f"{env}/v2/health")
-        assert request.status == 200
+
+    backend_request = requests.get(
+        "http://backend.genepinet.localdev:3000/v2/health"
+    )
+    assert backend_request.status_code == 200
