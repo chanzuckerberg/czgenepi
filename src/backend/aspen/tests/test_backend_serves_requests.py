e25df1793d20a4b0eb3e2346050ebd1cb2ca5d46
Jessica Gadling <jgadling@chanzuckerberg.com>
2023-01-12 16:24:03 -0800

    Fix async tests.

--- src/backend/aspen/tests/test_backend_serves_requests.py
+++ src/backend/aspen/tests/test_backend_serves_requests.py
@@ -3,7 +3,5 @@ import requests
 
 def test_backend_serves_requests():
 
-    backend_request = requests.get(
-        "http://backend.genepinet.localdev:3000/v2/health"
-    )
+    backend_request = requests.get("http://backend.genepinet.localdev:3000/v2/health")
     assert backend_request.status_code == 200
