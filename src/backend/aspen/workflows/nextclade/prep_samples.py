b25c3b614974d415c82c386101e6581808b1dbbb
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2023-01-20 14:14:31 -0800

    WIP: Pull sample ids for new nextclade RunTypes

--- src/backend/aspen/workflows/nextclade/prep_samples.py
+++ src/backend/aspen/workflows/nextclade/prep_samples.py
@@ -13,6 +13,7 @@ import subprocess
 import click
 import sqlalchemy as sa
 from sqlalchemy.orm import joinedload
+from sqlalchemy.orm.session import Session
 
 from aspen.config.config import Config
 from aspen.database.connection import (
@@ -21,8 +22,9 @@ from aspen.database.connection import (
     session_scope,
     SqlAlchemyInterface,
 )
-from aspen.database.models import Pathogen, Sample, UploadedPathogenGenome
+from aspen.database.models import Pathogen, Sample, SampleQCMetric, UploadedPathogenGenome, QCMetricCaller
 from aspen.workflows.nextclade.utils import extract_dataset_info
+
 
 # Running this CLI script must be one of these types of runs.
 class RunType(str, Enum):  # str mix-in gives nice == compare against strings
@@ -67,18 +69,49 @@ def cli(
         to samples by PK, not by private/public identifier.
     - pathogen_info_fh: Write out pathogen info for later use in workflow
     """
-    # print(run_type == RunType.SPECIFIED_IDS_ONLY)  # REMOVE
-
-    # VOODOO temp workshop
-    pathogen = "sars-cov-2"
-    curr_dataset_info = get_nextclade_dataset_info(pathogen, NEXTCLADE_DATASET_DIR, NEXTCLADE_TAG_FILENAME)
-    print(curr_dataset_info)
-    raise RuntimeError('donezo')
-    # END VOODOO
-
-    sample_ids: list[int] = [int(id_line) for id_line in sample_ids_fh]
     interface: SqlAlchemyInterface = init_db(get_db_uri(Config()))
     with session_scope(interface) as session:
+        print(f"Getting pathogen data for {pathogen_slug}")
+        target_pathogen_query: Pathogen = sa.select(Pathogen).filter(
+            Pathogen.slug == pathogen_slug
+        )
+        target_pathogen: Pathogen = session.execute(target_pathogen_query).scalars().one()
+
+        # This downloads the dataset info to disk for later use by nextclade.
+        # VOODOO TODO turn this back on, just want to avoid pointless download
+        # nextclade_dataset_info = download_nextclade_dataset(
+        #     target_pathogen.nextclade_dataset_name,
+        #     NEXTCLADE_DATASET_DIR,
+        #     NEXTCLADE_TAG_FILENAME,
+        #     )
+        nextclade_dataset_info = {'name': 'hMPXV', 'accession': 'NC_063383.1', 'tag': '2022-11-03T12:00:00Z'}
+
+        # Figure out which samples we need to run Nextclade on
+        sample_ids: list[int] = []
+        if run_type == RunType.SPECIFIED_IDS_ONLY:
+            sample_ids = [int(id_line) for id_line in sample_ids_fh]
+        elif run_type == RunType.REFRESH_STALE:
+            sample_ids = get_sample_ids_to_refresh(
+                session,
+                target_pathogen,
+                nextclade_dataset_info["name"],
+                nextclade_dataset_info["accession"],
+                nextclade_dataset_info["tag"],
+            )
+        elif run_type == RunType.FORCE_ALL:
+            sample_ids = get_all_sample_ids_for_pathogen(
+                session,
+                target_pathogen,
+            )
+        # TODO exit out gracefully if sample_ids is empty
+        # maybe wig out up above if it's a SPECIFIED_IDS_ONLY and empty?
+        print(f"{'=' * 30} BEGIN DEBUG PRINTING {'=' * 30}")  # REMOVE
+        print('sample_ids')  # REMOVE
+        print(sample_ids)  # REMOVE
+        print('len(sample_ids)')  # REMOVE
+        print(len(sample_ids))  # REMOVE
+        print(f"{'=' * 30}  END DEBUG PRINTING  {'=' * 30}")  # REMOVE
+
         print(f"Getting pathogen data for {pathogen_slug}")
         target_pathogen_query: Pathogen = sa.select(Pathogen).filter(
             Pathogen.slug == pathogen_slug
@@ -142,7 +175,7 @@ def download_nextclade_dataset(dataset_name: str, output_dir: str, tag_filename:
     to maintain two sources of truth. The overall bundle download is not
     very large and we do go on to use it if there's any calls to make, so
     that seemed reasonable to me. But if downloading here causes problems,
-    try switching to just fetchign the tag JSON and parsing it.
+    try switching to just fetching the tag JSON and parsing it.
     """
     subprocess.run(
         ['nextclade', 'dataset', 'get', '--name', dataset_name, '--output-dir', output_dir],
@@ -151,6 +184,72 @@ def download_nextclade_dataset(dataset_name: str, output_dir: str, tag_filename:
         )
     with open(Path(output_dir, tag_filename)) as tag_fh:
         return extract_dataset_info(tag_fh)
+
+
+def get_sample_ids_to_refresh(
+    session: Session,
+    target_pathogen: Pathogen,
+    latest_dataset_name: str,
+    latest_sequence_accession: str,
+    latest_dataset_tag: str,
+) -> list[int]:
+    """Determine which sample_ids need to have their Nextclade call refreshed.
+
+    The intent of this is to identify all samples of a given pathogen where:
+        A) The sample has never had a qc_metric (model: SampleQCMetric) called
+        against it, and thus needs to be called.
+            -- OR --
+        B) The sample has had a qc_metric called against it, but the underlying
+        dataset that was used for the call is stale (e.g., tag does not match
+        latest tag, etc). Two additional notes:
+            B.1: We are only concerned with qc_metrics that used Nextclade as
+            the calling tool. We'll need to figure out how to handle multiple
+            calling tools down the road once/if we support that.
+            B.2: Any Nextclade-called qc_metric **should** have name/accession/
+            tag, but it's possible for a bug or future change to accidentally
+            break this assumption and leave those cols as NULL. In that case,
+            we should consider the qc_metric stale and refresh the sample.
+    """
+    # The outer join here lets us also filter on samples with no qc_metric
+    refresh_samples_q = sa.select(Sample).join(Sample.qc_metrics, isouter=True).filter(
+        Sample.pathogen_id == target_pathogen.id,
+        sa.or_(
+            # B/c outer join, if no qc_metric, then no values, so id is NULL
+            SampleQCMetric.id == None,
+            sa.and_(
+                SampleQCMetric.qc_caller == QCMetricCaller.NEXTCLADE,
+                sa.or_(
+                    # The dual comparisons against not being the given value or
+                    # being None/NULL is due to the fact that SQL comparisons
+                    # ignore NULL when comparing values. If we want to check
+                    # against NULL as well, we have to explicitly check both.
+                    SampleQCMetric.reference_dataset_name != latest_dataset_name,
+                    SampleQCMetric.reference_dataset_name == None,
+                    SampleQCMetric.reference_sequence_accession != latest_sequence_accession,
+                    SampleQCMetric.reference_sequence_accession == None,
+                    SampleQCMetric.reference_dataset_tag != latest_dataset_tag,
+                    SampleQCMetric.reference_dataset_tag == None,
+                )
+            )
+        )
+    )
+    samples_to_refresh: list[Sample] = session.execute(refresh_samples_q).unique().scalars()
+    return [sample.id for sample in samples_to_refresh]
+
+
+def get_all_sample_ids_for_pathogen(
+    session: Session,
+    target_pathogen: Pathogen,
+) -> list[int]:
+    """Gets all the sample_ids for given pathogen.
+
+    Intent here is to have an easy way to pull all the samples for a pathogen
+    when it's a RunType.FORCE_ALL.
+    """
+    all_samples_for_pathogen_q = sa.select(Sample).filter(
+        Sample.pathogen_id == target_pathogen.id)
+    all_samples_for_pathogen: list[Sample] = session.execute(all_samples_for_pathogen_q).scalars()
+    return [sample.id for sample in all_samples_for_pathogen]
 
 
 def save_pathogen_info(pathogen_info_fh: IO[str], pathogen: Pathogen):
