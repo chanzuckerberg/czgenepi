6be097e1b0df93ed3e88672065c15fd5f96f39f9
ehoops-cz <erin.hoops+gh@chanzuckerberg.com>
2023-02-02 13:28:21 -0800

    Actually add stuff, fix flask size

---
+++ src/frontend/src/views/Data/components/SamplesView/components/BlankState/style.ts
@@ -0,0 +1,67 @@
+import styled from "@emotion/styled";
+import {
+  CommonThemeProps,
+  fontBodyM,
+  fontHeaderM,
+  fontHeaderXl,
+  getSpaces,
+  Icon,
+  Link,
+} from "czifui";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-self: center;
+  ${(props: CommonThemeProps) => {
+    const spaces = getSpaces(props);
+    return `
+    margin-top: ${spaces?.xl}px;
+  `;
+  }}
+`;
+
+export const FlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 240px;
+`;
+
+const marginBottomL = (props: CommonThemeProps) => {
+  const spaces = getSpaces(props);
+  return `
+    margin-bottom: ${spaces?.l}px;
+  `;
+};
+
+export const Header = styled.h1`
+  ${fontHeaderXl}
+  ${marginBottomL}
+`;
+
+export const BodyText = styled.p`
+  ${fontBodyM}
+  ${marginBottomL}
+  margin-top: 0;
+`;
+
+export const StyledLink = styled(Link)`
+  display: flex;
+  flex-direction: row;
+  cursor: pointer;
+`;
+
+export const StyledLinkText = styled.p`
+  margin: 0;
+  ${fontHeaderM}
+  ${(props: CommonThemeProps) => {
+    const spaces = getSpaces(props);
+    return `
+    margin-right: ${spaces?.xs}px;
+  `;
+  }}
+`;
+
+export const StyledLinkIcon = styled(Icon)`
+  align-self: center;
+`;
