b11d07ecb743afb928350a9b57d9a7d404e2ba6f
ehoops-cz <109251328+ehoops-cz@users.noreply.github.com>
2023-02-03 13:54:17 -0800

    Tree location default - use max depth (#1600)
    * wip
    
    * Use max depth to set nextstrain tree modal default location.
    
    * wip
    
    * remove unnecessary ?
    
    * Use query key to avoid unneeded refetch.
    
    * Make sure query param updates when switching between pathogens.
    
    * Add comment
    
    * lint rolling.

--- src/frontend/src/common/queries/locations.ts
+++ src/frontend/src/common/queries/locations.ts
@@ -12,14 +12,42 @@ export interface LocationsResponse {
   locations: GisaidLocation[];
 }
 
-const locationDepthPathogenConfig: PathogenConfigType<
+export const locationDepthPathogenConfig: PathogenConfigType<
   keyof GisaidLocation | null
 > = {
   [Pathogen.COVID]: null,
   [Pathogen.MONKEY_POX]: "division",
 };
 
-const fetchLocations = (): Promise<LocationsResponse> => {
+const fetchAllLocations = (): Promise<LocationsResponse> => {
+  return getBackendApiJson(API.LOCATIONS);
+};
+
+type QueryKeyType = {
+  entities: ENTITIES[];
+  id: string;
+};
+
+export const USE_LOCATIONS_INFO_QUERY_KEY: QueryKeyType = {
+  entities: [ENTITIES.LOCATION_INFO],
+  id: "locationInfo",
+};
+
+const ONE_HOUR = 1000 * 60 * 60; // for react-query; milliseconds
+
+export function useLocations(): UseQueryResult<LocationsResponse, unknown> {
+  return useQuery([USE_LOCATIONS_INFO_QUERY_KEY], fetchAllLocations, {
+    retry: false,
+    // Because locations are very stable and mildly heavy (~2Meg), we mark
+    // stale and re-fetch after an hour rather than default insta-stale.
+    // Note, this does not prevent garbage cleanup due to `cacheTime` if user
+    // does not have data "actively" used. In that case, re-fetch would happen
+    // next time a component using this data mounts and uses this func.
+    staleTime: ONE_HOUR,
+  });
+}
+
+const fetchPathogenDepthLocations = (): Promise<LocationsResponse> => {
   const state = store.getState();
   const pathogen = selectCurrentPathogen(state);
 
@@ -31,16 +59,19 @@ const fetchLocations = (): Promise<LocationsResponse> => {
   return getBackendApiJson(route);
 };
 
-export const USE_LOCATIONS_INFO_QUERY_KEY = {
+export const USE_PATHOGEN_DEPTH_LOCATIONS_INFO_QUERY_KEY: QueryKeyType = {
   entities: [ENTITIES.LOCATION_INFO],
-  id: "locationInfo",
+  id: "pathogenDepthLocationInfo",
 };
 
-const ONE_HOUR = 1000 * 60 * 60; // for react-query; milliseconds
-
-export function useLocations(): UseQueryResult<LocationsResponse, unknown> {
-  return useQuery([USE_LOCATIONS_INFO_QUERY_KEY], fetchLocations, {
+export function useNamedPathogenDepthLocations(
+  queryKey: QueryKeyType
+): UseQueryResult<NamedLocationsResponse, unknown> {
+  return useQuery([queryKey], fetchPathogenDepthLocations, {
     retry: false,
+    // Using `select` allows it to share cache with other USE_LOCATIONS_INFO_QUERY_KEY,
+    // but give a different view on the same data after processed by `select` func.
+    select: foldInNamesToLocations,
     // Because locations are very stable and mildly heavy (~2Meg), we mark
     // stale and re-fetch after an hour rather than default insta-stale.
     // Note, this does not prevent garbage cleanup due to `cacheTime` if user
@@ -77,7 +108,7 @@ export function useNamedLocations(): UseQueryResult<
   NamedLocationsResponse,
   unknown
 > {
-  return useQuery([USE_LOCATIONS_INFO_QUERY_KEY], fetchLocations, {
+  return useQuery([USE_LOCATIONS_INFO_QUERY_KEY], fetchAllLocations, {
     retry: false,
     // Using `select` allows it to share cache with other USE_LOCATIONS_INFO_QUERY_KEY,
     // but give a different view on the same data after processed by `select` func.
@@ -105,7 +136,7 @@ export function useNamedLocationsById(): UseQueryResult<
   NamedLocationsByIdResponse,
   unknown
 > {
-  return useQuery([USE_LOCATIONS_INFO_QUERY_KEY], fetchLocations, {
+  return useQuery([USE_LOCATIONS_INFO_QUERY_KEY], fetchAllLocations, {
     retry: false,
     // Using `select` allows it to share cache with other USE_LOCATIONS_INFO_QUERY_KEY,
     // but give a different view on the same data after processed by `select` func.
