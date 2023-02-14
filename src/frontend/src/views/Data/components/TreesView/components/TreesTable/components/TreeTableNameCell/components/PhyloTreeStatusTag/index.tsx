181ff387d752cc2c77670cc3323503ccbaf0c78e
maya <mlila@chanzuckerberg.com>
2023-02-14 08:19:23 -0800

    add mpx tooltips (#1625)

--- src/frontend/src/views/Data/components/TreesView/components/TreesTable/components/TreeTableNameCell/components/PhyloTreeStatusTag/index.tsx
+++ src/frontend/src/views/Data/components/TreesView/components/TreesTable/components/TreeTableNameCell/components/PhyloTreeStatusTag/index.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from "czifui";
 import { TREE_STATUS } from "src/common/constants/types";
 import { StyledChip } from "./style";
 
@@ -14,23 +15,35 @@ interface Props {
   treeStatus: TREE_STATUS;
 }
 
-interface MapType {
-  [key: string]: CHIP_STATUS;
-}
-
-const STATUS_MAP: MapType = {
-  COMPLETED: "success",
-  FAILED: "error",
-  STARTED: "beta",
+const STATUS_MAP: Record<TREE_STATUS, CHIP_STATUS> = {
+  [TREE_STATUS.Completed]: "success",
+  [TREE_STATUS.Failed]: "error",
+  [TREE_STATUS.Started]: "beta",
 };
 
-const PhyloTreeStatusTag = ({ treeStatus }: Props): JSX.Element => (
-  <StyledChip
-    isRounded
-    label={treeStatus}
-    size="small"
-    status={STATUS_MAP[treeStatus] as CHIP_STATUS}
-  />
-);
+const PhyloTreeStatusTag = ({ treeStatus }: Props): JSX.Element => {
+  const Chip = (
+    <StyledChip
+      isRounded
+      label={treeStatus}
+      size="small"
+      status={STATUS_MAP[treeStatus]}
+    />
+  );
+
+  if (treeStatus === TREE_STATUS.Failed) {
+    return (
+      <Tooltip
+        arrow
+        title="Trees may fail if no samples satisfy the specified location, date, or lineage."
+        sdsStyle="light"
+      >
+        <span>{Chip}</span>
+      </Tooltip>
+    );
+  }
+
+  return Chip;
+};
 
 export { PhyloTreeStatusTag };
