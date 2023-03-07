8d607151c1c942b7efe50585c97dc120ccba711b
Maya Lila <mlila@chanzuckerberg.com>
2023-03-07 15:25:33 -0800

    banner on small screens

--- src/frontend/src/components/NavBar/components/AppNavBar/components/Announcements/style.ts
+++ src/frontend/src/components/NavBar/components/AppNavBar/components/Announcements/style.ts
@@ -1,7 +1,18 @@
 import styled from "@emotion/styled";
-import { Banner } from "czifui";
+import { Banner, getSpaces } from "czifui";
 
 export const StyledBanner = styled(Banner)`
+  text-align: center;
+
+  > :first-child {
+    ${(props) => {
+      const spaces = getSpaces(props);
+      return `
+        margin: 0 ${spaces?.m}px;
+      `;
+    }}
+  }
+
   /* media query - if less than 1490px wide, show on two lines */
   @media (max-width: 1490px) {
     height: 60px;
