77b4e76a05bce18be5ff40d2ff30ded5955e9a83
Jessica Gadling <jgadling@chanzuckerberg.com>
2023-03-21 11:01:35 -0700

    Fix tests.

--- src/backend/pipeline_tests/test_gisaid_pipeline.py
+++ src/backend/pipeline_tests/test_gisaid_pipeline.py
@@ -13,7 +13,7 @@ from aspen.database.models import (
 
 
 def test_public_repo_db_objects_were_created(session):
-    strain_name = "SARS-CoV-2/human/USA/TX-DSHS-9180/2021"
+    strain_name = "USA/TX-DSHS-9180/2021"
     sample = (
         session.execute(
             sa.select(PublicRepositoryMetadata)
@@ -26,13 +26,14 @@ def test_public_repo_db_objects_were_created(session):
         .scalars()
         .one()
     )
-    assert sample.strain == "SARS-CoV-2/human/USA/TX-DSHS-9180/2021"
+    assert sample.strain == strain_name
     assert sample.lineage == "AY.14"
     assert sample.region == "North America"
     assert sample.country == "USA"
     assert sample.division == "Texas"
-    assert sample.location is None
-    assert sample.isl == "OK358761.1"
+    assert sample.location == ""
+    # TODO - I have no idea how this ISL is fetched by ncov-ingest
+    assert sample.isl == "EPI_ISL_4630413"
 
 
 def test_new_workflow_artifacts_exist(session):
@@ -56,14 +57,17 @@ def test_locations_import(session):
     location = (
         session.execute(  # type: ignore
             sa.select(Location)  # type: ignore
-            .where(Location.location == "Test Location")  # type: ignore
+            .where(Location.region == "North America")  # type: ignore
+            .where(Location.country == "USA")  # type: ignore
+            .where(Location.division == "Texas")  # type: ignore
+            .where(Location.location == None)  # type: ignore
             .limit(1)  # type: ignore
         )
         .scalars()
         .one()
     )
 
-    assert location.region == "Asia"
-    assert location.country == "China"
-    assert location.division == "Hubei"
-    assert location.location == "Test Location"
+    assert location.region == "North America"
+    assert location.country == "USA"
+    assert location.division == "Texas"
+    assert location.location is None
