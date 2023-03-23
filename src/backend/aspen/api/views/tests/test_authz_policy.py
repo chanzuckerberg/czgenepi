e6f2a2823955f66e89e2683907cf57bce2ec2586
Jessica Gadling <jgadling@chanzuckerberg.com>
2023-03-23 15:05:11 -0700

    Update tests.

--- src/backend/aspen/api/views/tests/test_authz_policy.py
+++ src/backend/aspen/api/views/tests/test_authz_policy.py
@@ -14,6 +14,7 @@ from aspen.test_infra.models.location import location_factory
 from aspen.test_infra.models.pathogen import pathogen_factory
 from aspen.test_infra.models.phylo_tree import phylorun_factory, phylotree_factory
 from aspen.test_infra.models.sample import sample_factory
+from aspen.test_infra.models.repository import random_repo_factory
 from aspen.test_infra.models.usergroup import (
     group_factory,
     grouprole_factory,
@@ -100,6 +101,7 @@ async def appdata(
     objects_to_commit = []
     trees: List[PhyloTree] = []
     pathogen = pathogen_factory()
+    repo = random_repo_factory()
     for i in range(len(groups)):
         group = groups[i]
         user = users[i]
@@ -118,7 +120,7 @@ async def appdata(
                 private_identifier=f"private_identifier_{group.name}_{i}",
                 public_identifier=f"public_identifier_{group.name}_{i}",
             )
-            phylo_run = phylorun_factory(group, pathogen=pathogen)
+            phylo_run = phylorun_factory(group, pathogen=pathogen, contextual_repository=repo)
             tree = phylotree_factory(phylo_run, [sample])
 
             group.test_samples.append(sample)
