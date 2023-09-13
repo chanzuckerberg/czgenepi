ac3705eac5ffd832563afecdafff3ebbdef605c6
phoenixAja <plogan@chanzuckerberg>
2023-09-13 10:27:36 -0700

    follow_redirects for failed tests

--- src/backend/aspen/api/views/tests/test_health.py
+++ src/backend/aspen/api/views/tests/test_health.py
@@ -5,6 +5,6 @@ pytestmark = pytest.mark.asyncio
 
 
 async def test_healthcheck(http_client: AsyncClient) -> None:
-    response = await http_client.get("/v2/health")
+    response = await http_client.get("/v2/health", follow_redirects=True)
     assert response.status_code == 200
     assert response.json() == {"healthy": True}
