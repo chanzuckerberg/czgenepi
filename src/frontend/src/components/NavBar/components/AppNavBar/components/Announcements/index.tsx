7f6757ed90407da8778cfa52d51ac4032dad8c71
Vincent Selhorst-Jones <vselhorst-jones@chanzuckerberg.com>
2024-06-14 11:04:53 -0700

    Add dashed underline to link for in-app

--- src/frontend/src/components/NavBar/components/AppNavBar/components/Announcements/index.tsx
+++ src/frontend/src/components/NavBar/components/AppNavBar/components/Announcements/index.tsx
@@ -23,6 +23,7 @@ export const Announcements = (): JSX.Element => {
           <B>THEIAGEN GLOBAL HEALTH INITIATIVE WILL MANAGE CZ GEN EPI ON OCTOBER 25, 2024. CLICK&nbsp;</B>
           <StyledNewTabLink
             href="https://help.czgenepi.org/hc/en-us/articles/20083077583764-FAQs-CZ-GEN-EPI-Transfer-to-Theiagen-Global-Health-Initiative-TGHI"
+            sdsStyle="dashed"
           >
             HERE
           </StyledNewTabLink>
